af6dbbf714e62ff89dce6116b091a4ff3a7534eb
Tom Jonkman <tom@tomjonkman.com>
2022-04-20 21:04:43 +0200

    Remove CSS property format from style-dictionary

--- proprietary/design-tokens/src/style-dictionary.config.js
+++ proprietary/design-tokens/src/style-dictionary.config.js
@@ -1,4 +1,3 @@
-const cssPropertyFormat = require('./css-property-formatter.js');
 const jsonListFormat = require('./json-list-formatter.js');
 
 const stringSort = (a, b) => (a === b ? 0 : a > b ? 1 : -1);
@@ -6,7 +5,6 @@ const destinationSort = (a, b) => stringSort(a.destination, b.destination);
 
 module.exports = {
   format: {
-    ...cssPropertyFormat,
     ...jsonListFormat,
   },
   source: ['../../components/**/*.tokens.json', './src/**/*.tokens.json'],
@@ -32,10 +30,6 @@ module.exports = {
             selector: '.nl-rvo-theme',
             outputReferences: true,
           },
-        },
-        {
-          destination: 'property.css',
-          format: 'css/property',
         },
       ].sort(destinationSort),
     },
