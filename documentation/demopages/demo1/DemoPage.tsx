269666cbc7aa96b9b65feacff2cbc30539929e4a
Robert Roose <rroose@gmail.com>
2022-05-17 16:14:33 +0200

    Feature/demopage (#81)
    * Fix storybook warning
    
    * Add initial demopage with normalize.css
    
    * Add initial style.css
    
    * Fix EOL
    
    * Add images and icons
    
    * Fix logo in demo-page
    
    * Add images and icons
    
    * Fix storybook story order
    
    * Adds -rvo prefix to classnames
    
    * Move demo1 to demopages
    
    * Fix CSS issues
    
    * Add normalize.css dependecy
    
    * Fix demopage rendering
    
    * Fix lint issues
    
    Co-authored-by: Tom Jonkman <tom@tomjonkman.com>
    Co-authored-by: Robert Roose <robert.roose@rvo.nl>
    Co-authored-by: Tom Jonkman <tom.jonkman@rvo.nl>

---
+++ documentation/demopages/demo1/DemoPage.tsx
@@ -0,0 +1,130 @@
+import React from 'react';
+import 'normalize.css';
+import './style.css';
+import logo from './assets/img/logo.svg';
+
+export const DemoPage = () => {
+  return (
+    <>
+      <header className="rvo-header">
+        <div className="rvo-max-width-wrapper">
+          <div className="rvo-logo__wrapper">
+            <a href="#" className="rvo-logo__link">
+              <img src={logo} className="rvo-logo__img" />
+            </a>
+          </div>
+        </div>
+      </header>
+      <div className="rvo-navigation rvo-navigation--hemelblauw">
+        <div className="rvo-max-width-wrapper">
+          <nav className="rvo-main-menu">
+            <ul className="rvo-menu rvo-menu--main-menu">
+              <li className="rvo-menu-item">
+                <a href="#" className="rvo-menu-link rvo-menu-link--medium">
+                  <span className="rvo-icon rvo-icon--medium rvo-icon--before rvo-icon--wit rvo-icon--home"></span>Mijn
+                  aanvragen
+                </a>
+              </li>
+              <li className="rvo-menu-item">
+                <a href="#" className="rvo-menu-link rvo-menu-link--medium">
+                  <span className="rvo-icon rvo-icon--medium rvo-icon--before rvo-icon--wit rvo-icon--plus"></span>
+                  Nieuwe aanvraag
+                </a>
+              </li>
+              <li className="rvo-menu-item">
+                <a href="#" className="rvo-menu-link rvo-menu-link--medium">
+                  <span className="rvo-icon rvo-icon--medium rvo-icon--before rvo-icon--wit rvo-icon--user"></span>Mijn
+                  gegevens
+                </a>
+              </li>
+              <li className="rvo-menu-item">
+                <a href="#" className="rvo-menu-link rvo-menu-link--medium">
+                  Uitloggen
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <nav className="rvo-toggle-menu">
+            <ul className="rvo-menu rvo-menu--toggle-menu">
+              <li className="rvo-menu-item">
+                <a href="#" className="rvo-menu-link rvo-menu-link--medium rvo-menu-link--toggle">
+                  <span className="rvo-icon rvo-icon--medium rvo-icon--before rvo-icon--wit rvo-icon--menu"></span>Menu
+                  toggle
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+
+      <main className="rvo-progress-tracker-active">
+        <div className="rvo-progress-tracker rvo-progress-tracker--vertical">
+          <div className="rvo-main-steps rvo-main-steps--no-sub-steps">
+            <div className="rvo-process-step rvo-process-step--process-name">Aanvraag WBSO</div>
+            <div className="rvo-process-step rvo-process-step--default rvo-process-step--medium rvo-process-step--doing">
+              <a href="vormvrije-aanvraag.html" className="rvo-process-step-link">
+                Vormvrije aanvraag
+              </a>
+            </div>
+            <div className="rvo-process-step rvo-process-step--default rvo-process-step--medium rvo-process-step--incomplete">
+              <a href="startpagina.html" className="rvo-process-step-link">
+                Startpagina
+              </a>
+            </div>
+            <div className="rvo-process-step rvo-process-step--default rvo-process-step--medium rvo-process-step--incomplete">
+              <a href="aanvrager.html" className="rvo-process-step-link">
+                Aanvrager
+              </a>
+            </div>
+            <div className="rvo-process-step rvo-process-step--default rvo-process-step--medium rvo-process-step--incomplete">
+              <a href="projecten.html" className="rvo-process-step-link">
+                Projecten
+              </a>
+            </div>
+            <div className="rvo-process-step rvo-process-step--default rvo-process-step--medium rvo-process-step--incomplete">
+              <a href="so-uren.html" className="rvo-process-step-link">
+                S&O uren
+              </a>
+            </div>
+            <div className="rvo-process-step rvo-process-step--default rvo-process-step--medium rvo-process-step--incomplete">
+              <a href="indienen.html" className="rvo-process-step-link">
+                Indienen
+              </a>
+            </div>
+            <div className="rvo-process-step rvo-process-step--process-completed">Aanvraag ingediend</div>
+          </div>
+        </div>
+        <div className="rvo-form">
+          <form action="startpagina.html" method="get">
+            <fieldset>
+              <h2>Vormvrije aanvraag</h2>
+              <div className="rvo-form-field rvo-form-field--options">
+                <legend>
+                  Wilt u een vormvrije aanvraag indienen?
+                  <span className="rvo-helper-text">
+                    Heeft u geen tijd om een volledige WBSO-aanvraag in te dienen? Dien dan een vormvrije aanvraag in. U
+                    vult een paar basisgegevens in en vult uw aanvraag later aan.
+                  </span>
+                </legend>
+                <div className="rvo-options rvo-options--rows">
+                  <label htmlFor="radio-a" className="rvo-option">
+                    <input type="radio" id="radio-a" name="radios" />
+                    Ja
+                  </label>
+                  <label htmlFor="radio-b" className="rvo-option">
+                    <input type="radio" id="radio-b" name="radios" />
+                    Nee
+                  </label>
+                </div>
+              </div>
+            </fieldset>
+            <div className="rvo-button-group">
+              <button className="rvo-button rvo-button--tertiary rvo-button--small rvo-button--error">Annuleren</button>
+              <button className="rvo-button rvo-button--primary rvo-button--medium">Start aanvraag</button>
+            </div>
+          </form>
+        </div>
+      </main>
+    </>
+  );
+};
