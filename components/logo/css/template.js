18d8e7fb5acc215c71cb2b02335ae3700415d4e9
Tom Jonkman <tom.jonkman@rvo.nl>
2022-05-30 11:12:53 +0200

    Add initial Logo

---
+++ components/logo/css/template.js
@@ -0,0 +1,11 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2021 Community for NL Design System
+ */
+import logo from '../logo.svg';
+
+export const Logo = () => `
+<a href="#" className="rvo-logo__link">
+<img src="${logo}" className="rvo-logo__img" />
+</a>
+`;
