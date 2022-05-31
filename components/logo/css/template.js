6507dce01f012eb9c9002e1a30876d0a3a8adf7c
Tom Jonkman <tom@tomjonkman.com>
2022-05-31 11:22:49 +0200

    Feature/logo ribbon (#112)
    * Add initial Logo
    
    * Add initial header
    
    * Fix responsiveness for header
    
    * Move asset types in separate folders
    
    Co-authored-by: Tom Jonkman <tom.jonkman@rvo.nl>

--- components/logo/css/template.js
+++ components/logo/css/template.js
@@ -2,7 +2,7 @@
  * @license EUPL-1.2
  * Copyright (c) 2021 Community for NL Design System
  */
-import logo from '@nl-rvo/assets/logo.svg';
+import logo from '@nl-rvo/assets/images/logo.svg';
 import clsx from 'clsx';
 import React from 'react';
 
