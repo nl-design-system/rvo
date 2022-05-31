2896474c8a1aaccc0ab3eed6d45f3de03e226e20
Tom Jonkman <tom.jonkman@rvo.nl>
2022-05-31 14:41:50 +0200

    Add initial icon

---
+++ components/icon/css/template.js
@@ -0,0 +1,41 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2021 Community for NL Design System
+ */
+import iconList from '@nl-rvo/assets/icons';
+import React from 'react';
+
+String.prototype.toProperCase = function () {
+  return this.replace(/\w\S*/g, function (txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+};
+
+const options = Object.keys(iconList).flatMap((categoryName) => {
+  return Object.keys(iconList[categoryName]).map((iconName) => `${categoryName} > ${iconName}`.toProperCase());
+});
+
+export const argTypes = {
+  icon: {
+    description: 'Icon',
+    control: 'select',
+    options: options,
+  },
+};
+
+export const defaultArgs = {
+  icon: options[0],
+};
+
+export const Icon = ({ icon = defaultArgs.icon }) => {
+  const [categoryName, iconName] = icon.split(' > ');
+  const iconUrl = iconList[categoryName.toUpperCase()][iconName.toUpperCase()];
+
+  console.log(iconUrl);
+
+  return (
+    <header className="rvo-header">
+      <div className="rvo-header__logo-wrapper">{icon}</div>
+    </header>
+  );
+};
