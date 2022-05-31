c03866108eff632d9370e1973b5cdc90a5f220da
Robert Roose <robert.roose@rvo.nl>
2022-05-31 16:16:02 +0200

    Add correct html tot aanvrager demo page

--- documentation/demopages/wbso/Aanvrager.tsx
+++ documentation/demopages/wbso/Aanvrager.tsx
@@ -114,6 +114,94 @@ const Aanvrager = () => {
               <dd>200000007</dd>
             </dl>
           </div>
+          <form>
+            <fieldset>
+              <h2>Contactpersoon</h2>
+              <div className="rvo-form-field">
+                <label htmlFor="contactpersoon">Contactpersoon</label>
+                <select id="contactpersoon">
+                  <option>Optie A</option>
+                </select>
+                <button className="rvo-button rvo-button--tertiary rvo-button--small rvo-button-in-form">
+                  <span className="rvo-icon rvo-icon--sm rvo-icon-before rvo-icon--hemelblauw rvo-icon--bewerken"></span>
+                  Contactpersoon & bedrijfsgegevens wijzigen
+                </button>
+              </div>
+              <h2>Organisatie</h2>
+              <div className="rvo-form-field rvo-form-field--options">
+                <legend>
+                  U drijft een onderneming in de zin van de Wet IB of Wet Vpb en bent geen publieke kennisinstelling?
+                  <span className="rvo-helper-text">
+                    Bent u geen ondernemer in de zin van de Wet IB (Inkomstenbelasting 2001) of Wet VpB
+                    (Vennootschapsbelasting 1969), dan kunt u geen WBSO aanvragen. U kunt ook geen WBSO aanvragen
+                    indienen als u een publieke kennisinstelling bent (zie artikel 1, onderdeel m van de WVA).
+                  </span>
+                </legend>
+                <div className="rvo-options rvo-options--rows">
+                  <label htmlFor="ib-ja" className="rvo-option">
+                    <input type="radio" id="ib-ja" name="ib"></input>Ja
+                  </label>
+                  <label htmlFor="ib-nee" className="rvo-option">
+                    <input type="radio" id="ib-nee" name="ib"></input>Nee
+                  </label>
+                </div>
+                <h2>Gegevens aanvrager in aanvraagperiode</h2>
+                <div className="rvo-form-field">
+                  <label htmlFor="aantal-werknemers">Prognose totaal aantal werknemers</label>
+                  <input type="text" inputMode="numeric" pattern="[0-9]*" id="aantal-werknemers"></input>
+                </div>
+                <div className="rvo-form-field">
+                  <label htmlFor="eigen-werknemers">(Waarvan) eigen S&O-werknemers</label>
+                  <input type="text" inputMode="numeric" pattern="[0-9]*" id="eigen-werknemers"></input>
+                </div>
+                <h2>S&O-uurloon</h2>
+                <label htmlFor="so-verklaring">
+                  Heeft u in 2020 S&O-werkzaamheden verricht waarvoor u een S&O-verklaring heeft ontvangen?
+                  <div className="rvo-helper-text rvo-helper-text--warning">
+                    Voor u geldt in 2022 een forfaitar uurloon van €29.
+                  </div>
+                </label>
+                <div className="rvo-options rvo-options--rows">
+                  <label htmlFor="so-ja" className="rvo-option">
+                    <input type="radio" id="so-ja" name="so"></input>Ja
+                  </label>
+                  <label htmlFor="so-nee" className="rvo-option">
+                    <input type="radio" id="so-nee" name="so"></input>Nee
+                  </label>
+                </div>
+                <h2>Technologiegebied</h2>
+                <div className="rvo-form-field">
+                  <label htmlFor="technologiegebied">Technologiegebied</label>
+                  <select id="technologiegebied">
+                    <option>Optie A</option>
+                  </select>
+                </div>
+                <details>
+                  <summary>Overzicht technologiegebieden</summary>
+                  <dl className="rvo-data">
+                    <dt>aard- en milieuwetenschappen</dt>
+                    <dd>geowetenschappen | meteorologie | klimaatonderzoek | water & ecologie onderzoek</dd>
+                  </dl>
+                </details>
+                <h2>Periode aanvraag</h2>
+                <div className="rvo-form-field">
+                  <label htmlFor="maand-aanvang">
+                    Maand aanvang
+                    <div className="rvo-helper-text">
+                      Vul hier de startmaand in van de periode waarover u WBSO aanvraagt.
+                    </div>
+                  </label>
+                  <select id="maand-aanvang">
+                    <option>Januari</option>
+                  </select>
+                </div>
+              </div>
+            </fieldset>
+            <div className="rvo-button-group">
+              <button className="rvo-button rvo-button--secondary rvo-button--medium">Opslaan en sluiten</button>
+              <button className="rvo-button rvo-button--primary rvo-button--medium">Opslaan en verder</button>
+            </div>
+          </form>
         </div>
       </main>
     </div>
