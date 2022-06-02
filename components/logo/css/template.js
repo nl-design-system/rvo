49b48c867439f98576a7bdc09c4ef4720fd08409
Tom Jonkman <tom.jonkman@rvo.nl>
2022-06-02 13:24:15 +0200

    Use static dir for logo image

--- components/logo/css/template.js
+++ components/logo/css/template.js
@@ -2,7 +2,6 @@
  * @license EUPL-1.2
  * Copyright (c) 2021 Community for NL Design System
  */
-import logo from '@nl-rvo/assets/images/logo.svg';
 import clsx from 'clsx';
 
-export const Logo = ({ className = '' }) => `<img src="${logo}" class="${clsx('rvo-logo', className)}" />`;
+export const Logo = ({ className = '' }) => `<img src="/images/logo.svg" class="${clsx('rvo-logo', className)}" />`;
