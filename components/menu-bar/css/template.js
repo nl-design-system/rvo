3631bd5a2fd0a4bada9dcafdfaa12b11bf38d00f
Tom Jonkman <tom.jonkman@rvo.nl>
2022-06-09 11:19:01 +0200

    Add initial menu bar component

---
+++ components/menu-bar/css/template.js
@@ -0,0 +1,29 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2021 Community for NL Design System
+ */
+import '@utrecht/component-library-css';
+
+export const argTypes = {};
+
+export const defaultArgs = {};
+
+export const MenuBar = () =>
+  `<div class="utrecht-navhtml">
+  <nav class="topnav">
+    <ul class="utrecht-topnav__list">
+      <li class="utrecht-topnav__item">
+        <a class="utrecht-topnav__link" href="https://example.com/">Wonen en leven</a>
+        </li>
+      <li class="utrecht-topnav__item">
+        <a class="utrecht-topnav__link" href="https://example.com/">Werk en inkomen</a>
+        </li>
+      <li class="utrecht-topnav__item">
+        <a class="utrecht-topnav__link" href="https://example.com/">Ondernemen</a>
+        </li>
+      <li class="utrecht-topnav__item">
+        <a class="utrecht-topnav__link" href="https://example.com/">Bestuur en organisatie</a>
+        </li>
+    </ul>
+  </nav>
+</div>`;
