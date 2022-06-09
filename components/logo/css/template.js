587df8bb6ee94d240baa5874f4e291aa58727a95
Tom Jonkman <tom.jonkman@rvo.nl>
2022-06-09 18:29:11 +0200

    Import logo asset

--- components/logo/css/template.js
+++ components/logo/css/template.js
@@ -3,5 +3,6 @@
  * Copyright (c) 2021 Community for NL Design System
  */
 import clsx from 'clsx';
+import '@nl-rvo/assets/images/logo.svg';
 
 export const Logo = ({ className = '' }) => `<img src="/images/logo.svg" class="${clsx('rvo-logo', className)}" />`;
