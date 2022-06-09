d965b4303401f74c7a6c1c1b5376133b7ebeab95
Tom Jonkman <tom@tomjonkman.com>
2022-06-09 17:31:48 +0200

    Feature/menu (#129)
    * Add initial menu bar component
    
    * Add max width layout token to menu bar
    
    * Add menu bar stories with and without icons
    
    * Add menu bar with icons story and story args
    
    * Rename menu-bar to menubar
    
    * Adjust menubar story description
    
    * Add sizes and component design-tokens
    
    Co-authored-by: Tom Jonkman <tom.jonkman@rvo.nl>

---
+++ components/menubar/css/template.js
@@ -0,0 +1,70 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2021 Community for NL Design System
+ */
+import '@utrecht/component-library-css';
+
+export const argTypes = {
+  size: {
+    options: ['small', 'medium'],
+    control: { type: 'radio' },
+  },
+  useIcons: {
+    control: 'boolean',
+  },
+  iconAlign: {
+    options: ['left', 'right'],
+    control: { type: 'radio' },
+  },
+};
+
+export const defaultArgs = {
+  size: 'medium',
+  useIcons: false,
+  iconAlign: 'left',
+};
+
+const parseMenuItem = (label, icon, size = defaultArgs.size, iconAlign = defaultArgs.iconAlign) => {
+  let menuItem = label;
+  if (icon) {
+    const iconMarkup = `<span class="rvo-icon rvo-icon--${icon} rvo-icon--${size} rvo-icon--wit"></span>`;
+    if (iconAlign === 'left') {
+      menuItem = `${iconMarkup} ${menuItem}`;
+    } else {
+      menuItem = `${menuItem} ${iconMarkup}`;
+    }
+  }
+  return menuItem;
+};
+
+export const MenuBar = ({
+  size = defaultArgs.size,
+  useIcons = defaultArgs.useIcons,
+  iconAlign = defaultArgs.iconAlign,
+}) => {
+  return `<nav class="rvo-topnav rvo-topnav--${size}">
+    <ul class="utrecht-topnav__list">
+      <li class="utrecht-topnav__item">
+        <a class="utrecht-topnav__link" href="#">
+          ${parseMenuItem('Wonen en leven', useIcons && 'home', 'medium', iconAlign)}
+        </a>
+      </li>
+      <li class="utrecht-topnav__item">
+        <a class="utrecht-topnav__link" href="#">
+         ${parseMenuItem('Werk en inkomen', useIcons && 'kalender', 'medium', iconAlign)}
+        </a>
+      </li>
+      <li class="utrecht-topnav__item">
+        <a class="utrecht-topnav__link" href="#">
+          ${parseMenuItem('Ondernemen', useIcons && 'kalender', 'medium', iconAlign)}
+        </a>
+      </li>
+      <li class="utrecht-topnav__item">
+        <a class="utrecht-topnav__link" href="#">
+          ${parseMenuItem('Bestuur en organisatie', useIcons && 'versleutelen', 'medium', iconAlign)}
+        </a>
+      </li>
+    </ul>
+  </nav>
+`;
+};
---
+++ components/menubar/design.md
@@ -0,0 +1,7 @@
+<!-- @license CC0-1.0 -->
+
+<!-- markdownlint-disable first-line-h1 -->
+
+## Design
+
+[Figma - RVO Design System - Menu item](hhttps://www.figma.com/file/NHV1JYxJ28vKZInSI9u200/?node-id=10%3A26)
