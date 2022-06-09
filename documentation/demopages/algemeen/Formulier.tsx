0b36c370a509ffcbbe53eef36fbe021cdda6d82d
Robert Roose <robert.roose@rvo.nl>
2022-06-09 16:58:11 +0200

    Add HTML to form demo page

--- documentation/demopages/algemeen/Formulier.tsx
+++ documentation/demopages/algemeen/Formulier.tsx
@@ -65,38 +65,38 @@ const Formulier = () => {
           <div className="rvo-sub-steps">
             <div className="rvo-process-step rvo-process-step--default rvo-process-step--sub rvo-process-step--small rvo-process-step--incomplete">
               <a href="#" className="rvo-process-step-link">
-                Process first sub-step
+                First sub-step
               </a>
             </div>
             <div className="rvo-process-step rvo-process-step--default rvo-process-step--sub rvo-process-step--small rvo-process-step--doing">
               <a href="#" className="rvo-process-step-link">
-                Process sub-step doing
+                Sub-step doing
               </a>
             </div>
             <div className="rvo-process-step rvo-process-step--default rvo-process-step--sub rvo-process-step--small rvo-process-step--completed">
               <a href="#" className="rvo-process-step-link">
-                Process sub-step completed
+                Sub-step completed
               </a>
             </div>
             <div className="rvo-process-step rvo-process-step--default rvo-process-step--sub rvo-process-step--small rvo-process-step--incomplete">
-              <span className="rvo-process-step-link rvo-process-step-link--disabled">Process sub-step disabled</span>
+              <span className="rvo-process-step-link rvo-process-step-link--disabled">Sub-step disabled</span>
             </div>
             <div className="rvo-process-step rvo-process-step--default rvo-process-step--sub rvo-process-step--small rvo-process-step--incomplete">
               <a href="#" className="rvo-process-step-link">
-                Process last sub-step
+                Last sub-step
               </a>
             </div>
           </div>
           <div className="rvo-main-steps">
             <div className="rvo-process-step rvo-process-step--default rvo-process-step--medium rvo-process-step--incomplete">
-              <span className="rvo-process-step-link rvo-process-step-link--disabled">Process step disabled</span>
+              <span className="rvo-process-step-link rvo-process-step-link--disabled">Step disabled</span>
             </div>
             <div className="rvo-process-step rvo-process-step--process-completed">Process completed</div>
           </div>
         </div>
         <div className="rvo-form">
           <div className="intro">
-            <a href="startpagina.html" className="back-link">
+            <a href="startpagina.html" className="rvo-back-link">
               <span className="rvo-icon rvo-icon--sm rvo-icon--before rvo-icon--hemelblauw rvo-icon--terug"></span>Back
               link
             </a>
@@ -104,93 +104,142 @@ const Formulier = () => {
           </div>
           <form>
             <fieldset>
-              <h2>Contactpersoon</h2>
+              <h2>Keyboard inputs</h2>
               <div className="rvo-form-field">
-                <label htmlFor="contactpersoon">Contactpersoon</label>
-                <select id="contactpersoon">
-                  <option>Optie A</option>
-                </select>
-                <button className="rvo-button rvo-button--tertiary rvo-button--small rvo-button-in-form">
-                  <span className="rvo-icon rvo-icon--sm rvo-icon-before rvo-icon--hemelblauw rvo-icon--bewerken"></span>
-                  Contactpersoon & bedrijfsgegevens wijzigen
-                </button>
+                <label htmlFor="text">Text</label>
+                <input type="text" id="text"></input>
               </div>
-              <h2>Organisatie</h2>
+              <div className="rvo-form-field">
+                <label htmlFor="text-w-helper-text">
+                  Text with helper text
+                  <div className="rvo-helper-text">This is a helper text which can be used for instructions.</div>
+                </label>
+                <input type="text" id="text-w-helper-text"></input>
+              </div>
+              <div className="rvo-form-field">
+                <label htmlFor="text-w-expandable-helper-text">
+                  Text with expandable helper text
+                  <details className="rvo-helper-text">
+                    <summary>Expandable helper text</summary>
+                    <span className="rvo-details-text">
+                      When instructions run over multiple lines an expandable helper text can be used.
+                    </span>
+                  </details>
+                </label>
+                <input type="text" id="text-w-expandable-helper-text"></input>
+              </div>
+              <div className="rvo-form-field">
+                <label htmlFor="text-disabled">Text disabled</label>
+                <input type="text" id="text-disabled" disabled></input>
+              </div>
+              <div className="rvo-form-field">
+                <label htmlFor="text-disabled-w-value">Text disabled with value</label>
+                <input type="text" id="text-disabled-w-value" value="Value" disabled></input>
+              </div>
+              <div className="rvo-form-field">
+                <label htmlFor="number">Number</label>
+                <input type="text" inputMode="numeric" pattern="[0-9]*" id="number"></input>
+              </div>
+              <div className="rvo-form-field">
+                <label htmlFor="textarea">Textarea</label>
+                <textarea id="textarea"></textarea>
+              </div>
+            </fieldset>
+            <fieldset>
+              <h2>Options</h2>
+              <div className="rvo-form-field rvo-form-field--options">
+                <legend>Legend</legend>
+                <div className="rvo-options">
+                  <label htmlFor="option-a" className="rvo-option">
+                    <input type="radio" id="option-a" name="radio-options"></input>Option A
+                  </label>
+                  <label htmlFor="option-b" className="rvo-option">
+                    <input type="radio" id="option-b" name="radio-options"></input>Option B
+                  </label>
+                  <label htmlFor="option-c" className="rvo-option">
+                    <input type="radio" id="option-c" name="radio-options"></input>Option C
+                  </label>
+                </div>
+              </div>
               <div className="rvo-form-field rvo-form-field--options">
                 <legend>
-                  U drijft een onderneming in de zin van de Wet IB of Wet Vpb en bent geen publieke kennisinstelling?
-                  <span className="rvo-helper-text">
-                    Bent u geen ondernemer in de zin van de Wet IB (Inkomstenbelasting 2001) of Wet VpB
-                    (Vennootschapsbelasting 1969), dan kunt u geen WBSO aanvragen. U kunt ook geen WBSO aanvragen
-                    indienen als u een publieke kennisinstelling bent (zie artikel 1, onderdeel m van de WVA).
-                  </span>
+                  Legend
+                  <span className="rvo-helper-text">Radio buttons displayed on the same row.</span>
                 </legend>
                 <div className="rvo-options rvo-options--rows">
-                  <label htmlFor="ib-ja" className="rvo-option">
-                    <input type="radio" id="ib-ja" name="ib"></input>Ja
+                  <label htmlFor="option-row-a" className="rvo-option">
+                    <input type="radio" id="option-row-a" name="radio-options-row"></input>Option A
                   </label>
-                  <label htmlFor="ib-nee" className="rvo-option">
-                    <input type="radio" id="ib-nee" name="ib"></input>Nee
+                  <label htmlFor="option-row-b" className="rvo-option">
+                    <input type="radio" id="option-row-b" name="radio-options-row"></input>Option B
+                  </label>
+                  <label htmlFor="option-row-c" className="rvo-option">
+                    <input type="radio" id="option-row-c" name="radio-options-row"></input>Option C
                   </label>
                 </div>
-                <h2>Gegevens aanvrager in aanvraagperiode</h2>
-                <div className="rvo-form-field">
-                  <label htmlFor="aantal-werknemers">Prognose totaal aantal werknemers</label>
-                  <input type="text" inputMode="numeric" pattern="[0-9]*" id="aantal-werknemers"></input>
+              </div>
+              <div className="rvo-form-field rvo-form-field--options">
+                <legend>Legend</legend>
+                <div className="rvo-options">
+                  <label htmlFor="checkbox-option-a" className="rvo-option">
+                    <input type="checkbox" id="checkbox-option-a" name="checkboxes-options"></input>Option A
+                  </label>
+                  <label htmlFor="checkbox-option-b" className="rvo-option">
+                    <input type="checkbox" id="checkbox-option-b" name="checkboxes-options"></input>Option B
+                  </label>
+                  <label htmlFor="checkbox-option-c" className="rvo-option">
+                    <input type="checkbox" id="checkbox-option-c" name="checkboxes-options"></input>Option C
+                  </label>
                 </div>
-                <div className="rvo-form-field">
-                  <label htmlFor="eigen-werknemers">(Waarvan) eigen S&O-werknemers</label>
-                  <input type="text" inputMode="numeric" pattern="[0-9]*" id="eigen-werknemers"></input>
-                </div>
-                <h2>S&O-uurloon</h2>
-                <label htmlFor="so-verklaring">
-                  Heeft u in 2020 S&O-werkzaamheden verricht waarvoor u een S&O-verklaring heeft ontvangen?
-                  <div className="rvo-helper-text rvo-helper-text--warning">
-                    Voor u geldt in 2022 een forfaitar uurloon van €29.
-                  </div>
-                </label>
+              </div>
+              <div className="rvo-form-field rvo-form-field--options">
+                <legend>
+                  Legend
+                  <span className="rvo-helper-text">Checkboxes displayed on the same row.</span>
+                </legend>
                 <div className="rvo-options rvo-options--rows">
-                  <label htmlFor="so-ja" className="rvo-option">
-                    <input type="radio" id="so-ja" name="so"></input>Ja
+                  <label htmlFor="checkbox-option-row-a" className="rvo-option">
+                    <input type="checkbox" id="checkbox-option-row-a" name="checkboxes-options-row"></input>Option A
                   </label>
-                  <label htmlFor="so-nee" className="rvo-option">
-                    <input type="radio" id="so-nee" name="so"></input>Nee
+                  <label htmlFor="checkbox-option-row-b" className="rvo-option">
+                    <input type="checkbox" id="checkbox-option-row-b" name="checkboxes-options-row"></input>Option B
+                  </label>
+                  <label htmlFor="checkbox-option-row-c" className="rvo-option">
+                    <input type="checkbox" id="checkbox-option-row-c" name="checkboxes-options-row"></input>Option C
                   </label>
                 </div>
-                <h2>Technologiegebied</h2>
-                <div className="rvo-form-field">
-                  <label htmlFor="technologiegebied">Technologiegebied</label>
-                  <select id="technologiegebied">
-                    <option>Optie A</option>
-                  </select>
-                </div>
-                <details>
-                  <summary>Overzicht technologiegebieden</summary>
-                  <dl className="rvo-data">
-                    <dt>aard- en milieuwetenschappen</dt>
-                    <dd>geowetenschappen | meteorologie | klimaatonderzoek | water & ecologie onderzoek</dd>
-                  </dl>
-                </details>
-                <h2>Periode aanvraag</h2>
-                <div className="rvo-form-field">
-                  <label htmlFor="maand-aanvang">
-                    Maand aanvang
-                    <div className="rvo-helper-text">
-                      Vul hier de startmaand in van de periode waarover u WBSO aanvraagt.
-                    </div>
-                  </label>
-                  <select id="maand-aanvang">
-                    <option>Januari</option>
-                  </select>
-                </div>
+              </div>
+              <div className="rvo-form-field">
+                <label htmlFor="Select">Select</label>
+                <select id="select">
+                  <option>Option A</option>
+                  <option>Option B</option>
+                  <option>Option C</option>
+                </select>
+              </div>
+            </fieldset>
+            <fieldset>
+              <div className="rvo-form-field">
+                <label htmlFor="warning-text">
+                  Warning
+                  <span className="rvo-helper-text rvo-helper-text--warning">This is a warning</span>
+                </label>
+                <input type="text" id="warning-text"></input>
+              </div>
+              <div className="rvo-form-field">
+                <label htmlFor="warning-error">
+                  Error
+                  <span className="rvo-helper-text rvo-helper-text--error">This is a warning</span>
+                </label>
+                <input type="text" id="error-text" className="rvo-input-error"></input>
               </div>
             </fieldset>
             <div className="rvo-button-group">
               <button className="utrecht-button utrecht-button--secondary-action rvo-button--medium">
-                Opslaan en sluiten
+                Secondary action
               </button>
               <button className="utrecht-button utrecht-button--primary-action rvo-button--medium">
-                Opslaan en verder
+                Primary action
               </button>
             </div>
           </form>
