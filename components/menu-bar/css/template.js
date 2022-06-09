e2d0c3c38e8fcd5e546850fa3d66d6b731370f98
Tom Jonkman <tom.jonkman@rvo.nl>
2022-06-09 16:30:54 +0200

    Add menu bar with icons story and story args

--- components/menu-bar/css/template.js
+++ components/menu-bar/css/template.js
@@ -4,37 +4,63 @@
  */
 import '@utrecht/component-library-css';
 
-export const argTypes = {};
+export const argTypes = {
+  size: {
+    options: ['small', 'medium'],
+    control: { type: 'radio' },
+  },
+  useIcons: {
+    control: 'boolean',
+  },
+  iconAlign: {
+    options: ['left', 'right'],
+    control: { type: 'radio' },
+  },
+};
 
-export const defaultArgs = {};
+export const defaultArgs = {
+  size: 'medium',
+  useIcons: false,
+  iconAlign: 'left',
+};
 
-export const MenuBar = ({ useIcons = false }) =>
-  `
-  <nav class="rvo-topnav">
+const parseMenuItem = (label, icon, size = defaultArgs.size, iconAlign = defaultArgs.iconAlign) => {
+  let menuItem = label;
+  if (icon) {
+    const iconMarkup = `<span class="rvo-icon rvo-icon--${icon} rvo-icon--${size} rvo-icon--wit"></span>`;
+    if (iconAlign === 'left') {
+      menuItem = `${iconMarkup} ${menuItem}`;
+    } else {
+      menuItem = `${menuItem} ${iconMarkup}`;
+    }
+  }
+  return menuItem;
+};
+
+export const MenuBar = ({ useIcons = false, iconAlign = 'left' }) => {
+  return `<nav class="rvo-topnav">
     <ul class="utrecht-topnav__list">
       <li class="utrecht-topnav__item">
-        <a class="utrecht-topnav__link" href="https://example.com/">
-          ${useIcons ? `<span class="rvo-icon rvo-icon--home rvo-icon--medium rvo-icon--wit"></span> ` : ''}
-        Wonen en leven</a>
-      </li>
-      <li class="utrecht-topnav__item">
-        <a class="utrecht-topnav__link" href="https://example.com/">
-          ${useIcons ? `<span class="rvo-icon rvo-icon--kalender rvo-icon--medium rvo-icon--wit"></span> ` : ''}
-          Werk en inkomen
+        <a class="utrecht-topnav__link" href="#">
+          ${parseMenuItem('Wonen en leven', useIcons && 'home', 'medium', iconAlign)}
         </a>
       </li>
       <li class="utrecht-topnav__item">
-        <a class="utrecht-topnav__link" href="https://example.com/">
-          ${useIcons ? `<span class="rvo-icon rvo-icon--nieuws rvo-icon--medium rvo-icon--wit"></span> ` : ''}
-          Ondernemen
+        <a class="utrecht-topnav__link" href="#">
+         ${parseMenuItem('Werk en inkomen', useIcons && 'kalender', 'medium', iconAlign)}
         </a>
       </li>
       <li class="utrecht-topnav__item">
-        <a class="utrecht-topnav__link" href="https://example.com/">
-          ${useIcons ? `<span class="rvo-icon rvo-icon--versleutelen rvo-icon--medium rvo-icon--wit"></span> ` : ''}
-          Bestuur en organisatie
+        <a class="utrecht-topnav__link" href="#">
+          ${parseMenuItem('Ondernemen', useIcons && 'kalender', 'medium', iconAlign)}
+        </a>
+      </li>
+      <li class="utrecht-topnav__item">
+        <a class="utrecht-topnav__link" href="#">
+          ${parseMenuItem('Bestuur en organisatie', useIcons && 'versleutelen', 'medium', iconAlign)}
         </a>
       </li>
     </ul>
   </nav>
 `;
+};
