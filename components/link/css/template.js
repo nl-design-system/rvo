1eb15252a96bc2ccd6e7b4887e4ec0db19ab41df
Tom Jonkman <tom.jonkman@rvo.nl>
2022-06-14 11:38:30 +0200

    Add link component

---
+++ components/link/css/template.js
@@ -0,0 +1,45 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2021 Community for NL Design System
+ */
+import '@utrecht/component-library-css';
+import clsx from 'clsx';
+
+export const argTypes = {
+  linkContent: {
+    control: 'text',
+  },
+  linkUrl: {
+    control: 'text',
+  },
+  showIcon: {
+    options: ['no', 'before', 'after'],
+    control: { type: 'radio' },
+  },
+};
+
+export const defaultArgs = {
+  linkContent: 'Dit is een link',
+  linkUrl: '#',
+  showIcon: 'no',
+};
+
+export const Link = ({
+  linkContent = defaultArgs.linkContent,
+  linkUrl = defaultArgs.linkUrl,
+  showIcon = defaultArgs.showIcon,
+  icon = 'pijl-naar-rechtsboven',
+}) => {
+  const iconMarkup = `<span class="${clsx(
+    'rvo-link__icon',
+    { 'rvo-link__icon--before': showIcon === 'before', 'rvo-link__icon--after': showIcon === 'after' },
+    'rvo-icon',
+    'rvo-icon--medium',
+    'rvo-icon--hemelblauw',
+    `rvo-icon--${icon}`,
+  )}"></span>`;
+
+  return `<a href="${linkUrl}" class="rvo-link">${showIcon === 'before' ? iconMarkup : ''}${linkContent}${
+    showIcon === 'after' ? iconMarkup : ''
+  }</a>`;
+};
