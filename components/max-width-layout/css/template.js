aaa943020790c11fb846a015416ca754bc12ccb8
Tom Jonkman <tom.jonkman@rvo.nl>
2022-06-16 11:47:20 +0200

    Remove unneccessary logo imports

--- components/max-width-layout/css/template.js
+++ components/max-width-layout/css/template.js
@@ -2,7 +2,6 @@
  * @license EUPL-1.2
  * Copyright (c) 2021 Community for NL Design System
  */
-import '@nl-rvo/assets/images/logo.svg';
 import * as designTokens from '@nl-rvo/design-tokens/dist';
 
 export const argTypes = {
