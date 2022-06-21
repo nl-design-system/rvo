b43b2fbdaf52d47330e08d771a08934e02595bb2
Tom Jonkman <tom.jonkman@rvo.nl>
2022-06-21 14:14:53 +0200

    Add error and warning helper text to form field

--- components/form-field/css/template.js
+++ components/form-field/css/template.js
@@ -11,7 +11,7 @@ export const argTypes = {
     control: 'text',
   },
   helperText: { control: 'text' },
-  useExpandableHelperText: { control: 'boolean' },
+  expandableHelperText: { control: 'boolean' },
   expandableHelperTextTitle: { control: 'text' },
   warningText: { control: 'text' },
   errorText: { control: 'text' },
@@ -20,9 +20,9 @@ export const argTypes = {
 export const defaultArgs = {
   fieldId: 'fieldId',
   labelText: 'Field label',
-  helperText: 'Helper text which can be used for instructions.',
-  useExpandableHelperText: false,
-  expandableHelperTextTitle: '',
+  helperText: '',
+  expandableHelperText: false,
+  expandableHelperTextTitle: 'Expandable helper text title',
   warningText: '',
   errorText: '',
 };
@@ -31,21 +31,36 @@ export const Field = ({
   fieldId = defaultArgs.fieldId,
   labelText = defaultArgs.labelText,
   helperText = defaultArgs.helperText,
-  useExpandableHelperText = defaultArgs.expandableHelperText,
+  expandableHelperText = defaultArgs.expandableHelperText,
   expandableHelperTextTitle = defaultArgs.expandableHelperTextTitle,
-  //   warningText = defaultArgs.warningText,
-  //   errorText = defaultArgs.errorText,
+  warningText = defaultArgs.warningText,
+  errorText = defaultArgs.errorText,
 }) => {
+  // Parse helper text markup
   let helperTextMarkup = '';
+  if (helperText && helperText.length) {
+    if (!expandableHelperText) {
+      helperTextMarkup = `<div class="rvo-form-field__helper-text">${helperText}</div>`;
+    } else {
+      helperTextMarkup = `<div class="rvo-form-field__helper-text"><details class="rvo-expandable-text"><summary class="rvo-expandable-text__summary">${expandableHelperTextTitle}</summary><span class="rvo-expandable-text__details">${helperText}</span></details></div>`;
+    }
+  }
 
-  if (helperText && !useExpandableHelperText) {
-    helperTextMarkup = `<div class="rvo-form-field__helper-text">${helperText}</div>`;
-  } else if (helperText && useExpandableHelperText) {
-    helperTextMarkup = `<div class="rvo-form-field__helper-text"><details class="rvo-expandable-text"><summary class="rvo-expandable-text__summary">${expandableHelperTextTitle}</summary><span class="rvo-expandable-text__details">${helperText}</span></details></div>`;
+  // Parse error text markup
+  let errorTextMarkup = '';
+  if (errorText) {
+    errorTextMarkup = `<div class="rvo-form-field__helper-text rvo-form-field__helper-text--error"><span class="rvo-icon rvo-icon--foutmelding rvo-status-icon--foutmelding rvo-icon--medium"></span>${errorText}</div>`;
+  }
+
+  // Parse warning text markup
+  let warningTextMarkup = '';
+  if (warningText) {
+    warningTextMarkup = `<div class="rvo-form-field__helper-text rvo-form-field__helper-text--warning"><span class="rvo-icon rvo-icon--waarschuwing rvo-status-icon--waarschuwing rvo-icon--medium"></span>${warningText}</div>`;
   }
 
   return `<div class="rvo-form-field">
-  <label class="rvo-form-field__label" for="${fieldId}"><span class="rvo-form-field__label-text">${labelText}</span>${helperTextMarkup}</label>
-  <input id="${fieldId}" class="utrecht-textbox utrecht-textbox--html-input" value=""></input>
-</fieldset>`;
+  <label class="rvo-form-field__label" for="${fieldId}"><span class="rvo-form-field__label-text">${labelText}</span>${helperTextMarkup}${errorTextMarkup}${warningTextMarkup}</label>
+  <input id="${fieldId}" class="utrecht-textbox utrecht-textbox--html-input${
+    errorText ? ' utrecht-textbox--invalid' : ''
+  }" value=""></input></div>`;
 };
