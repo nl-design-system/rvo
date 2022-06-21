fb77997988ba17b71f2ca12d2c9cbc4bd703d716
Tom Jonkman <tom.jonkman@rvo.nl>
2022-06-21 14:15:15 +0200

    Add examples with fields to Fieldset

--- components/form-fieldset/css/template.js
+++ components/form-fieldset/css/template.js
@@ -2,12 +2,10 @@
  * @license EUPL-1.2
  * Copyright (c) 2021 Community for NL Design System
  */
+import { Field } from '../../form-field/css/template';
 
 export const argTypes = {
   legend: {
-    control: 'text',
-  },
-  content: {
     control: 'text',
   },
 };
@@ -17,12 +15,34 @@ export const defaultArgs = {
   content: 'Fieldset content',
 };
 
-export const Fieldset = ({ content = defaultArgs.content, legend = defaultArgs.legend }) =>
-  `<fieldset class="utrecht-form-fieldset">
+export const Fieldset = ({ legend = defaultArgs.legend, withFields = false }) => {
+  // Parse fieldset content
+  let fieldsetContent = 'content';
+  if (withFields) {
+    fieldsetContent = Field({ fieldId: 'fieldA', labelText: 'Field', helperText: '' });
+    fieldsetContent += Field({
+      fieldId: 'fieldB',
+      labelText: 'Field met helper tekst',
+      helperText: 'Deze helpertekst kan gebruikt worden voor instructies',
+    });
+    fieldsetContent += Field({
+      fieldId: 'fieldC',
+      labelText: 'Field met waarschuwing',
+      warningText: 'Dit is een waarschuwing',
+    });
+    fieldsetContent += Field({
+      fieldId: 'fieldD',
+      labelText: 'Field met foutmelding',
+      errorText: 'Dit is een foutmelding',
+    });
+  }
+
+  return `<fieldset class="utrecht-form-fieldset">
   ${
     legend
       ? `<legend class="utrecht-form-fieldset__legend utrecht-form-fieldset__legend--distanced">${legend}</legend>`
       : ''
   }
-  ${content}
+  ${fieldsetContent}
 </fieldset>`;
+};
