d3a0f68d41b3a120e4d54ec60739b919a31c4e32
Robert Roose <robert.roose@rvo.nl>
2022-06-23 16:03:06 +0200

    Add blockquote

--- documentation/demopages/algemeen/Content.tsx
+++ documentation/demopages/algemeen/Content.tsx
@@ -44,7 +44,7 @@ const Content = () => {
 
       <main className="rvo-max-width-layout--md">
         <div className="rvo-content">
-          <h1>Heading 1</h1>
+          <h1 className="utrecht-heading-1">Heading 1</h1>
           <p className="rvo-paragraph rvo-paragraph--sm">
             <strong>Paragraph small.</strong> De overheid zet zich in voor een uitstekend ondernemersklimaat. De
             ministeries stippelen daar beleid voor uit. En de taak om dit uit te voeren ligt bij RVO: de Rijksdienst
@@ -54,6 +54,7 @@ const Content = () => {
             </a>
             Tussen bedrijven, kennis- en financiële instellingen, ambassades en lokale overheden.
           </p>
+          <h2 className="utrecht-heading-2">Heading 2</h2>
           <p className="rvo-paragraph rvo-paragraph--md">
             <strong>Paragraph medium.</strong> De overheid zet zich in voor een uitstekend ondernemersklimaat. De
             ministeries stippelen daar beleid voor uit. En de taak om dit uit te voeren ligt bij RVO: de Rijksdienst
@@ -63,6 +64,7 @@ const Content = () => {
             </a>
             Tussen bedrijven, kennis- en financiële instellingen, ambassades en lokale overheden.
           </p>
+          <h3 className="utrecht-heading-3">Heading 3</h3>
           <p className="rvo-paragraph rvo-paragraph--lg">
             <strong>Paragraph large.</strong> De overheid zet zich in voor een uitstekend ondernemersklimaat. De
             ministeries stippelen daar beleid voor uit. En de taak om dit uit te voeren ligt bij RVO: de Rijksdienst
@@ -82,6 +84,11 @@ const Content = () => {
             <li className="rvo-list-item">Second item</li>
             <li className="rvo-list-item">Third item</li>
           </ol>
+          <blockquote className="rvo-blockquote">
+            <strong>Dit is een blockquote.</strong> De overheid zet zich in voor een uitstekend ondernemersklimaat. De
+            ministeries stippelen daar beleid voor uit. En de taak om dit uit te voeren ligt bij RVO: de Rijksdienst
+            voor Ondernemend Nederland.
+          </blockquote>
         </div>
       </main>
     </div>
