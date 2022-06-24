4493e78ed3e16772423e853e4deb1b3e4b1ba560
Robert Roose <rroose@gmail.com>
2022-06-24 10:03:16 +0200

    Feature/demopage content (#149)
    * Add content demopage
    
    * Fix content.tsx
    
    * Change content demo page
    
    * Add paragraph variants to content demo page
    
    * Add blockquote
    
    * Change import order
    
    * Removing redundant icon CSS
    
    Co-authored-by: Robert Roose <robert.roose@rvo.nl>
    Co-authored-by: Tom Jonkman <tom.jonkman@rvo.nl>

---
+++ documentation/demopages/algemeen/Content.tsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import 'normalize.css';
+import '../common/style.scss';
+import Header from '../common/components/Header';
+
+const Content = () => {
+  return (
+    <div className="rvo-demo-page">
+      <Header />
+      <div className="rvo-navigation rvo-navigation--hemelblauw">
+        <div className="rvo-max-width-layout--md">
+          <nav className="rvo-main-menu">
+            <ul className="rvo-menu rvo-menu--main-menu">
+              <li className="rvo-menu-item">
+                <a href="#" className="rvo-menu-link rvo-menu-link--md">
+                  Menu item
+                </a>
+              </li>
+              <li className="rvo-menu-item">
+                <a href="#" className="rvo-menu-link rvo-menu-link--md">
+                  <span className="rvo-icon rvo-icon--md rvo-icon--before rvo-icon--wit rvo-icon--plus"></span>
+                  Menu item with icon
+                </a>
+              </li>
+              <li className="rvo-menu-item">
+                <a href="#" className="rvo-menu-link rvo-menu-link--md">
+                  Last menu item
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <nav className="rvo-toggle-menu">
+            <ul className="rvo-menu rvo-menu--toggle-menu">
+              <li className="rvo-menu-item">
+                <a href="#" className="rvo-menu-link rvo-menu-link--md rvo-menu-link--toggle">
+                  <span className="rvo-icon rvo-icon--md rvo-icon--before rvo-icon--wit rvo-icon--menu"></span>Menu
+                  toggle
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+
+      <main className="rvo-max-width-layout--md">
+        <div className="rvo-content">
+          <h1 className="utrecht-heading-1">Heading 1</h1>
+          <p className="rvo-paragraph rvo-paragraph--sm">
+            <strong>Paragraph small.</strong> De overheid zet zich in voor een uitstekend ondernemersklimaat. De
+            ministeries stippelen daar beleid voor uit. En de taak om dit uit te voeren ligt bij RVO: de Rijksdienst
+            voor Ondernemend Nederland. Onze mensen maken wereldwijd verbinding.{' '}
+            <a href="#" className="rvo-link">
+              Dit is een link.
+            </a>
+            Tussen bedrijven, kennis- en financiële instellingen, ambassades en lokale overheden.
+          </p>
+          <h2 className="utrecht-heading-2">Heading 2</h2>
+          <p className="rvo-paragraph rvo-paragraph--md">
+            <strong>Paragraph medium.</strong> De overheid zet zich in voor een uitstekend ondernemersklimaat. De
+            ministeries stippelen daar beleid voor uit. En de taak om dit uit te voeren ligt bij RVO: de Rijksdienst
+            voor Ondernemend Nederland. Onze mensen maken wereldwijd verbinding.{' '}
+            <a href="#" className="rvo-link">
+              Dit is een link.
+            </a>
+            Tussen bedrijven, kennis- en financiële instellingen, ambassades en lokale overheden.
+          </p>
+          <h3 className="utrecht-heading-3">Heading 3</h3>
+          <p className="rvo-paragraph rvo-paragraph--lg">
+            <strong>Paragraph large.</strong> De overheid zet zich in voor een uitstekend ondernemersklimaat. De
+            ministeries stippelen daar beleid voor uit. En de taak om dit uit te voeren ligt bij RVO: de Rijksdienst
+            voor Ondernemend Nederland. Onze mensen maken wereldwijd verbinding.{' '}
+            <a href="#" className="rvo-link">
+              Dit is een link.
+            </a>
+            Tussen bedrijven, kennis- en financiële instellingen, ambassades en lokale overheden.
+          </p>
+          <ul className="rvo-list rvo-list--unordered">
+            <li className="rvo-list-item">Unordered list</li>
+            <li className="rvo-list-item">Second item</li>
+            <li className="rvo-list-item">Third item</li>
+          </ul>
+          <ol className="rvo-list rvo-list--ordered">
+            <li className="rvo-list-item">Ordered list</li>
+            <li className="rvo-list-item">Second item</li>
+            <li className="rvo-list-item">Third item</li>
+          </ol>
+          <blockquote className="rvo-blockquote">
+            <strong>Dit is een blockquote.</strong> De overheid zet zich in voor een uitstekend ondernemersklimaat. De
+            ministeries stippelen daar beleid voor uit. En de taak om dit uit te voeren ligt bij RVO: de Rijksdienst
+            voor Ondernemend Nederland.
+          </blockquote>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Content;
