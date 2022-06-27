cd30c5e17ab60c4b796b245151cefa2419e11f50
Tom Jonkman <tom.jonkman@rvo.nl>
2022-06-27 14:20:53 +0200

    Remove Form Feedback from checkbox and radio button

--- components/form-radio-button/css/template.group.js
+++ components/form-radio-button/css/template.group.js
@@ -4,7 +4,6 @@
  */
 import clsx from 'clsx';
 import './index.scss';
-import { FormFeedback } from '../../form-feedback/css/template';
 import { RadioButton } from './template';
 
 export const argTypes = {
@@ -12,43 +11,21 @@ export const argTypes = {
     options: ['vertical', 'horizontal'],
     control: { type: 'radio' },
   },
-  warningText: { control: 'text' },
-  errorText: { control: 'text' },
+  invalid: { control: 'boolean' },
 };
 
 export const defaultArgs = {
   layout: 'vertical',
-  legend: '',
-  warningText: '',
-  errorText: '',
+  invalid: false,
 };
 
-export const RadioButtonGroup = ({
-  layout = defaultArgs.layout,
-  warningText = defaultArgs.warningText,
-  errorText = defaultArgs.errorText,
-}) => {
-  // Parse error text markup
-  let errorTextMarkup = '';
-  if (errorText) {
-    errorTextMarkup = FormFeedback({ text: errorText, type: 'error' });
-  }
-
-  // Parse warning text markup
-  let warningTextMarkup = '';
-  if (warningText) {
-    warningTextMarkup = FormFeedback({ text: warningText, type: 'warning' });
-  }
-
+export const RadioButtonGroup = ({ layout = defaultArgs.layout, invalid = defaultArgs.invalid }) => {
   let markup = `<div class="${clsx(
     'rvo-custom-radio-button__group',
     layout === 'horizontal' && 'rvo-custom-radio-button__group--horizontal',
     layout === 'vertical' && 'rvo-custom-radio-button__group--vertical',
-    warningText.length && 'rvo-custom-radio-button__group--warning',
-    errorText.length && 'rvo-custom-radio-button__group--error',
+    invalid && 'rvo-custom-radio-button__group--error',
   )}">`;
-  markup += errorTextMarkup;
-  markup += warningTextMarkup;
   markup += RadioButton({ id: 'optionA', name: 'group', labelText: 'Option A' });
   markup += RadioButton({ id: 'optionB', name: 'group', labelText: 'Option B' });
   markup += RadioButton({ id: 'optionC', name: 'group', labelText: 'Option C' });
