22af359fb09aa91b3e7cc0a27bcb249bbb09ebc8
Robert Roose <robert.roose@rvo.nl>
2022-06-30 11:38:28 +0200

    Add alerts to demo page

--- documentation/demopages/algemeen/Alerts.tsx
+++ documentation/demopages/algemeen/Alerts.tsx
@@ -36,11 +36,120 @@ const Alerts = () => {
       <main className=" rvo-max-width-layout rvo-max-width-layout--sm">
         <div className="rvo-alerts">
           <div className="rvo-alert rvo-alert--info">
-            <h2 className="utrecht-heading-2">Info</h2>
-            <p>
-              Dit is een uitleg lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac leo sem. Vivamus diam
-              risus, vestibulum vitae bibendum finibus.
-            </p>
+            <div className="rvo-icon rvo-icon--info rvo-icon--lg rvo-icon--hemelblauw"></div>
+            <div className="rvo-alert-text">
+              <p>This is an example of an info alert.</p>
+            </div>
+          </div>
+          <div className="rvo-alert rvo-alert--info">
+            <div className="rvo-icon rvo-icon--info rvo-icon--lg rvo-icon--hemelblauw"></div>
+            <div className="rvo-alert-text">
+              <h2 className="rvo-heading utrecht-heading-2">Info with heading</h2>
+              <p>This is an example of an info alert with a heading.</p>
+            </div>
+          </div>
+          <div className="rvo-alert rvo-alert--info">
+            <div className="rvo-icon rvo-icon--info rvo-icon--lg rvo-icon--hemelblauw"></div>
+            <div className="rvo-alert-text">
+              <p>This is an example of an info alert with a button.</p>
+              <button className="utrecht-button utrecht-button--primary-action rvo-button--sm">Button</button>
+            </div>
+          </div>
+          <div className="rvo-alert rvo-alert--info">
+            <div className="rvo-icon rvo-icon--info rvo-icon--lg rvo-icon--hemelblauw"></div>
+            <div className="rvo-alert-text">
+              <p>This is an example of an info alert with an optional close button.</p>
+            </div>
+            <button className="utrecht-button rvo-button--tertiary-action rvo-button--sm">
+              <div className="rvo-button__icon--tertiary-action rvo-icon rvo-icon--kruis rvo-icon--md"></div>
+            </button>
+          </div>
+          <div className="rvo-alert rvo-alert--warning">
+            <div className="rvo-icon rvo-icon--waarschuwing rvo-status-icon--waarschuwing rvo-icon--lg"></div>
+            <div className="rvo-alert-text">
+              <p>This is an example of a warning alert.</p>
+            </div>
+          </div>
+          <div className="rvo-alert rvo-alert--warning">
+            <div className="rvo-icon rvo-icon--waarschuwing rvo-status-icon--waarschuwing rvo-icon--lg"></div>
+            <div className="rvo-alert-text">
+              <h2 className="rvo-heading utrecht-heading-2">Warning with heading</h2>
+              <p>This is an example of a warning alert with a heading.</p>
+            </div>
+          </div>
+          <div className="rvo-alert rvo-alert--warning">
+            <div className="rvo-icon rvo-icon--waarschuwing rvo-status-icon--waarschuwing rvo-icon--lg"></div>
+            <div className="rvo-alert-text">
+              <p>This is an example of a warning alert with a button.</p>
+              <button className="utrecht-button utrecht-button--primary-action rvo-button--sm">Button</button>
+            </div>
+          </div>
+          <div className="rvo-alert rvo-alert--warning">
+            <div className="rvo-icon rvo-icon--waarschuwing rvo-status-icon--waarschuwing rvo-icon--lg"></div>
+            <div className="rvo-alert-text">
+              <p>This is an example of a warning alert with an optional close button.</p>
+            </div>
+            <button className="utrecht-button rvo-button--tertiary-action rvo-button--sm">
+              <div className="rvo-button__icon--tertiary-action rvo-icon rvo-icon--kruis rvo-icon--md"></div>
+            </button>
+          </div>
+          <div className="rvo-alert rvo-alert--error">
+            <div className="rvo-icon rvo-icon--foutmelding rvo-status-icon--foutmelding rvo-icon--lg"></div>
+            <div className="rvo-alert-text">
+              <p>This is an example of a error alert.</p>
+            </div>
+          </div>
+          <div className="rvo-alert rvo-alert--error">
+            <div className="rvo-icon rvo-icon--foutmelding rvo-status-icon--foutmelding rvo-icon--lg"></div>
+            <div className="rvo-alert-text">
+              <h2 className="rvo-heading utrecht-heading-2">error with heading</h2>
+              <p>This is an example of a error alert with a heading.</p>
+            </div>
+          </div>
+          <div className="rvo-alert rvo-alert--error">
+            <div className="rvo-icon rvo-icon--foutmelding rvo-status-icon--foutmelding rvo-icon--lg"></div>
+            <div className="rvo-alert-text">
+              <p>This is an example of a error alert with a button.</p>
+              <button className="utrecht-button utrecht-button--primary-action rvo-button--sm">Button</button>
+            </div>
+          </div>
+          <div className="rvo-alert rvo-alert--error">
+            <div className="rvo-icon rvo-icon--foutmelding rvo-status-icon--foutmelding rvo-icon--lg"></div>
+            <div className="rvo-alert-text">
+              <p>This is an example of a error alert with an optional close button.</p>
+            </div>
+            <button className="utrecht-button rvo-button--tertiary-action rvo-button--sm">
+              <div className="rvo-button__icon--tertiary-action rvo-icon rvo-icon--kruis rvo-icon--md"></div>
+            </button>
+          </div>
+          <div className="rvo-alert rvo-alert--succes">
+            <div className="rvo-icon rvo-icon--bevestiging rvo-status-icon--bevestiging rvo-icon--lg"></div>
+            <div className="rvo-alert-text">
+              <p>This is an example of a succes alert.</p>
+            </div>
+          </div>
+          <div className="rvo-alert rvo-alert--succes">
+            <div className="rvo-icon rvo-icon--bevestiging rvo-status-icon--bevestiging rvo-icon--lg"></div>
+            <div className="rvo-alert-text">
+              <h2 className="rvo-heading utrecht-heading-2">succes with heading</h2>
+              <p>This is an example of a succes alert with a heading.</p>
+            </div>
+          </div>
+          <div className="rvo-alert rvo-alert--succes">
+            <div className="rvo-icon rvo-icon--bevestiging rvo-status-icon--bevestiging rvo-icon--lg"></div>
+            <div className="rvo-alert-text">
+              <p>This is an example of a succes alert with a button.</p>
+              <button className="utrecht-button utrecht-button--primary-action rvo-button--sm">Button</button>
+            </div>
+          </div>
+          <div className="rvo-alert rvo-alert--succes">
+            <div className="rvo-icon rvo-icon--bevestiging rvo-status-icon--bevestiging rvo-icon--lg"></div>
+            <div className="rvo-alert-text">
+              <p>This is an example of a succes alert with an optional close button.</p>
+            </div>
+            <button className="utrecht-button rvo-button--tertiary-action rvo-button--sm">
+              <div className="rvo-button__icon--tertiary-action rvo-icon rvo-icon--kruis rvo-icon--md"></div>
+            </button>
           </div>
         </div>
       </main>
