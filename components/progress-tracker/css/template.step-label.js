ba1d7a288a2121c92b41dbc76829b068ede5f6cb
Tom Jonkman <tom.jonkman@rvo.nl>
2022-07-04 17:23:49 +0200

    Add initial step label component

---
+++ components/progress-tracker/css/template.step-label.js
@@ -0,0 +1,59 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2021 Community for NL Design System
+ */
+import clsx from 'clsx';
+import './index.scss';
+
+export const argTypes = {
+  type: {
+    options: ['default', 'start title', 'end title'],
+    mapping: { default: 'default', 'start title': 'start', 'end title': 'end' },
+    control: { type: 'radio' },
+  },
+  disabled: {
+    control: { type: 'boolean' },
+  },
+  size: {
+    options: ['small', 'medium'],
+    mapping: { small: 'sm', medium: 'md' },
+    control: { type: 'radio' },
+  },
+  textContent: {
+    control: 'text',
+  },
+  link: {
+    control: 'text',
+  },
+};
+
+export const defaultArgs = {
+  type: 'default',
+  disabled: false,
+  size: 'medium',
+  textContent: 'Step label',
+  link: '#',
+};
+
+export const StepLabel = ({
+  type = defaultArgs.type,
+  disabled = defaultArgs.disabled,
+  size = defaultArgs.size,
+  textContent = defaultArgs.textContent,
+  link = defaultArgs.link,
+}) => {
+  if (!disabled && type === 'default') {
+    return `<a href="${link}" class="${clsx(
+      'rvo-progress-tracker__step-label',
+      `rvo-progress-tracker__step-label--${type}`,
+      `rvo-progress-tracker__step-label--${size}`,
+    )}">${textContent}</a>`;
+  } else {
+    return `<span class="${clsx(
+      'rvo-progress-tracker__step-label',
+      `rvo-progress-tracker__step-label--${type}`,
+      `rvo-progress-tracker__step-label--${size}`,
+      { 'rvo-progress-tracker__step-label--disabled': disabled },
+    )}">${textContent}</span>`;
+  }
+};
