ccec91bc4c4546256729a615bc89fe09163ced66
Robert Roose <robert.roose@rvo.nl>
2022-07-04 17:13:10 +0200

    Add search in nav demo page

---
+++ documentation/demopages/algemeen/SearchInNav.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import 'normalize.css';
+import '../common/style.scss';
+import Header from '../common/components/Header';
+
+const SearchInNav = () => {
+  return (
+    <div className="rvo-demo-page">
+      <Header />
+      <nav className="rvo-topnav rvo-topnav--md">
+        <ul className="utrecht-topnav__list">
+          <li className="utrecht-topnav__item">
+            <a className="utrecht-topnav__link" href="#">
+              <div className="rvo-icon rvo-icon--home rvo-icon--md rvo-icon--wit"></div> Home
+            </a>
+          </li>
+          <li className="utrecht-topnav__item">
+            <a className="utrecht-topnav__link" href="#">
+              <div className="rvo-icon rvo-icon--publicatie rvo-icon--md rvo-icon--wit"></div> Mijn aanvragen
+            </a>
+          </li>
+          <li className="utrecht-topnav__item">
+            <a className="utrecht-topnav__link" href="#">
+              <div className="rvo-icon rvo-icon--plus rvo-icon--md rvo-icon--wit"></div> Nieuwe aanvraag
+            </a>
+          </li>
+          <div className="rvo-topnav__spacer"></div>
+          <li className="utrecht-topnav__item">
+            <a className="utrecht-topnav__link rvo-topnav__link--search-active" href="#">
+              <div className="rvo-icon rvo-icon--zoek rvo-icon--md rvo-icon--hemelblauw"></div> Zoeken
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div className="search-form">
+        <fieldset>
+          <div className="rvo-form-field">
+            <label className="rvo-form-field__label" htmlFor="search">
+              <span className="rvo-form-field__label-text">Field label</span>
+              <div className="rvo-form-field__helper-text">Helper text</div>
+            </label>
+            <input id="search" className="utrecht-textbox utrecht-textbox--html-input" value=""></input>
+          </div>
+        </fieldset>
+      </div>
+      <main className="rvo-max-width-layout rvo-max-width-layout--sm">
+        <div className="rvo-content">
+          <h1 className="utrecht-heading-1">Search in Navigation</h1>
+
+          <p className="rvo-paragraph rvo-paragraph--md">
+            This demopage demonstrates how a search element can be included in the navbar.
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default SearchInNav;
