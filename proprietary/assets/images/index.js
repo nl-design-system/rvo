003a78dacec8352e32a2cdeeb19cf1c17e6ef0c0
Tom Jonkman <tom.jonkman@rvo.nl>
2022-07-05 14:29:14 +0200

    Add progress tracker SVG assets

--- proprietary/assets/images/index.js
+++ proprietary/assets/images/index.js
@@ -1,6 +1,13 @@
 export default {
   LOGO: 'logo.svg',
   PROGRESS_TRACKER: {
-    PROGRESS_LINE_INDENT: 'progress-tracker/progress-line-indent.svg',
+    PROGRESS_TRACKER_COMPLETED_MD: 'progress-tracker/progress-tracker-completed-md.svg',
+    PROGRESS_TRACKER_COMPLETED_SM: 'progress-tracker/progress-tracker-completed-sm.svg',
+    PROGRESS_TRACKER_DOING_MD: 'progress-tracker/progress-tracker-doing-md.svg',
+    PROGRESS_TRACKER_DOING_SM: 'progress-tracker/progress-tracker-doing-sm.svg',
+    PROGRESS_TRACKER_INCOMPLETE_MD: 'progress-tracker/progress-tracker-incomplete-md.svg',
+    PROGRESS_TRACKER_INCOMPLETE_SM: 'progress-tracker/progress-tracker-incomplete-sm.svg',
+    PROGRESS_TRACKER_LINE_INDENT: 'progress-tracker/progress-tracker-line-indent.svg',
+    PROGRESS_TRACKER_START_END: 'progress-tracker/progress-tracker-start-end.svg',
   },
 };
