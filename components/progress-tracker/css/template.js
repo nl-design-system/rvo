26dcd02e57d67e23b5ede3f85d08b137ecdce150
Tom Jonkman <tom.jonkman@rvo.nl>
2022-07-05 11:31:46 +0200

    Add all progress tracker items

--- components/progress-tracker/css/template.js
+++ components/progress-tracker/css/template.js
@@ -7,6 +7,16 @@ import { Step } from './template.step';
 
 const defaultSteps = [
   { state: 'start', labelType: 'start', disabled: false, label: 'Progress Tracker', link: '#', size: 'md' },
+  { state: 'completed', labelType: 'default', disabled: false, label: 'Step completed', link: '#', size: 'md' },
+  { state: 'doing', labelType: 'default', disabled: false, label: 'Step doing', link: '#', size: 'md' },
+  { state: 'incomplete', labelType: 'default', disabled: false, label: 'Step incomplete', link: '#', size: 'md' },
+  { state: 'incomplete', labelType: 'default', disabled: false, label: 'First sub-step', link: '#', size: 'sm' },
+  { state: 'doing', labelType: 'default', disabled: false, label: 'Sub-step doing', link: '#', size: 'sm' },
+  { state: 'completed', labelType: 'default', disabled: false, label: 'Sub-step completed', link: '#', size: 'sm' },
+  { state: 'incomplete', labelType: 'default', disabled: true, label: 'Sub-step disabled', link: '#', size: 'sm' },
+  { state: 'incomplete', labelType: 'default', disabled: false, label: 'Last sub-step', link: '#', size: 'sm' },
+  { state: 'incomplete', labelType: 'default', disabled: true, label: 'Step disabled', link: '#', size: 'md' },
+  { state: 'end', labelType: 'end', disabled: false, label: 'Process completed', link: '#', size: 'md' },
 ];
 
 export const argTypes = {
