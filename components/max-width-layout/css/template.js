0360c9688e554ba230b360db2e7eb16f85770332
Tom Jonkman <tom.jonkman@rvo.nl>
2022-07-12 12:18:14 +0200

    Add content option to MaxWidthLayout

--- components/max-width-layout/css/template.js
+++ components/max-width-layout/css/template.js
@@ -14,24 +14,33 @@ export const argTypes = {
     },
     control: { type: 'radio' },
   },
+  content: {
+    control: 'text',
+  },
 };
 
 export const defaultArgs = {
   size: 'medium',
+  content: 'Deze gecentreerde layout container heeft een maximale breedte van {maxWidth}.',
 };
 
-export const MaxWidthLayout = ({ size = defaultArgs.size }) => {
-  let maxWidth;
-  switch (size) {
-    case 'sm':
-      maxWidth = designTokens.rvoLayoutMaxWidthSm;
-      break;
-    case 'md':
-      maxWidth = designTokens.rvoLayoutMaxWidthMd;
-      break;
-    case 'lg':
-      maxWidth = designTokens.rvoLayoutMaxWidthLg;
-      break;
+export const MaxWidthLayout = ({ size = defaultArgs.size, content = defaultArgs.content }) => {
+  let parsedContent = content;
+  if (parsedContent.indexOf('{maxWidth}') > -1) {
+    let maxWidth;
+    switch (size) {
+      case 'sm':
+        maxWidth = designTokens.rvoLayoutMaxWidthSm;
+        break;
+      case 'md':
+        maxWidth = designTokens.rvoLayoutMaxWidthMd;
+        break;
+      case 'lg':
+        maxWidth = designTokens.rvoLayoutMaxWidthLg;
+        break;
+    }
+    parsedContent = parsedContent.replace(/{maxWidth}/g, maxWidth);
   }
-  return `<div class="rvo-max-width-layout rvo-max-width-layout--${size}">Deze gecentreerde layout container heeft een maximale breedte van ${maxWidth}.</div>`;
+
+  return `<div class="rvo-max-width-layout rvo-max-width-layout--${size}">${parsedContent}</div>`;
 };
