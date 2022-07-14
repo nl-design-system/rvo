962cb9ec41c6f96e380623a59cdc182b31d558a6
Tom Jonkman <tom.jonkman@rvo.nl>
2022-07-14 12:57:49 +0200

    Use React for icon component

--- components/icon/css/template.tsx
+++ components/icon/css/template.tsx
@@ -5,12 +5,16 @@
 import iconList from '@nl-rvo/assets/icons';
 import '@nl-rvo/assets/icons/index.css';
 import clsx from 'clsx';
+import React from 'react';
 
-String.prototype.toProperCase = function () {
-  return this.replace(/\w\S*/g, function (txt) {
-    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-  });
-};
+interface IIconProps {
+  icon: string;
+  size: string;
+  color?: string;
+}
+
+const toProperCase = (inputString) =>
+  inputString.replace(/\w\S*/g, (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase());
 
 // eslint-disable-next-line no-unused-vars
 const { STATUS, ...iconListWithoutStatus } = iconList;
@@ -18,10 +22,10 @@ const { STATUS, ...iconListWithoutStatus } = iconList;
 export const options = Object.keys(iconListWithoutStatus).flatMap((categoryOrIconName) => {
   if (typeof iconList[categoryOrIconName] === 'object') {
     return Object.keys(iconList[categoryOrIconName]).map((iconName) =>
-      `${categoryOrIconName} > ${iconName}`.toProperCase(),
+      toProperCase(`${categoryOrIconName} > ${iconName}`),
     );
   } else {
-    return iconList[categoryOrIconName].replace('.svg', '').toProperCase();
+    return toProperCase(iconList[categoryOrIconName].replace('.svg', ''));
   }
 });
 
@@ -49,25 +53,28 @@ export const argTypes = {
   },
 };
 
-export const defaultArgs = {
+export const defaultArgs: IIconProps = {
   icon: argTypes.icon.options[0],
   size: argTypes.size.options[1],
   color: argTypes.color.options[0],
 };
 
-export const Icon = ({ icon = defaultArgs.icon, size = defaultArgs.size, color = defaultArgs.color }) => {
+export const Icon: React.FC<IIconProps> = ({
+  icon = defaultArgs.icon,
+  size = defaultArgs.size,
+  color = defaultArgs.color,
+}) => {
   let iconName = icon;
   if (icon.indexOf(' > ') > -1) {
     iconName = icon.split(' > ')[1];
   }
 
-  return `<div class="${clsx(
-    'rvo-icon',
-    `rvo-icon--${iconName.toLowerCase().replace(/_/g, '-')}`,
-    `rvo-icon--${size}`,
-    {
-      'rvo-icon--wit': color === 'wit',
-      'rvo-icon--hemelblauw': color === 'hemelblauw',
-    },
-  )}"></div>`;
+  return (
+    <div
+      className={clsx('rvo-icon', `rvo-icon--${iconName.toLowerCase().replace(/_/g, '-')}`, `rvo-icon--${size}`, {
+        'rvo-icon--wit': !color || color === 'wit',
+        'rvo-icon--hemelblauw': color === 'hemelblauw',
+      })}
+    ></div>
+  );
 };
