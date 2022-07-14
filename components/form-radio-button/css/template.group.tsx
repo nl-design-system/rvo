13edf9ed08139f3c578fa8b0f558b320c781a506
Tom Jonkman <tom.jonkman@rvo.nl>
2022-07-14 10:29:37 +0200

    Use React for radio button component

---
+++ components/form-radio-button/css/template.group.tsx
@@ -0,0 +1,70 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2021 Community for NL Design System
+ */
+import clsx from 'clsx';
+import './index.scss';
+import React from 'react';
+import { RadioButton } from './template';
+
+interface IRadioButtonOption {
+  id: string;
+  labelText: string;
+  checked?: boolean;
+}
+
+export interface IRadioButtonGroupProps {
+  layout: string;
+  name: string;
+  invalid: boolean;
+  options: IRadioButtonOption[];
+}
+
+const defaultOptions: IRadioButtonOption[] = [
+  { id: 'optionA', labelText: 'Option A' },
+  { id: 'optionB', labelText: 'Option B', checked: true },
+  { id: 'optionC', labelText: 'Option C' },
+  { id: 'optionD', labelText: 'Option D' },
+];
+
+export const argTypes = {
+  layout: {
+    options: ['vertical', 'horizontal'],
+    control: { type: 'radio' },
+  },
+  name: { control: 'text' },
+  invalid: { control: 'boolean' },
+  options: {
+    type: {
+      name: 'array',
+      required: true,
+    },
+  },
+};
+
+export const defaultArgs: IRadioButtonGroupProps = {
+  layout: 'vertical',
+  name: 'group',
+  invalid: false,
+  options: defaultOptions,
+};
+
+export const RadioButtonGroup: React.FC<IRadioButtonGroupProps> = ({
+  layout = defaultArgs.layout,
+  name = defaultArgs.name,
+  invalid = defaultArgs.invalid,
+  options = defaultArgs.options,
+}) => (
+  <div
+    className={clsx(
+      'rvo-custom-radio-button__group',
+      layout === 'horizontal' && 'rvo-custom-radio-button__group--horizontal',
+      layout === 'vertical' && 'rvo-custom-radio-button__group--vertical',
+      invalid && 'rvo-custom-radio-button__group--error',
+    )}
+  >
+    {options.map((option) => (
+      <RadioButton key={option.id} id={option.id} name={name} labelText={option.labelText} checked={option.checked} />
+    ))}
+  </div>
+);
