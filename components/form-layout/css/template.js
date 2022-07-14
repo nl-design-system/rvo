294aedcc38a633f0ea835e7000cf7c8532a50adf
Tom Jonkman <tom.jonkman@rvo.nl>
2022-07-14 12:42:12 +0200

    Use react for form-layout component

--- components/form-layout/css/template.js
+++
@@ -1,9 +0,0 @@
-/**
- * @license EUPL-1.2
- * Copyright (c) 2021 Community for NL Design System
- */
-import * as designTokens from '@nl-rvo/design-tokens/dist';
-
-export const FormLayout = () => {
-  return `<div class="rvo-form-layout">Deze gecentreerde container heeft een maximale breedte van ${designTokens.rvoFormLayoutMaxWidth} en dient gebruikt te worden voor formulieren.</div>`;
-};
