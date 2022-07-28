52296f8bb459b580352520259c598aa9c7fdda40
Tom Jonkman <tom.jonkman@rvo.nl>
2022-07-28 22:47:20 +0200

    Remove @use in SCSS

--- components/icon/css/template.tsx
+++ components/icon/css/template.tsx
@@ -2,7 +2,7 @@
  * @license EUPL-1.2
  * Copyright (c) 2021 Community for NL Design System
  */
-import iconList from '@nl-rvo/assets/icons';
+import iconList from '@nl-rvo/assets/icons/index.js';
 import clsx from 'clsx';
 import React from 'react';
 
