03356182140535aa6af509ed956f0e45e408e467
Robert Roose <103183050+rroose-rvo@users.noreply.github.com>
2022-08-15 09:37:29 +0200

    Refactor search in nav demopage (#216)

--- documentation/demopages/algemeen/SearchInNav.tsx
+++ documentation/demopages/algemeen/SearchInNav.tsx
@@ -1,62 +1,64 @@
+import { Button } from '@nl-rvo/components/button/css/template';
+import { TextInputField } from '@nl-rvo/components/form-field/css/textinput-field.template';
+import { Fieldset } from '@nl-rvo/components/form-fieldset/css/template';
 import { Header } from '@nl-rvo/components/header/css/template';
+import { LayoutColumnRow } from '@nl-rvo/components/layout-column-row/css/template';
+import { MaxWidthLayout } from '@nl-rvo/components/max-width-layout/css/template';
+import { MenuBar } from '@nl-rvo/components/menubar/css/template';
 import '../common/style.scss';
 
 const SearchInNav = () => {
   return (
     <div className="rvo-demo-page">
       <Header />
-      <nav className="rvo-topnav rvo-topnav--md">
-        <ul className="utrecht-topnav__list">
-          <li className="utrecht-topnav__item">
-            <a className="utrecht-topnav__link" href="#">
-              <div className="rvo-icon rvo-icon-home rvo-icon--md rvo-icon--wit"></div> Home
-            </a>
-          </li>
-          <li className="utrecht-topnav__item">
-            <a className="utrecht-topnav__link" href="#">
-              <div className="rvo-icon rvo-icon-publicatie rvo-icon--md rvo-icon--wit"></div> Mijn aanvragen
-            </a>
-          </li>
-          <li className="utrecht-topnav__item">
-            <a className="utrecht-topnav__link" href="#">
-              <div className="rvo-icon rvo-icon-plus rvo-icon--md rvo-icon--wit"></div> Nieuwe aanvraag
-            </a>
-          </li>
-          <div className="rvo-topnav__spacer"></div>
-          <li className="utrecht-topnav__item">
-            <a className="utrecht-topnav__link rvo-topnav__link--search-active" href="#">
-              <div className="rvo-icon rvo-icon-zoek rvo-icon--md rvo-icon--hemelblauw"></div> Zoeken
-            </a>
-          </li>
-        </ul>
-      </nav>
-      <div className="search-form">
-        <fieldset>
-          <div className="rvo-form-layout">
-            <div className="rvo-inline-form">
-              <div className="rvo-form-field">
-                <label className="rvo-form-field__label" htmlFor="search">
-                  <span className="rvo-form-field__label-text">Zoekterm</span>
-                </label>
-                <input id="search" className="utrecht-textbox utrecht-textbox--html-input" defaultValue=""></input>
-              </div>
-              <button className="utrecht-button utrecht-button--primary-action rvo-button--md">
-                <div className="rvo-button__icon--primary-action rvo-icon rvo-icon-zoek rvo-icon--md"></div>
-                Zoeken
-              </button>
+      <MenuBar
+        items={[
+          { label: 'Home', icon: 'home', link: '#' },
+          { label: 'Mijn aanvragen', icon: 'publicatie', link: '#' },
+          { label: 'Nieuwe aanvraag', icon: 'plus', link: '#' },
+          { label: 'Zoeken', icon: 'zoek', link: '#', align: 'right' },
+        ]}
+        size="md"
+        useIcons={true}
+        iconPlacement="before"
+      />
+      <LayoutColumnRow size="md">
+        <div className="search-form">
+          <Fieldset legend="">
+            <div className="rvo-form-layout">
+              <MaxWidthLayout size="sm">
+                <div className="rvo-inline-form">
+                  <TextInputField labelText="Zoekterm"></TextInputField>
+                  <Button
+                    kind="primary"
+                    size="md"
+                    textContent="Zoeken"
+                    active={false}
+                    busy={false}
+                    focus={false}
+                    focusVisible={false}
+                    disabled={false}
+                    hover={false}
+                    showIcon="no"
+                  ></Button>
+                </div>
+              </MaxWidthLayout>
             </div>
-          </div>
-        </fieldset>
-      </div>
-      <main className="rvo-max-width-layout rvo-max-width-layout--sm">
-        <div className="rvo-content">
-          <h1 className="utrecht-heading-1">Search in Navigation</h1>
-
-          <p className="rvo-paragraph rvo-paragraph--md">
-            This demopage demonstrates how a search element can be included in the navbar.
-          </p>
+          </Fieldset>
         </div>
-      </main>
+        <MaxWidthLayout size="sm">
+          <main>
+            <div className="rvo-content">
+              <h1 className="utrecht-heading-1">Search in Navigation</h1>
+
+              <p className="rvo-paragraph rvo-paragraph--md">
+                This demopage demonstrates how a search element can be included in the navbar. As you can see this in
+                included.
+              </p>
+            </div>
+          </main>
+        </MaxWidthLayout>
+      </LayoutColumnRow>
     </div>
   );
 };
