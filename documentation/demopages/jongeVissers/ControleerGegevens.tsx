a7858902e79de34fcd8945ee840963924c629f10
Robert Roose <103183050+rroose-rvo@users.noreply.github.com>
2022-08-15 15:53:55 +0200

    Feature/dp jonge vissers (#221)
    * Create Jonge Vissers aanvraag demo pages
    
    * Add doing state to progress tracker
    
    * Add warning below form field
    
    * Temp fix for radio button issues
    
    * Change default icon-size value to md in Link component
    
    * Add JV demo pages
    
    * Add JV datum demo page
    
    * Update style.scss
    
    * Fix css
    
    * Fix CSS

---
+++ documentation/demopages/jongeVissers/ControleerGegevens.tsx
@@ -0,0 +1,120 @@
+import { Button } from '@nl-rvo/components/button/css/template';
+import { Header } from '@nl-rvo/components/header/css/template';
+import { Heading } from '@nl-rvo/components/heading/css/template';
+import { LayoutColumnRow } from '@nl-rvo/components/layout-column-row/css/template';
+import { Link } from '@nl-rvo/components/link/css/template';
+import { MaxWidthLayout } from '@nl-rvo/components/max-width-layout/css/template';
+import { MenuBar } from '@nl-rvo/components/menubar/css/template';
+import { ProgressTracker } from '@nl-rvo/components/progress-tracker/css/template';
+import '../common/style.scss';
+
+const ControleerGegevens = () => {
+  return (
+    <div className="rvo-demo-page">
+      <Header />
+      <MenuBar
+        items={[
+          { label: 'Home', icon: 'home', link: '#' },
+          { label: 'Uitloggen', icon: 'versleutelen', link: '#', align: 'right' },
+        ]}
+        size="md"
+        useIcons={true}
+        iconPlacement="before"
+        menuMaxWidth="md"
+      />
+      <MaxWidthLayout size="md">
+        <main className="rvo-progress-tracker-active">
+          <ProgressTracker
+            steps={[
+              { state: 'start', label: 'Jonge Vissers 2020', link: '#', size: 'md', line: 'straight' },
+              {
+                state: 'doing',
+                label: 'Controleer uw gegevens',
+                link: '/iframe.html?args=&id=demo-pagina-s-jonge-vissers--controleer-uw-gegevens&viewMode=story',
+                size: 'md',
+                line: 'straight',
+              },
+              {
+                state: 'incomplete',
+                label: 'Correspondentie',
+                link: '/iframe.html?args=&id=demo-pagina-s-jonge-vissers--correspondentie&viewMode=story',
+                size: 'md',
+                line: 'straight',
+              },
+              {
+                state: 'incomplete',
+                label: 'Datum verleningsverzoek',
+                link: '/iframe.html?args=&id=demo-pagina-s-jonge-vissers--datum&viewMode=story',
+                size: 'md',
+                line: 'straight',
+              },
+              { state: 'incomplete', label: 'Kosten', link: '#', size: 'md', line: 'straight' },
+              { state: 'incomplete', label: 'Bijlagen', link: '#', size: 'md', line: 'straight' },
+              { state: 'incomplete', label: 'Samenvatting', link: '#', size: 'md', line: 'straight' },
+              { state: 'incomplete', label: 'Ondertekenen', link: '#', size: 'md', line: 'straight' },
+              { state: 'end', label: 'Bevestiging', link: '#', size: 'md', line: 'none' },
+            ]}
+          />
+          <div className="rvo-form">
+            <LayoutColumnRow size="xl">
+              <div className="intro">
+                <Heading type="h1" textContent="Controleer uw gegevens"></Heading>
+                <LayoutColumnRow size="sm">
+                  <dl className="rvo-data">
+                    <dt>BSN</dt>
+                    <dd>35012085</dd>
+                    <dt>Naam</dt>
+                    <dd>Albert Heijn B.V.</dd>
+                    <dt>Rekeningnummer</dt>
+                    <dd>NLSNBD093845843</dd>
+                    <dt>Adres</dt>
+                    <dd>Haarsteeweg 25, 4560 KL, Zutphen</dd>
+                    <dt>E-mailadres</dt>
+                    <dd>albert@heijn.nl</dd>
+                  </dl>
+                  <Link showIcon="before" url="#" icon="bewerken" content="Wijzig deze gegevens"></Link>
+                </LayoutColumnRow>
+              </div>
+              <form>
+                <LayoutColumnRow size="md">
+                  <div className="rvo-button-group">
+                    <Button
+                      kind="secondary"
+                      size="md"
+                      textContent="Opslaan en sluiten"
+                      active={false}
+                      busy={false}
+                      focus={false}
+                      focusVisible={false}
+                      disabled={false}
+                      hover={false}
+                      showIcon="no"
+                    >
+                      Secondary action
+                    </Button>
+                    <Button
+                      kind="primary"
+                      size="md"
+                      textContent="Opslaan en verder"
+                      active={false}
+                      busy={false}
+                      focus={false}
+                      focusVisible={false}
+                      disabled={false}
+                      hover={false}
+                      showIcon="no"
+                    >
+                      Secondary action
+                    </Button>
+                  </div>
+                </LayoutColumnRow>
+              </form>
+            </LayoutColumnRow>
+          </div>
+        </main>
+      </MaxWidthLayout>
+    </div>
+  );
+};
+
+export default ControleerGegevens;
---
+++ documentation/demopages/jongeVissers/Datum.tsx
@@ -0,0 +1,110 @@
+import { Button } from '@nl-rvo/components/button/css/template';
+import { Fieldset } from '@nl-rvo/components/form-fieldset/css/template';
+import { Header } from '@nl-rvo/components/header/css/template';
+import { Heading } from '@nl-rvo/components/heading/css/template';
+import { LayoutColumnRow } from '@nl-rvo/components/layout-column-row/css/template';
+import { MaxWidthLayout } from '@nl-rvo/components/max-width-layout/css/template';
+import { MenuBar } from '@nl-rvo/components/menubar/css/template';
+import { ProgressTracker } from '@nl-rvo/components/progress-tracker/css/template';
+import '../common/style.scss';
+
+const Datum = () => {
+  return (
+    <div className="rvo-demo-page">
+      <Header />
+      <MenuBar
+        items={[
+          { label: 'Home', icon: 'home', link: '#' },
+          { label: 'Uitloggen', icon: 'versleutelen', link: '#', align: 'right' },
+        ]}
+        size="md"
+        useIcons={true}
+        iconPlacement="before"
+        menuMaxWidth="md"
+      />
+      <MaxWidthLayout size="md">
+        <main className="rvo-progress-tracker-active">
+          <ProgressTracker
+            steps={[
+              { state: 'start', label: 'Jonge Vissers 2020', link: '#', size: 'md', line: 'straight' },
+              {
+                state: 'completed',
+                label: 'Controleer uw gegevens',
+                link: '/iframe.html?args=&id=demo-pagina-s-jonge-vissers--controleer-uw-gegevens&viewMode=story',
+                size: 'md',
+                line: 'straight',
+              },
+              {
+                state: 'completed',
+                label: 'Correspondentie',
+                link: '/iframe.html?args=&id=demo-pagina-s-jonge-vissers--correspondentie&viewMode=story',
+                size: 'md',
+                line: 'straight',
+              },
+              {
+                state: 'doing',
+                label: 'Datum verleningsverzoek',
+                link: '/iframe.html?args=&id=demo-pagina-s-jonge-vissers--datum&viewMode=story',
+                size: 'md',
+                line: 'straight',
+              },
+              { state: 'incomplete', label: 'Kosten', link: '#', size: 'md', line: 'straight' },
+              { state: 'incomplete', label: 'Bijlagen', link: '#', size: 'md', line: 'straight' },
+              { state: 'incomplete', label: 'Samenvatting', link: '#', size: 'md', line: 'straight' },
+              { state: 'incomplete', label: 'Ondertekenen', link: '#', size: 'md', line: 'straight' },
+              { state: 'end', label: 'Bevestiging', link: '#', size: 'md', line: 'none' },
+            ]}
+          />
+          <div className="rvo-form">
+            <LayoutColumnRow size="xl">
+              <div className="intro">
+                <Heading type="h1" textContent="Datum verleningsverzoek"></Heading>
+              </div>
+              <form>
+                <LayoutColumnRow size="md">
+                  <Fieldset legend="">
+                    <label>Wat is de datum op de poststempel van het verleningsverzoek?</label>
+                    <input type="date" className="rvo-date"></input>
+                  </Fieldset>
+
+                  <div className="rvo-button-group">
+                    <Button
+                      kind="secondary"
+                      size="md"
+                      textContent="Opslaan en sluiten"
+                      active={false}
+                      busy={false}
+                      focus={false}
+                      focusVisible={false}
+                      disabled={false}
+                      hover={false}
+                      showIcon="no"
+                    >
+                      Secondary action
+                    </Button>
+                    <Button
+                      kind="primary"
+                      size="md"
+                      textContent="Opslaan en verder"
+                      active={false}
+                      busy={false}
+                      focus={false}
+                      focusVisible={false}
+                      disabled={false}
+                      hover={false}
+                      showIcon="no"
+                    >
+                      Secondary action
+                    </Button>
+                  </div>
+                </LayoutColumnRow>
+              </form>
+            </LayoutColumnRow>
+          </div>
+        </main>
+      </MaxWidthLayout>
+    </div>
+  );
+};
+
+export default Datum;
