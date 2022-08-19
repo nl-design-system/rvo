9d0a4379e57aca5534014a7f5a9795ab18772873
Robert Roose <103183050+rroose-rvo@users.noreply.github.com>
2022-08-19 11:19:39 +0200

    Refactor submenu demo page (#235)

--- documentation/demopages/algemeen/Submenu.tsx
+++ documentation/demopages/algemeen/Submenu.tsx
@@ -1,57 +1,49 @@
 import { Header } from '@nl-rvo/components/header/css/template';
+import { LayoutColumnRow } from '@nl-rvo/components/layout-column-row/css/template';
+import { MenuBar } from '@nl-rvo/components/menubar/css/template';
 import '../common/style.scss';
 
 const Submenu = () => {
   return (
-    <div className="rvo-demo-page">
+    <div className="rvo-demo-page rvo-demo-page--sub-menu">
       <Header />
-      <nav className="rvo-topnav rvo-topnav--md">
-        <ul className="utrecht-topnav__list">
-          <li className="utrecht-topnav__item">
-            <a className="utrecht-topnav__link" href="#">
-              <div className="rvo-icon rvo-icon-home rvo-icon--md rvo-icon--wit"></div> Home
-            </a>
-          </li>
-          <li className="utrecht-topnav__item">
-            <a className="utrecht-topnav__link rvo-topnav__link--expanded" href="#">
-              Expanded menu item <div className="rvo-icon rvo-icon-delta-omlaag rvo-icon--md rvo-icon--wit"></div>
-            </a>
-          </li>
-          <li className="utrecht-topnav__item">
-            <a className="utrecht-topnav__link" href="#">
-              Menu item
-            </a>
-          </li>
-        </ul>
-      </nav>
-      <nav className="rvo-topnav rvo-topnav--md rvo-topnav--submenu">
-        <ul className="utrecht-topnav__list rvo-topnav__list--submenu">
-          <li className="utrecht-topnav__item">
-            <a className="utrecht-topnav__link" href="#">
-              <div className="rvo-icon rvo-icon-plus rvo-icon--md rvo-icon--wit"></div> Start
-            </a>
-          </li>
-          <li className="utrecht-topnav__item">
-            <a className="utrecht-topnav__link" href="#">
-              Another submenu item
-            </a>
-          </li>
-          <li className="utrecht-topnav__item">
-            <a className="utrecht-topnav__link" href="#">
-              Another submenu item
-            </a>
-          </li>
-        </ul>
-      </nav>
-      <main className="rvo-max-width-layout rvo-max-width-layout--sm">
-        <div className="rvo-content">
-          <h1 className="utrecht-heading-1">Default submenu</h1>
-          <p className="rvo-paragraph rvo-paragraph--md">
-            A submenu is expanded when a user clicks on an expandable menu item which label is followed by a downwards
-            chevron. The submenu expands in place not overlapping existing content.
-          </p>
+      <LayoutColumnRow size="md">
+        <div className="rvo-navigation">
+          <MenuBar
+            items={[
+              { label: 'Home', icon: 'home', link: '#' },
+              { label: 'Expanded menu item', icon: 'delta-omlaag', link: '#' },
+              { label: 'Uitloggen', icon: 'versleutelen', link: '#', align: 'right' },
+            ]}
+            size="md"
+            useIcons={false}
+            iconPlacement="none"
+            menuMaxWidth="md"
+          />
+          <div className="rvo-tmp-submenu-wrapper">
+            <MenuBar
+              items={[
+                { label: 'Submenu item', icon: 'home', link: '#' },
+                { label: 'Submenu item', icon: 'delta-omlaag', link: '#' },
+                { label: 'Submenu item', icon: 'versleutelen', link: '#', align: 'right' },
+              ]}
+              size="md"
+              useIcons={false}
+              iconPlacement="none"
+              menuMaxWidth="md"
+            />
+          </div>
         </div>
-      </main>
+        <main className="rvo-max-width-layout rvo-max-width-layout--sm">
+          <div className="rvo-content">
+            <h1 className="utrecht-heading-1">Default submenu</h1>
+            <p className="rvo-paragraph rvo-paragraph--md">
+              A submenu is expanded when a user clicks on an expandable menu item which label is followed by a downwards
+              chevron. The submenu expands in place not overlapping existing content.
+            </p>
+          </div>
+        </main>
+      </LayoutColumnRow>
     </div>
   );
 };
