f5738d2ec5e1b578242d2390a32e6c9e53cf76ae
Tom Jonkman <tom.jonkman@rvo.nl>
2022-08-29 12:28:38 +0200

    Add Alert component

---
+++ components/alert/css/template.tsx
@@ -0,0 +1,96 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2022 Community for NL Design System
+ */
+import '@utrecht/component-library-css';
+import clsx from 'clsx';
+import React from 'react';
+import { StatusIcon } from '../../status-icon/css/template';
+import validateHTML from '../../utils/validateHTML';
+
+export interface IAlertProps {
+  kind?: string;
+  heading?: string;
+  content?: string;
+  closable?: boolean;
+}
+
+export const argTypes = {
+  kind: {
+    options: ['info', 'warning', 'error', 'success'],
+    control: { type: 'radio' },
+  },
+  heading: {
+    control: 'text',
+  },
+  content: {
+    control: 'text',
+  },
+  closable: {
+    control: 'boolean',
+  },
+};
+
+export const defaultArgs: IAlertProps = {
+  kind: 'info',
+  heading: '',
+  content: 'This is an example of an alert.',
+  closable: false,
+};
+
+export const Alert: React.FC<IAlertProps> = ({
+  kind = defaultArgs.kind,
+  heading = defaultArgs.heading,
+  content = defaultArgs.content,
+  closable = defaultArgs.closable,
+}) => {
+  let iconMarkup;
+  switch (kind) {
+    case 'info':
+      iconMarkup = StatusIcon({ type: 'info', size: 'xl' });
+      break;
+    case 'warning':
+      iconMarkup = StatusIcon({ type: 'waarschuwing', size: 'xl' });
+      break;
+    case 'error':
+      iconMarkup = StatusIcon({ type: 'foutmelding', size: 'xl' });
+      break;
+    case 'success':
+      iconMarkup = StatusIcon({ type: 'bevestiging', size: 'xl' });
+      break;
+  }
+
+  const isValidHTML = validateHTML(content);
+
+  return (
+    <div className={clsx('rvo-alert', `rvo-alert--${kind}`)}>
+      {iconMarkup}
+      <div className="rvo-alert-text">
+        {heading?.length > 0 && <strong>{heading}</strong>}
+        {isValidHTML ? <div dangerouslySetInnerHTML={{ __html: content }}></div> : <div>{content}</div>}
+      </div>
+      {closable && (
+        <button className="utrecht-button rvo-button--close">
+          <div className="rvo-icon rvo-icon-kruis rvo-icon--md rvo-icon--zwart"></div>
+        </button>
+      )}
+    </div>
+  );
+};
+
+export const AllAlertKinds: React.FC<IAlertProps> = (alertArgs) => (
+  <div>
+    <p>
+      <Alert {...alertArgs} kind="info" />
+    </p>
+    <p>
+      <Alert {...alertArgs} kind="warning" />
+    </p>
+    <p>
+      <Alert {...alertArgs} kind="error" />
+    </p>
+    <p>
+      <Alert {...alertArgs} kind="success" />
+    </p>
+  </div>
+);
