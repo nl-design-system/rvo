78dc6af87fc79ee6daf8bf4fe26b0fc9bfa76155
Tom Jonkman <tom.jonkman@rvo.nl>
2022-08-30 11:42:51 +0200

    Fix parsing of HTML strings without starting/closing tags

--- components/utils/validateHTML.js
+++ components/utils/validateHTML.js
@@ -1,6 +1,11 @@
 export default (htmlString) => {
+  // Add encompassing HTML tags if needed
+  let htmlStringToParse = htmlString;
+  if (htmlString.match(/<.+?>/g) && (!htmlString.match(/^<.+?>/g) || !htmlString.match(/<.+?>$/gm))) {
+    htmlStringToParse = `<div>${htmlString}</div>`;
+  }
   const parser = new DOMParser();
-  const doc = parser.parseFromString(htmlString, 'application/xml');
+  const doc = parser.parseFromString(htmlStringToParse, 'application/xml');
   const errorNode = doc.querySelector('parsererror');
   return errorNode === null ? true : false;
 };
