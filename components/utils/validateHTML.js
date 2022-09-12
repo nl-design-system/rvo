a473e7109d0370ee6f8872883bd694952a365479
Tom Jonkman <tom.jonkman@rvo.nl>
2022-09-12 11:53:12 +0200

    Simplify validation even more

--- components/utils/validateHTML.js
+++ components/utils/validateHTML.js
@@ -1,12 +1,5 @@
 export default (htmlString) => {
-  // Add encompassing HTML tags if needed
-  let htmlStringToParse = htmlString;
-  const htmlStringContainsHTML = htmlString.match(/<.+?>/gi);
-  const htmlStringHasEncompassingElement = htmlString.match(/^<([A-Z][A-Z0-9]*)>\b[^>]*>(.*?)<\/\1>/gi);
-
-  if (htmlStringContainsHTML && !htmlStringHasEncompassingElement) {
-    htmlStringToParse = `<div>${htmlString}</div>`;
-  }
+  let htmlStringToParse = `<div>${htmlString}</div>`;
   const parser = new DOMParser();
   const doc = parser.parseFromString(htmlStringToParse, 'application/xml');
   const errorNode = doc.querySelector('parsererror');
