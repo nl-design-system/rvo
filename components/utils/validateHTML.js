29a666e6301f66e34f7d2a6d95726e87c0023f58
Tom Jonkman <tom.jonkman@rvo.nl>
2022-09-12 11:41:33 +0200

    Simplify addition of encompassing HTML tags

--- components/utils/validateHTML.js
+++ components/utils/validateHTML.js
@@ -1,7 +1,10 @@
 export default (htmlString) => {
   // Add encompassing HTML tags if needed
   let htmlStringToParse = htmlString;
-  if (htmlString.match(/<.+?>/g) && (!htmlString.match(/^<.+?>/g) || !htmlString.match(/<.+?>$/gm))) {
+  const htmlStringContainsHTML = htmlString.match(/<.+?>/gi);
+  const htmlStringHasEncompassingElement = htmlString.match(/^<([A-Z][A-Z0-9]*)>\b[^>]*>(.*?)<\/\1>/gi);
+
+  if (htmlStringContainsHTML && !htmlStringHasEncompassingElement) {
     htmlStringToParse = `<div>${htmlString}</div>`;
   }
   const parser = new DOMParser();
