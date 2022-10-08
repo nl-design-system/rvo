nl-design-system/rvo

commit cf18d4a70945b9019754a509edaccb069b158901

Add prefix / suffix option to text input

components/form-textinput/css/template.tsx
@@ -16,7 +16,9 @@ export interface ITextInputProps {
   inputType?: string;
   placeholder?: string;
   value?: string;
-  valueType?: string;
+  validation?: string;
+  prefix?: string;
+  suffix?: string;
 }
 
 export const argTypes = {
@@ -46,7 +48,13 @@ export const argTypes = {
   value: {
     control: 'text',
   },
-  valueType: { options: ['text', 'number', 'currency'], control: { type: 'radio' } },
+  validation: { options: ['text', 'number', 'currency'], control: { type: 'radio' } },
+  prefix: {
+    control: 'text',
+  },
+  suffix: {
+    control: 'text',
+  },
 };
 
 export const defaultArgs: ITextInputProps = {
@@ -59,7 +67,9 @@ export const defaultArgs: ITextInputProps = {
   inputType: 'text',
   placeholder: '',
   value: '',
-  valueType: 'text',
+  validation: 'text',
+  prefix: '',
+  suffix: '',
 };
 
 export const TextInput: React.FC<ITextInputProps> = ({
@@ -72,7 +82,9 @@ export const TextInput: React.FC<ITextInputProps> = ({
   inputType = defaultArgs.inputType,
   placeholder = defaultArgs.placeholder,
   value = defaultArgs.value,
-  valueType = defaultArgs.valueType,
+  validation = defaultArgs.validation,
+  prefix = defaultArgs.prefix,
+  suffix = defaultArgs.suffix,
 }) => {
   const props = {
     id,
@@ -91,16 +103,22 @@ export const TextInput: React.FC<ITextInputProps> = ({
     readOnly: readOnly || null,
     placeholder: placeholder || null,
     defaultValue: value,
-    ...((valueType === 'number' || valueType === 'currency') && {
+    ...((validation === 'number' || validation === 'currency') && {
       inputMode: 'numeric' as any,
-      pattern: valueType === 'currency' ? '[0-9.,]*' : '[0-9]*',
+      pattern: validation === 'currency' ? '[0-9.,]*' : '[0-9]*',
     }),
   };
 
   if (inputType === 'text') {
     const inputMarkup = <input type="text" {...props} />;
-    if (valueType === 'currency') {
-      return <div className="rvo-layout-row rvo-layout-gap--md">€{inputMarkup}</div>;
+    if (prefix || suffix) {
+      return (
+        <div className="rvo-layout-row rvo-layout-gap--md">
+          {prefix}
+          {inputMarkup}
+          {suffix}
+        </div>
+      );
     } else {
       return inputMarkup;
     }

components/form-textinput/css/form-textinput.prefix-suffix.stories.mdx
@@ -1,0 +1,30 @@
+import frameworkDecorator from "@nl-rvo/storybook/frameworkDecorator";
+import { Canvas, Meta, Story } from "@storybook/addon-docs";
+import { argTypes, defaultArgs, TextInput } from "./template";
+
+<Meta
+  title="CSS Components/Form/Text input/Prefix &frasl; Suffix"
+  argTypes={argTypes}
+  parameters={{
+    status: {
+      type: "WORK IN PROGRESS",
+    },
+  }}
+  decorators={[frameworkDecorator]}
+/>
+
+# Prefix
+
+<Canvas>
+  <Story name="Invalid" args={{ ...defaultArgs, valueType: "currency", prefix: "€" }}>
+    {TextInput.bind({})}
+  </Story>
+</Canvas>
+
+## Suffix
+
+<Canvas>
+  <Story name="Focus" args={{ ...defaultArgs, valueType: "number", suffix: "m³" }}>
+    {TextInput.bind({})}
+  </Story>
+</Canvas>

commit b7a612561d9ce9ab482240d132f59a6387b79186

fix: lint issues introduced by cf18d4a

components/form-textinput/css/template.tsx
@@ -4,8 +4,8 @@
  */
 import './index.scss';
 import { Textarea, Textbox } from '@utrecht/component-library-react';
-import React from 'react';
 import clsx from 'clsx';
+import React from 'react';
 
 export interface ITextInputProps {
   key?: string;

documentation/demopages/algemeen/Formulier.tsx
@@ -49,7 +49,7 @@ const Formulier = () => {
                       />
                       <TextInputField labelText="Text disabled" disabled={true} />
                       <TextInputField labelText="Text disabled with value" disabled={true} value="Value" />
-                      <TextInputField labelText="Number" valueType="number" />
+                      <TextInputField labelText="Number" validation="number" />
                       <TextInputField labelText="Textarea" inputType="textarea" />
                     </Fieldset>
 

documentation/demopages/jongeVissers/KostenOpvoeren.tsx
@@ -114,7 +114,7 @@ const KostenOpvoeren = () => {
                     <Fieldset legend="">
                       <TextInputField
                         labelText="Hoeveel kost het vissersvaartuig (exclusief btw)?"
-                        valueType="number"
+                        validation="number"
                       ></TextInputField>
                     </Fieldset>
                     <Fieldset legend="">
@@ -136,7 +136,7 @@ const KostenOpvoeren = () => {
                         helperText="Uitgebreide uitleg over de subsidie."
                         expandableHelperText={true}
                         expandableHelperTextTitle="Meer informatie"
-                        valueType="number"
+                        validation="number"
                       ></TextInputField>
                     </Fieldset>
 

documentation/demopages/jongeVissers/Project.tsx
@@ -152,18 +152,18 @@ const Project = () => {
                       ></RadioButtonField>
                       <TextInputField
                         labelText="Wat is dit EU-identificatienummer?"
-                        valueType="number"
+                        validation="number"
                       ></TextInputField>
                       <TextInputField labelText="In welk land is het vaartuig dat u aanschaft geregistreerd in het EU-vlootregister?"></TextInputField>
                     </Fieldset>
                     <Fieldset legend="">
                       <TextInputField
                         labelText="Hoeveel personen (in fte per jaar) verwacht u dat er op het visservaartuig komen werken, met uzelf erbij?"
-                        valueType="number"
+                        validation="number"
                       ></TextInputField>
                       <TextInputField
                         labelText="Hoeveel personen (in fte per jaar) verwacht u hebben niet eerder gewerkt in de visserij of maritieme sector?"
-                        valueType="number"
+                        validation="number"
                       ></TextInputField>
                     </Fieldset>
 

documentation/demopages/mijnrvo/MijnZaken.tsx
@@ -50,7 +50,7 @@ const MijnZaken = () => {
               <div className="rvo-tmp-filters">
                 <Fieldset legend="">
                   <TextInputField labelText="Zoek op omschrijving"></TextInputField>
-                  <TextInputField labelText="Zoek op zaaknummer" valueType="numeric"></TextInputField>
+                  <TextInputField labelText="Zoek op zaaknummer" validation="numeric"></TextInputField>
                   <details className="rvo-tmp-filter">
                     <summary>
                       <div className="rvo-form-field rvo-layout-column rvo-layout-gap--sm">

documentation/demopages/nl-design-system/algemeen/Formulier.tsx
@@ -52,7 +52,7 @@ const Formulier = () => {
                       />
                       <TextInputField labelText="Text disabled" disabled={true} />
                       <TextInputField labelText="Text disabled with value" disabled={true} value="Value" />
-                      <TextInputField labelText="Number" valueType="number" />
+                      <TextInputField labelText="Number" validation="number" />
                       <TextInputField labelText="Textarea" inputType="textarea" />
                     </Fieldset>
 

documentation/demopages/wbso/Aanvrager.tsx
@@ -104,8 +104,8 @@ const Aanvrager = () => {
                       ></RadioButtonField>
                     </Fieldset>
                     <Fieldset legend="Gegevens aanvrager in aanvraagperiode">
-                      <TextInputField labelText="Prognose totaal aantal werknemers" valueType="number" />
-                      <TextInputField labelText="(Waarvan) eigen S&O-werknemers" valueType="number" />
+                      <TextInputField labelText="Prognose totaal aantal werknemers" validation="number" />
+                      <TextInputField labelText="(Waarvan) eigen S&O-werknemers" validation="number" />
                     </Fieldset>
                     <Fieldset legend="S&O-uurloon">
                       <RadioButtonField