2b4a718d2a9430e2a06a2af67ede43aa9e52866b
Robbert Broersma <git@robbertbroersma.nl>
2022-10-10 11:32:42 +0200

    feat: react component library

---
+++ packages/components-react/src/Link.tsx
@@ -0,0 +1,58 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2021 Community for NL Design System
+ */
+import { Link as UtrechtLink } from '@utrecht/component-library-react';
+import type { LinkProps as UtrechtLinkProps } from '@utrecht/component-library-react/dist/Link';
+import clsx from 'clsx';
+import React, { ForwardedRef, forwardRef, PropsWithChildren } from 'react';
+import { Icon } from './index';
+
+export interface ILinkProps extends UtrechtLinkProps {
+  showIcon?: string;
+  icon?: string;
+  iconSize?: string;
+  iconColor?: string;
+  noUnderline?: boolean;
+}
+
+export const Link: React.FC<PropsWithChildren<ILinkProps>> = forwardRef(
+  (
+    { children, showIcon, icon, iconSize, iconColor, noUnderline = false, className, ...restProps },
+    ref: ForwardedRef<HTMLAnchorElement>,
+  ) => {
+    const iconMarkup = icon ? (
+      <Icon
+        icon={icon}
+        size={iconSize}
+        color={iconColor}
+        className={clsx({
+          'rvo-link__icon--before': showIcon === 'before',
+          'rvo-link__icon--after': showIcon === 'after',
+        })}
+      />
+    ) : null;
+
+    return (
+      <UtrechtLink
+        ref={ref}
+        className={clsx(
+          'rvo-link',
+          className,
+          {
+            'rvo-link--focus': focus,
+          },
+          showIcon !== 'no' && ['rvo-layout-row', 'rvo-layout-gap--sm'],
+          noUnderline && 'rvo-link--no-underline',
+        )}
+        {...restProps}
+      >
+        {showIcon === 'before' && iconMarkup}
+        {children}
+        {showIcon === 'after' && iconMarkup}
+      </UtrechtLink>
+    );
+  },
+);
+
+Link.displayName = 'Link';
