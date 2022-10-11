1fa56cd7ac69c58afe3c99ca13d914eaae962423
Robbert Broersma <git@robbertbroersma.nl>
2022-10-11 09:40:42 +0200

    feat: add React component variants with CSS import side effect

---
+++ packages/components-react/src/css-module/index.ts
@@ -0,0 +1,10 @@
+/**
+ * @license EUPL-1.2
+ * Copyright (c) 2021 Community for NL Design System
+ */
+
+// Components by RVO
+export { Footer } from './Footer';
+export { Icon } from './Icon';
+export { Link } from './Link';
+export { Logo } from './Logo';
