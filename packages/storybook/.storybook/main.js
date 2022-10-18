3ed765c51d4b70447d601a4e578afcdc5ede9ca2
Tom Jonkman <tom.jonkman@rvo.nl>
2022-10-18 21:04:40 +0200

    Again, try a fix

--- packages/storybook/.storybook/main.js
+++ packages/storybook/.storybook/main.js
@@ -13,7 +13,6 @@ module.exports = {
     previewMdx2: true,
     buildStoriesJson: true,
   },
-  // typescript: { reactDocgen: false },
   framework: '@storybook/react',
   addons: [
     '@etchteam/storybook-addon-status/register',
