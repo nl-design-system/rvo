93d5088697c269a36b97ab3909853d45735991b0
Robert Roose <robert.roose@rvo.nl>
2022-10-20 12:38:22 +0200

    Add cases list

---
+++ documentation/demopages/common/defaultMenuBarItemsMijnRVO.ts
@@ -0,0 +1,5 @@
+export const defaultMenuBarItemsMijnRVO = [
+  { label: 'Profiel', icon: 'user', link: '#' },
+  { label: 'Zaken', icon: 'map', link: '#' },
+  { label: 'Uitloggen', icon: 'secure_link', link: '#', align: 'right' },
+];
