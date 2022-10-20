4fcc573f14bafcf90422d81f5981effdf74a42cc
Tom Jonkman <tom.jonkman@rvo.nl>
2022-10-20 16:26:51 +0200

    Fix emotion resolve

--- packages/storybook/.storybook/main.js
+++ packages/storybook/.storybook/main.js
@@ -1,4 +1,21 @@
 /* eslint-env node */
+const path = require('path');
+const fs = require('fs');
+
+function getPackageDir(filepath) {
+  let currDir = path.dirname(require.resolve(filepath));
+  while (true) {
+    if (fs.existsSync(path.join(currDir, 'package.json'))) {
+      return currDir;
+    }
+    const { dir, root } = path.parse(currDir);
+    if (dir === root) {
+      throw new Error(`Could not find package.json in the parent directories starting from ${filepath}.`);
+    }
+    currDir = dir;
+  }
+}
+
 module.exports = {
   core: {
     builder: 'webpack5',
@@ -27,6 +44,18 @@ module.exports = {
   webpackFinal: async (config) => {
     const scssRule = config.module.rules.find((rule) => rule.test.toString().replace(/\\/g, '') == '/.s[ca]ss$/');
     scssRule.use = ['style-loader', 'css-loader', 'resolve-url-loader', 'sass-loader'];
-    return { ...config, performance: { hints: false } };
+    return {
+      ...config,
+      performance: { hints: false },
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          '@emotion/core': getPackageDir('@emotion/react'),
+          '@emotion/styled': getPackageDir('@emotion/styled'),
+          'emotion-theming': getPackageDir('@emotion/react'),
+        },
+      },
+    };
   },
 };
