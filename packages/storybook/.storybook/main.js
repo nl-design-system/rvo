047b080b835bb9d8eb247bee458653f98663a1df
Tom Jonkman <tom@tomjonkman.com>
2022-10-20 16:44:37 +0200

    Switch to storybook for React (#293)
    Co-authored-by: Tom Jonkman <tom.jonkman@rvo.nl>

--- packages/storybook/.storybook/main.js
+++ packages/storybook/.storybook/main.js
@@ -1,7 +1,25 @@
 /* eslint-env node */
+const path = require('path');
+const fs = require('fs');
+
+function getPackageDir(filepath) {
+  let currDir = path.dirname(require.resolve(filepath));
+  while (true) {
+    if (fs.existsSync(path.join(currDir, 'package.json'))) {
+      return currDir;
+    }
+    const { dir, root } = path.parse(currDir);
+    if (dir === root) {
+      throw new Error(`Could not find package.json in the parent directories starting from ${filepath}.`);
+    }
+    currDir = dir;
+  }
+}
+
 module.exports = {
   core: {
     builder: 'webpack5',
+    disableTelemetry: true,
   },
   stories: [
     '../../../documentation/**/*stories.@(js|jsx|mdx|ts|tsx|js|jsx)',
@@ -10,9 +28,11 @@ module.exports = {
   features: {
     postcss: false,
     previewMdx2: true,
+    buildStoriesJson: true,
   },
+  framework: '@storybook/react',
   addons: [
-    '@etchteam/storybook-addon-status',
+    '@etchteam/storybook-addon-status/register',
     '@storybook/addon-a11y',
     '@storybook/addon-docs',
     '@storybook/addon-controls',
@@ -24,6 +44,18 @@ module.exports = {
   webpackFinal: async (config) => {
     const scssRule = config.module.rules.find((rule) => rule.test.toString().replace(/\\/g, '') == '/.s[ca]ss$/');
     scssRule.use = ['style-loader', 'css-loader', 'resolve-url-loader', 'sass-loader'];
-    return config;
+    return {
+      ...config,
+      performance: { hints: false },
+      resolve: {
+        ...config.resolve,
+        alias: {
+          ...config.resolve.alias,
+          '@emotion/core': getPackageDir('@emotion/react'),
+          '@emotion/styled': getPackageDir('@emotion/styled'),
+          'emotion-theming': getPackageDir('@emotion/react'),
+        },
+      },
+    };
   },
 };
