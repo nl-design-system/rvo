c64dad7a8fb855b3b2030d67e228e096d4c0b571
Robert Roose <robert.roose@rvo.nl>
2022-10-24 15:28:34 +0200

    Add profiel demopage

---
+++ documentation/demopages/mijnrvo/Profiel.tsx
@@ -0,0 +1,147 @@
+import { Header } from '@nl-rvo/components/header/css/template';
+import { Heading } from '@nl-rvo/components/heading/css/template';
+import { Icon } from '@nl-rvo/components/icon/css/template';
+import { LayoutColumnRow } from '@nl-rvo/components/layout-column-row/css/template';
+import { Link } from '@nl-rvo/components/link/css/template';
+import { MaxWidthLayout } from '@nl-rvo/components/max-width-layout/css/template';
+import { MenuBar } from '@nl-rvo/components/menubar/css/template';
+import '../common/style.scss';
+import { defaultMenuBarItemsMijnRVO } from '../common/defaultMenuBarItemsMijnRVO';
+
+const Profiel = () => {
+  return (
+    <div className="rvo-demo-page rvo-demo-page--mijn-rvo">
+      <div className="rvo-header-content">
+        <Header />
+
+        <details className="rvo-responsive-menu">
+          <summary>
+            <span className="utrecht-icon rvo-icon rvo-icon-menu rvo-icon--lg rvo-icon--wit"></span>
+            <span className="utrecht-icon rvo-icon rvo-icon-kruis rvo-icon--lg rvo-icon--wit"></span>Menu
+          </summary>
+          <MenuBar
+            items={defaultMenuBarItemsMijnRVO}
+            size="lg"
+            useIcons={true}
+            iconPlacement="before"
+            menuMaxWidth="md"
+          />
+        </details>
+        <MenuBar
+          items={defaultMenuBarItemsMijnRVO}
+          size="lg"
+          useIcons={true}
+          iconPlacement="before"
+          menuMaxWidth="md"
+        />
+
+        <MaxWidthLayout size="md">
+          <main className="rvo-main rvo-main--zaak">
+            <div className="rvo-main-content">
+              <LayoutColumnRow size="2xl">
+                <LayoutColumnRow size="md">
+                  <h1 className="utrecht-heading-1">Profiel</h1>
+                </LayoutColumnRow>
+              </LayoutColumnRow>
+            </div>
+            <aside className="rvo-sidebar"></aside>
+          </main>
+        </MaxWidthLayout>
+      </div>
+      <footer className="rvo-footer">
+        <MaxWidthLayout size="lg">
+          <ul className="rvo-footer-menu">
+            <li className="rvo-footer-menu-title">
+              <Heading type="h3" textContent="Algemeen"></Heading>
+            </li>
+            <li className="rvo-footer-menu-item">
+              <Icon icon="delta-naar-rechts" color="wit" size="sm" />
+              <Link url="#" content="Help"></Link>
+            </li>
+            <li className="rvo-footer-menu-item">
+              <Icon icon="delta-naar-rechts" color="wit" size="sm" />
+              <Link url="#" content="Toegankelijkheid"></Link>
+            </li>
+            <li className="rvo-footer-menu-item">
+              <Icon icon="delta-naar-rechts" color="wit" size="sm" />
+              <Link url="#" content="Webservices"></Link>
+            </li>
+            <li className="rvo-footer-menu-item">
+              <Icon icon="delta-naar-rechts" color="wit" size="sm" />
+              <Link url="#" content="Privacy"></Link>
+            </li>
+            <li className="rvo-footer-menu-item">
+              <Icon icon="delta-naar-rechts" color="wit" size="sm" />
+              <Link url="#" content="Sitemap"></Link>
+            </li>
+          </ul>
+          <ul className="rvo-footer-menu">
+            <li className="rvo-footer-menu-title">
+              <Heading type="h3" textContent="Hoe werkt"></Heading>
+            </li>
+            <li className="rvo-footer-menu-item">
+              <Icon icon="delta-naar-rechts" color="wit" size="sm" />
+              <Link url="#" content="Machtigingen en TAN-codes"></Link>
+            </li>
+            <li className="rvo-footer-menu-item">
+              <Icon icon="delta-naar-rechts" color="wit" size="sm" />
+              <Link url="#" content="Zaken regelen bij RVO"></Link>
+            </li>
+            <li className="rvo-footer-menu-item">
+              <Icon icon="delta-naar-rechts" color="wit" size="sm" />
+              <Link url="#" content="Inschrijven en registreren"></Link>
+            </li>
+            <li className="rvo-footer-menu-item">
+              <Icon icon="delta-naar-rechts" color="wit" size="sm" />
+              <Link url="#" content="Bedrijfsoverdracht melden"></Link>
+            </li>
+            <li className="rvo-footer-menu-item">
+              <Icon icon="delta-naar-rechts" color="wit" size="sm" />
+              <Link url="#" content="Overlijden melden"></Link>
+            </li>
+          </ul>
+          <ul className="rvo-footer-menu">
+            <li className="rvo-footer-menu-title">
+              <Heading type="h3" textContent="Naar"></Heading>
+            </li>
+            <li className="rvo-footer-menu-item">
+              <Icon icon="delta-naar-rechts" color="wit" size="sm" />
+              <Link url="#" content="Abonneren op nieuwsbrief"></Link>
+            </li>
+            <li className="rvo-footer-menu-item">
+              <Icon icon="delta-naar-rechts" color="wit" size="sm" />
+              <Link url="#" content="Klantenpanel"></Link>
+            </li>
+            <li className="rvo-footer-menu-item">
+              <Icon icon="delta-naar-rechts" color="wit" size="sm" />
+              <Link url="#" content="Berichtenbox voor bedrijven"></Link>
+            </li>
+            <li className="rvo-footer-menu-item">
+              <Icon icon="delta-naar-rechts" color="wit" size="sm" />
+              <Link url="#" content="RVO.nl"></Link>
+            </li>
+          </ul>
+          <ul className="rvo-footer-menu">
+            <li className="rvo-footer-menu-title">
+              <Heading type="h3" textContent="Contact"></Heading>
+            </li>
+            <li className="rvo-footer-menu-item">
+              <Icon icon="delta-naar-rechts" color="wit" size="sm" />
+              <Link url="#" content="Contactgegevens"></Link>
+            </li>
+            <li className="rvo-footer-menu-item">
+              <Icon icon="delta-naar-rechts" color="wit" size="sm" />
+              <Link url="#" content="Contactformulier"></Link>
+            </li>
+            <li className="rvo-footer-menu-item">
+              <Icon icon="delta-naar-rechts" color="wit" size="sm" />
+              <Link url="#" content="Aanvraag voorlichting/spreker"></Link>
+            </li>
+          </ul>
+        </MaxWidthLayout>
+      </footer>
+    </div>
+  );
+};
+
+export default Profiel;
