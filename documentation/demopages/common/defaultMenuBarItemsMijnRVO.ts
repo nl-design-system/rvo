4fb04010abb11bfc3d08cf5ee707615bd8b75a48
Robert Roose <robert.roose@rvo.nl>
2022-10-25 14:59:33 +0200

    Add language selector to default mijn rvo menu

--- documentation/demopages/common/defaultMenuBarItemsMijnRVO.ts
+++ documentation/demopages/common/defaultMenuBarItemsMijnRVO.ts
@@ -2,4 +2,5 @@ export const defaultMenuBarItemsMijnRVO = [
   { label: 'Profiel', icon: 'user', link: '#' },
   { label: 'Zaken', icon: 'map', link: '#' },
   { label: 'Uitloggen', icon: 'secure_link', link: '#', align: 'right' },
+  { label: 'English', icon: 'wereldbol_met_loep', link: '#', align: 'right' },
 ];
