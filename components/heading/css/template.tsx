82970b8dfc702725a9f5d920f91a3118b85f2810
Tom Jonkman <tom.jonkman@rvo.nl>
2022-10-27 16:43:22 +0200

    Make heading linkable

--- components/heading/css/template.tsx
+++ components/heading/css/template.tsx
@@ -5,10 +5,12 @@
 
 import clsx from 'clsx';
 import React from 'react';
+import { Link } from '../../link/css/template';
 
 interface IHeadingProps {
   type: string;
   textContent: string;
+  link?: string;
   distanced?: boolean;
 }
 
@@ -20,34 +22,52 @@ export const argTypes = {
   textContent: {
     control: 'text',
   },
+  link: {
+    control: 'text',
+  },
 };
 
 export const defaultArgs: IHeadingProps = {
   type: 'h1',
   textContent: 'Heading',
+  link: '',
 };
 
 export const Heading: React.FC<IHeadingProps> = ({
   type = defaultArgs.type,
   textContent = defaultArgs.textContent,
+  link = defaultArgs.link,
 }) => {
   const props = {
     className: clsx(`utrecht-heading-${type.replace('h', '')}`),
   };
 
+  let headingMarkup;
   switch (type) {
     default:
     case 'h1':
-      return <h1 {...props}>{textContent}</h1>;
+      headingMarkup = <h1 {...props}>{textContent}</h1>;
+      break;
     case 'h2':
-      return <h2 {...props}>{textContent}</h2>;
+      headingMarkup = <h2 {...props}>{textContent}</h2>;
+      break;
     case 'h3':
-      return <h3 {...props}>{textContent}</h3>;
+      headingMarkup = <h3 {...props}>{textContent}</h3>;
+      break;
     case 'h4':
-      return <h4 {...props}>{textContent}</h4>;
+      headingMarkup = <h4 {...props}>{textContent}</h4>;
+      break;
     case 'h5':
-      return <h5 {...props}>{textContent}</h5>;
+      headingMarkup = <h5 {...props}>{textContent}</h5>;
+      break;
     case 'h6':
-      return <h6 {...props}>{textContent}</h6>;
+      headingMarkup = <h6 {...props}>{textContent}</h6>;
+      break;
+  }
+
+  if (link && link.length) {
+    return <Link url={link} content={headingMarkup} noUnderline={true} />;
+  } else {
+    return headingMarkup;
   }
 };
