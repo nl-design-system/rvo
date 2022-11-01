4a215cdbeaa2484bb047c99e29e2c7a8aff8fd51
Robert Roose <robert.roose@rvo.nl>
2022-11-01 11:01:52 +0100

    Add routing to Mijn RVO demopages

--- documentation/demopages/common/defaultMenuBarItemsMijnRVO.ts
+++ documentation/demopages/common/defaultMenuBarItemsMijnRVO.ts
@@ -3,6 +3,11 @@ export const defaultMenuBarItemsMijnRVO = [
   { label: 'Zaken', icon: '', link: '/iframe.html?args=&id=pagina-s-mijn-rvo--mijn-zaken&viewMode=story' },
   { label: 'Diensten', icon: '', link: '/iframe.html?args=&id=pagina-s-mijn-rvo--diensten&viewMode=story' },
   { label: 'Help', icon: '', link: '#' },
-  { label: 'Uitloggen', icon: '', link: '#', align: 'right' },
+  {
+    label: 'Uitloggen',
+    icon: '',
+    link: '/iframe.html?args=&id=pagina-s-mijn-rvo--inloggen&viewMode=story',
+    align: 'right',
+  },
   { label: 'English', icon: 'wereldbol', link: '#', align: 'right' },
 ];
