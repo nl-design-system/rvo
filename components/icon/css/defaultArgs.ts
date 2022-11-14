828101e370cbb50b5ccf55842eeca7c21b662ceb
Tom Jonkman <tom.jonkman@rvo.nl>
2022-11-14 16:40:16 +0100

    Add TODO: Fix to Icon component

--- components/icon/css/defaultArgs.ts
+++ components/icon/css/defaultArgs.ts
@@ -1,7 +1,9 @@
 import { argTypes, IIconProps } from './template';
 
 export const defaultArgs: IIconProps = {
-  icon: argTypes.icon.options[0],
+  // TODO: Fix this
+  // icon: argTypes.icon.options[0],
+  icon: 'pijltje_omlaag',
   size: argTypes.size.options[2],
   color: argTypes.color.options[0],
 };
