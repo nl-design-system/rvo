9567f485d20ef4b4a548e8c1e793bad2711ba9ee
Tom Jonkman <tom.jonkman@rvo.nl>
2022-11-14 22:08:46 +0100

    Fix uxpId for FormLayout

--- components/form-layout/css/presets/0-default.jsx
+++ components/form-layout/css/presets/0-default.jsx
@@ -1,4 +1,4 @@
 import { defaultArgs } from '../defaultArgs';
 import FormLayout from '../template';
 
-export default <FormLayout uxpId="fieldset" {...defaultArgs} />;
+export default <FormLayout uxpId="formLayout" {...defaultArgs} />;
