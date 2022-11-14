140432639db3f28978e0d220bc24a3af3c0fca82
Tom Jonkman <tom.jonkman@rvo.nl>
2022-11-14 21:31:29 +0100

    Add webp to webpack in uxpin-merge package

--- uxpin-merge/webpack.config.js
+++ uxpin-merge/webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
         },
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
         loader: {
           loader: 'url-loader',
           options: {
