d6fe927786e3767300b2a6ee4dc75a9162e6f686
Tom Jonkman <tom.jonkman@rvo.nl>
2022-11-14 13:42:25 +0100

    Move Form components into their own category

--- uxpin-merge/uxpin.config.js
+++ uxpin-merge/uxpin.config.js
@@ -4,13 +4,14 @@ module.exports = {
       {
         name: 'General',
         include: [
-          '../components/alert/css/template.tsx',
-          '../components/breadcrumbs/css/template.tsx',
-          '../components/button/css/template.tsx',
-          '../components/button-group/css/template.tsx',
-          '../components/data-list/css/template.tsx',
-          '../components/expandable-text/css/template.tsx',
-          '../components/footer/css/template.tsx',
+          '../components/form-checkbox/css/template.tsx',
+          '../components/form-dateinput/css/template.tsx',
+          '../components/form-feedback/css/template.tsx',
+        ],
+      },
+      {
+        name: 'Forms',
+        include: [
           '../components/form-checkbox/css/template.tsx',
           '../components/form-dateinput/css/template.tsx',
           '../components/form-feedback/css/template.tsx',
