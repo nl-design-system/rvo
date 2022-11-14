60f8652ea3f57a16d6f24f750acd28379a8557a6
Tom Jonkman <tom.jonkman@rvo.nl>
2022-11-14 18:15:40 +0100

    Hardcode icon defaults for now

--- components/icon/css/defaultArgs.ts
+++ components/icon/css/defaultArgs.ts
@@ -1,9 +1,9 @@
-import { argTypes, IIconProps } from './template';
+import { IIconProps } from './template';
 
 export const defaultArgs: IIconProps = {
   // TODO: Fix this
   // icon: argTypes.icon.options[0],
-  icon: 'pijltje_omlaag',
-  size: argTypes.size.options[2],
-  color: argTypes.color.options[0],
+  icon: 'home',
+  size: 'md',
+  color: 'hemelblauw',
 };
