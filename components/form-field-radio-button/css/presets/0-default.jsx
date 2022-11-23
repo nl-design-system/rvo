762d2d9d4ad9cb5846159875cdb822834249171c
Tom Jonkman <tom.jonkman@rvo.nl>
2022-11-23 14:35:44 +0100

    Add children to checkbox / radio field preset

--- components/form-field-radio-button/css/presets/0-default.jsx
+++ components/form-field-radio-button/css/presets/0-default.jsx
@@ -1,4 +1,10 @@
+import RadioButton from '../../../form-radio-button/css/template';
 import { defaultArgs } from '../defaultArgs';
 import RadioButtonField from '../template';
 
-export default <RadioButtonField uxpId="radioButtonField" {...defaultArgs} />;
+export default (
+  <RadioButtonField uxpId="radioButtonField" {...defaultArgs}>
+    <RadioButton uxpId="radio1" name="group" label="Value A" value="valueA" />
+    <RadioButton uxpId="radio2" name="group" label="Value B" value="valueB" />
+  </RadioButtonField>
+);
