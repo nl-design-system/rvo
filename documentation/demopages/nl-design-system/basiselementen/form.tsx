09571dd60c23fe3790cf2c43636cd44416321d5b
egor <studio@dutchcelt.com>
2022-11-24 15:43:05 +0100

    style: flex layout system for forms

--- documentation/demopages/nl-design-system/basiselementen/form.tsx
+++ documentation/demopages/nl-design-system/basiselementen/form.tsx
@@ -1,16 +1,26 @@
 import { RadioButtonField } from '@nl-rvo/components/form-field-radio-button/css/template';
-import { TextInputField } from '@nl-rvo/components/form-field-textinput/css/template';
-import { LayoutColumnRow } from '@nl-rvo/components/layout-column-row/css/template';
 import '../common/style.scss';
-import { Button, ButtonGroup, Fieldset, Heading1 } from '@utrecht/component-library-react';
+import {
+  Button,
+  ButtonGroup,
+  Fieldset,
+  FormField,
+  FormLabel,
+  Heading1,
+  Textbox,
+} from '@utrecht/component-library-react';
 
 const Form = () => {
   return (
     <div className="basiselementen-demo-page">
       <Heading1>Gegevens zonnepanelen invullen</Heading1>
-      <form className="rvo-layout-spacer rvo-layout-spacer--xs">
-        <Fieldset>
-          <TextInputField readOnly={true} label={'Uw naam'} />
+      <form className="basiselementen-layout-form">
+        <Fieldset className="basiselementen-layout-form__fieldset">
+          <FormField className={'basiselementen-layout-form__field'}>
+            <FormLabel htmlFor={'realname'}>Uw naam</FormLabel>
+            <Textbox id={'realname'}></Textbox>
+          </FormField>
+
           <RadioButtonField
             name="radio-buttons"
             label="Staat uw naam op de energierekening?"
@@ -20,8 +30,11 @@ const Form = () => {
             ]}
           ></RadioButtonField>
         </Fieldset>
-        <Fieldset>
-          <TextInputField readOnly={true} label={'Uw naam'} />
+        <Fieldset className="basiselementen-layout-form__fieldset">
+          <FormField className={'basiselementen-layout-form__field'}>
+            <FormLabel htmlFor={'billingname'}>Uw naam</FormLabel>
+            <Textbox id={'billingname'}></Textbox>
+          </FormField>
 
           <RadioButtonField
             name="radio-buttons"
@@ -32,29 +45,59 @@ const Form = () => {
             ]}
           ></RadioButtonField>
         </Fieldset>
-        <Fieldset>
-          <div className={'utrecht-form-label'}>Factuurdatum</div>
+        <Fieldset className="basiselementen-layout-form__fieldset">
+          <h3 className={'utrecht-form-label'}>Factuurdatum</h3>
           <div className={'utrecht-form-field-description'}>
             Vul de datum in die op de factuur van de zonnepanelen staat.
           </div>
-          <LayoutColumnRow size="sm" row={true}>
-            <TextInputField size={'sm'} label="Dag" />
-            <TextInputField size={'sm'} label="Maand" />
-            <TextInputField size={'sm'} label="Jaar" />
-          </LayoutColumnRow>
+          <FormField
+            className={
+              'basiselementen-layout-form__field basiselementen-layout-form__field--xs basiselementen-layout-form__field--collapse'
+            }
+          >
+            <FormLabel htmlFor={'day'}>Dag</FormLabel>
+            <Textbox id={'day'}></Textbox>
+          </FormField>
+          <FormField
+            className={
+              'basiselementen-layout-form__field basiselementen-layout-form__field--xs basiselementen-layout-form__field--collapse'
+            }
+          >
+            <FormLabel htmlFor={'month'}>Maand</FormLabel>
+            <Textbox id={'month'}></Textbox>
+          </FormField>
+          <FormField
+            className={
+              'basiselementen-layout-form__field basiselementen-layout-form__field--sm basiselementen-layout-form__field--collapse'
+            }
+          >
+            <FormLabel htmlFor={'year'}>Jaar</FormLabel>
+            <Textbox id={'year'}></Textbox>
+          </FormField>
         </Fieldset>
-        <Fieldset>
+        <Fieldset className="basiselementen-layout-form__fieldset">
           <div className={'utrecht-form-label'}>Adres zonnepanelen</div>
           <div className={'utrecht-form-field-description'}>Vul het adres in waar de zonnepanelen zijn geplaatst.</div>
-          <LayoutColumnRow size="sm" row={true}>
-            <TextInputField size={'md'} label="Straatnaam" />
-            <TextInputField size={'sm'} label="Huisnummer" />
-            <TextInputField size={'sm'} label="Toevoeging (niet verplicht)" />
-          </LayoutColumnRow>
-          <LayoutColumnRow size="sm" row={true}>
-            <TextInputField size={'sm'} label="Postcode" />
-            <TextInputField size={'md'} label="Plaatsnaam" />
-          </LayoutColumnRow>
+          <FormField className={'basiselementen-layout-form__field basiselementen-layout-form__field--xl'}>
+            <FormLabel htmlFor={'street'}>Straatnaam</FormLabel>
+            <Textbox id={'street'}></Textbox>
+          </FormField>
+          <FormField className={'basiselementen-layout-form__field basiselementen-layout-form__field--xs'}>
+            <FormLabel htmlFor={'housenumber'}>Huisnummer</FormLabel>
+            <Textbox id={'housenumber'}></Textbox>
+          </FormField>
+          <FormField className={'basiselementen-layout-form__field basiselementen-layout-form__field--xs'}>
+            <FormLabel htmlFor={'suffix'}>Toevoeging</FormLabel>
+            <Textbox id={'suffix'}></Textbox>
+          </FormField>
+          <FormField className={'basiselementen-layout-form__field basiselementen-layout-form__field--sm'}>
+            <FormLabel htmlFor={'postcode'}>Postcode</FormLabel>
+            <Textbox id={'postcode'}></Textbox>
+          </FormField>
+          <FormField className={'basiselementen-layout-form__field basiselementen-layout-form__field--xl'}>
+            <FormLabel htmlFor={'city'}>Plaatsnaam</FormLabel>
+            <Textbox id={'city'}></Textbox>
+          </FormField>
         </Fieldset>
         <ButtonGroup className="utrecht-button-group--horizontal">
           <Button appearance="secondary-action-button" busy={false} disabled={false}>
