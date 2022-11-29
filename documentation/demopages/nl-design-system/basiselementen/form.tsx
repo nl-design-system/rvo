1c208227d433f1324f837103c5b976fd344084c3
egor <studio@dutchcelt.com>
2022-11-29 13:37:38 +0100

    style: moved grid to fieldsets and fields

--- documentation/demopages/nl-design-system/basiselementen/form.tsx
+++ documentation/demopages/nl-design-system/basiselementen/form.tsx
@@ -1,10 +1,12 @@
-import { RadioButtonField } from '@nl-rvo/components/form-field-radio-button/css/template';
 import '../common/style.scss';
 import {
   Button,
   ButtonGroup,
+  CustomRadioButton,
   Fieldset,
+  FieldsetLegend,
   FormField,
+  FormFieldDescription,
   FormLabel,
   Heading1,
   Textbox,
@@ -12,82 +14,86 @@ import {
 
 const Form = () => {
   return (
-    <div className="basiselementen-demo-page">
+    <div className={'basiselementen-demo-page'}>
       <Heading1>Gegevens zonnepanelen invullen</Heading1>
       <form className="basiselementen-layout-form">
-        <Fieldset className="basiselementen-layout-form__fieldset">
-          <FormField className={'basiselementen-layout-form__field'}>
+        <Fieldset>
+          <FormField>
             <FormLabel htmlFor={'realname'}>Uw naam</FormLabel>
             <Textbox id={'realname'}></Textbox>
           </FormField>
-
-          <RadioButtonField
-            name="hasrealname"
-            label="Staat uw naam op de energierekening?"
-            options={[
-              { id: 'realnameA', label: 'Ja' },
-              { id: 'realnameB', label: 'Nee' },
-            ]}
-          ></RadioButtonField>
         </Fieldset>
-        <Fieldset className="basiselementen-layout-form__fieldset">
-          <FormField className={'basiselementen-layout-form__field'}>
-            <FormLabel htmlFor={'billingname'}>Uw naam</FormLabel>
-            <Textbox id={'billingname'}></Textbox>
+        <Fieldset>
+          <FieldsetLegend className={'utrecht-form-fieldset__legend--label'}>
+            Staat uw naam op de energierekening
+          </FieldsetLegend>
+          <FormField className={'utrecht-form-field--xs utrecht-form-field--start'}>
+            <FormLabel htmlFor={'realnameA'}>
+              <CustomRadioButton name={'hasrealname'} id={'realnameA'} />
+              Ja
+            </FormLabel>
           </FormField>
-          <RadioButtonField
-            name="hasbillingname"
-            label="Staat uw naam op de energierekening?"
-            options={[
-              { id: 'billingnameA', label: 'Ja' },
-              { id: 'billingnameB', label: 'Nee' },
-            ]}
-          ></RadioButtonField>
+          <FormField className={'utrecht-form-field--xs utrecht-form-field--start'}>
+            <FormLabel htmlFor={'realnameB'}>
+              <CustomRadioButton name={'hasrealname'} id={'realnameB'} />
+              Nee
+            </FormLabel>
+          </FormField>
         </Fieldset>
-        <Fieldset className="basiselementen-layout-form__fieldset">
-          <FormField className={'basiselementen-layout-form__field'}>
-            <h3 className={'utrecht-form-label'}>Factuurdatum</h3>
-            <div className={'utrecht-form-field-description'}>
-              Vul de datum in die op de factuur van de zonnepanelen staat.
-            </div>
+        <Fieldset className={'utrecht-form-fieldset--subtle'}>
+          <FieldsetLegend className={'utrecht-form-fieldset__legend--label'}>
+            Staat uw naam op de factuur van de zonnepanelen
+          </FieldsetLegend>
+          <FormField className={'utrecht-form-field--xs utrecht-form-field--start'}>
+            <FormLabel htmlFor={'billingnameA'}>
+              <CustomRadioButton name={'hasbillingname'} id={'billingnameA'} />
+              Ja
+            </FormLabel>
           </FormField>
-          <FormField className={'basiselementen-layout-form__field basiselementen-layout-form__field--xs '}>
+          <FormField className={'utrecht-form-field--xs utrecht-form-field--start'}>
+            <FormLabel htmlFor={'billingnameB'}>
+              <CustomRadioButton name={'hasbillingname'} id={'billingnameB'} />
+              Nee
+            </FormLabel>
+          </FormField>
+        </Fieldset>
+        <Fieldset className={'utrecht-form-fieldset--subtle'}>
+          <FieldsetLegend className={'utrecht-form-fieldset__legend--label'}>Factuurdatum</FieldsetLegend>
+          <FormFieldDescription>Vul de datum in die op de factuur van de zonnepanelen staat.</FormFieldDescription>
+          <FormField className={'utrecht-form-field--xs'}>
             <FormLabel htmlFor={'day'}>Dag</FormLabel>
-            <Textbox id={'day'}></Textbox>
+            <Textbox id={'day'} invalid={true}></Textbox>
           </FormField>
-          <FormField className={'basiselementen-layout-form__field basiselementen-layout-form__field--xs '}>
+          <FormField className={'utrecht-form-field--xs '}>
             <FormLabel htmlFor={'month'}>Maand</FormLabel>
-            <Textbox id={'month'}></Textbox>
+            <Textbox id={'month'} invalid={true}></Textbox>
           </FormField>
-          <FormField className={'basiselementen-layout-form__field basiselementen-layout-form__field--sm '}>
+          <FormField className={'utrecht-form-field--sm '}>
             <FormLabel htmlFor={'year'}>Jaar</FormLabel>
-            <Textbox id={'year'}></Textbox>
+            <Textbox id={'year'} invalid={true}></Textbox>
           </FormField>
+          <FormFieldDescription invalid={true}>Vul een geldige datum in.</FormFieldDescription>
         </Fieldset>
-        <Fieldset className="basiselementen-layout-form__fieldset">
-          <FormField className={'basiselementen-layout-form__field'}>
-            <div className={'utrecht-form-label'}>Adres zonnepanelen</div>
-            <div className={'utrecht-form-field-description'}>
-              Vul het adres in waar de zonnepanelen zijn geplaatst.
-            </div>
-          </FormField>
-          <FormField className={'basiselementen-layout-form__field basiselementen-layout-form__field--xl'}>
+        <Fieldset className={'utrecht-form-fieldset--subtle'}>
+          <FieldsetLegend className={'utrecht-form-fieldset__legend--label'}>Adres zonnepanelen</FieldsetLegend>
+          <FormFieldDescription>Vul het adres in waar de zonnepanelen zijn geplaatst.</FormFieldDescription>
+          <FormField className={'utrecht-form-field--lg'}>
             <FormLabel htmlFor={'street'}>Straatnaam</FormLabel>
             <Textbox id={'street'}></Textbox>
           </FormField>
-          <FormField className={'basiselementen-layout-form__field basiselementen-layout-form__field--xs'}>
+          <FormField className={'utrecht-form-field--sm'}>
             <FormLabel htmlFor={'housenumber'}>Huisnummer</FormLabel>
             <Textbox id={'housenumber'}></Textbox>
           </FormField>
-          <FormField className={'basiselementen-layout-form__field basiselementen-layout-form__field--xs'}>
+          <FormField className={'utrecht-form-field--sm'}>
             <FormLabel htmlFor={'suffix'}>Toevoeging</FormLabel>
             <Textbox id={'suffix'}></Textbox>
           </FormField>
-          <FormField className={'basiselementen-layout-form__field basiselementen-layout-form__field--sm'}>
+          <FormField className={'utrecht-form-field--sm utrecht-form-field--start'}>
             <FormLabel htmlFor={'postcode'}>Postcode</FormLabel>
             <Textbox id={'postcode'}></Textbox>
           </FormField>
-          <FormField className={'basiselementen-layout-form__field basiselementen-layout-form__field--xl'}>
+          <FormField className={'utrecht-form-field--xl'}>
             <FormLabel htmlFor={'city'}>Plaatsnaam</FormLabel>
             <Textbox id={'city'}></Textbox>
           </FormField>
