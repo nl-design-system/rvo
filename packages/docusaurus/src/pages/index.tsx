8d89953d4efe2f18b55da60443d61dc8eb80613d
Tom Jonkman <tom.jonkman@rvo.nl>
2023-01-02 12:56:30 +0100

    Fix @site path alias in docusaurus

--- packages/docusaurus/src/pages/index.tsx
+++ packages/docusaurus/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import HomepageFeatures from '@site/components/HomepageFeatures';
 import Layout from '@theme/Layout';
 import clsx from 'clsx';
 import React from 'react';
