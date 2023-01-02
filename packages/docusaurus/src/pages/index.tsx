3131811182dedb3c5ad915d59c91972291bf14ec
Tom Jonkman <tom.jonkman@rvo.nl>
2023-01-02 13:24:38 +0100

    Move HomePageHeader to its own component

--- packages/docusaurus/src/pages/index.tsx
+++ packages/docusaurus/src/pages/index.tsx
@@ -1,26 +1,14 @@
-import Link from '@docusaurus/Link';
+import HomepageFeatures from '@components/HomepageFeatures';
+import HomepageHeader from '@components/HomepageHeader';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import HomepageFeatures from '@site/components/HomepageFeatures';
 import Layout from '@theme/Layout';
-import clsx from 'clsx';
 import React from 'react';
-import styles from './index.module.css';
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout title={`Hello from ${siteConfig.title}`} description="Description will go into a meta tag in <head />">
-      <header className={clsx('hero hero--primary', styles.heroBanner)}>
-        <div className="container">
-          <h1 className="hero__title">{siteConfig.title}</h1>
-          <p className="hero__subtitle">{siteConfig.tagline}</p>
-          <div className={styles.buttons}>
-            <Link className="button button--secondary button--lg" to="/docs/intro">
-              Start hier 🚀
-            </Link>
-          </div>
-        </div>
-      </header>
+    <Layout title={siteConfig.title} description={`${siteConfig.title}: ${siteConfig.tagline}`}>
+      <HomepageHeader />
       <main>
         <HomepageFeatures />
       </main>
