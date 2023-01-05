439c80d99295c4635ba9b18528729d9be0443851
Tom Jonkman <tom.jonkman@rvo.nl>
2023-01-05 12:20:20 +0100

    Move home page back to docusaurus

---
+++ packages/docusaurus/src/pages/index.tsx
@@ -0,0 +1,44 @@
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import styled from '@emotion/styled/macro';
+import Layout from '@theme/Layout';
+import clsx from 'clsx';
+import React from 'react';
+
+export default function Home(): JSX.Element {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout title={siteConfig.title} description={siteConfig.title}>
+      <header className={clsx('hero hero--primary')}>
+        <div className="container">
+          <h1 className="hero__title">Ontwerp met de componenten van ROOS</h1>
+          <p className="hero__subtitle">
+            Gebruik het ROOS design system om uw applicatie RVO-consistent te maken. Leer van de research en ervaring
+            van andere ontwikkelaars en voorkom werk dat al gedaan is.
+          </p>
+          <div>
+            <Link className="button button--secondary button--lg" to="/docs/intro">
+              Aan de slag 🚀
+            </Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        <Hero>
+          <div className="container">
+            <div className="row">test</div>
+          </div>
+        </Hero>
+      </main>
+    </Layout>
+  );
+}
+
+const Hero = styled.section`
+  .features {
+    align-items: center;
+    display: flex;
+    padding: 2rem 0;
+    width: 100%;
+  }
+`;
