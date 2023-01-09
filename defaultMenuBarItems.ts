5686512266c00a087cc3c69c3d7d62e311db7d48
tomjonkman <tom@tomjonkman.com>
2023-01-09 08:39:12 +0000

    Deploying to gh-pages from @ nl-design-system/rvo@e07dd36e6c43782f3187f56e10c675e3b6a2ecf7 🚀

---
+++ defaultMenuBarItems.ts
@@ -0,0 +1,6 @@
+export const defaultMenuBarItems = [
+  { label: 'Home', icon: 'home' as const, link: '#' },
+  { label: 'Mijn aanvragen', icon: 'publicatie' as const, link: '#' },
+  { label: 'Nieuwe aanvraag', icon: 'plus' as const, link: '#' },
+  { label: 'Uitloggen', icon: 'versleutelen' as const, link: '#', align: 'right' as const },
+];
---
+++ defaultMenuBarItemsMijnRVO.ts
@@ -0,0 +1,12 @@
+export const defaultMenuBarItemsMijnRVO = [
+  { label: 'Zaken', link: 'iframe.html?args=&id=pagina-s-mijn-rvo--mijn-zaken&viewMode=story' },
+  { label: 'Diensten', link: 'iframe.html?args=&id=pagina-s-mijn-rvo--diensten&viewMode=story' },
+  { label: 'Help', link: '#' },
+  { label: 'Profiel', link: 'iframe.html?args=&id=pagina-s-mijn-rvo--profiel&viewMode=story' },
+  {
+    label: 'Uitloggen',
+    link: 'iframe.html?args=&id=pagina-s-mijn-rvo--inloggen&viewMode=story',
+    align: 'right' as const,
+  },
+  { label: 'English', icon: 'wereldbol' as const, link: '#', align: 'right' as const },
+];
