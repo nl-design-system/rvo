b2e2ecde72df3d34674fd73a2bdc6f7e9f0abf09
rroose-rvo <103183050+rroose-rvo@users.noreply.github.com>
2023-01-23 09:32:32 +0000

    Deploying to gh-pages from @ nl-design-system/rvo@597f0f436db417c31e1384a451dc202da7550614 🚀

--- defaultMenuBarItemsMijnRVO.ts
+++ defaultMenuBarItemsMijnRVO.ts
@@ -1,11 +1,11 @@
 export const defaultMenuBarItemsMijnRVO = [
-  { label: 'Zaken', link: 'iframe.html?args=&id=pagina-s-mijn-rvo--mijn-zaken&viewMode=story' },
-  { label: 'Diensten', link: 'iframe.html?args=&id=pagina-s-mijn-rvo--diensten&viewMode=story' },
+  { label: 'Zaken', link: 'iframe.html?args=&id=demo-pagina-s-mijn-rvo--mijn-zaken&viewMode=story' },
+  { label: 'Diensten', link: 'iframe.html?args=&id=demo-pagina-s-mijn-rvo--diensten&viewMode=story' },
   { label: 'Help', link: '#' },
-  { label: 'Profiel', link: 'iframe.html?args=&id=pagina-s-mijn-rvo--profiel&viewMode=story' },
+  { label: 'Profiel', link: 'iframe.html?args=&id=demo-pagina-s-mijn-rvo--profiel&viewMode=story' },
   {
     label: 'Uitloggen',
-    link: 'iframe.html?args=&id=pagina-s-mijn-rvo--inloggen&viewMode=story',
+    link: 'iframe.html?args=&id=demo-pagina-s-mijn-rvo--inloggen&viewMode=story',
     align: 'right' as const,
   },
   { label: 'English', icon: 'wereldbol' as const, link: '#', align: 'right' as const },
