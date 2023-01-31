52d52c7781aa13d81171ca03e3903aede1c8761f
Tom Jonkman <tom.jonkman@rvo.nl>
2023-01-31 12:20:20 +0100

    Include HTML codeblock in ComponentExample

--- packages/docusaurus/src/components/ComponentExample/index.tsx
+++ packages/docusaurus/src/components/ComponentExample/index.tsx
@@ -1,13 +1,27 @@
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import { ExpandableText } from '@nl-rvo/components';
 import { Link } from '@nl-rvo/components';
+import CodeBlock from '@theme/CodeBlock';
+import prettierBabel from 'prettier/parser-babel';
+import prettier from 'prettier/standalone';
 import { useMemo } from 'react';
+import * as ReactDOMServer from 'react-dom/server';
 import { serialize } from '../../utils/react-serialize';
 import styles from './styles.module.css';
 
 const ComponentExample = ({ children }) => {
   const serialized = useMemo(() => encodeURIComponent(serialize(children)), [children]);
   const previewLink = `${useDocusaurusContext().siteConfig.baseUrl}preview?${serialized}`;
+
+  const html = useMemo(() => {
+    return prettier
+      .format(ReactDOMServer.renderToStaticMarkup(children), {
+        parser: 'babel',
+        plugins: [prettierBabel],
+      })
+      .replace(/\{" "\}/gm, ' ')
+      .replace(/(;)[^;]*$/g, '');
+  }, [children]);
 
   return (
     <div className={styles.componentExample}>
@@ -16,7 +30,7 @@ const ComponentExample = ({ children }) => {
       </div>
       <div className={styles.componentContainer}>{children}</div>
       <div className={styles.infoContainer}>
-        <ExpandableText title="HTML" content={<>test</>} />
+        <ExpandableText title="HTML" content={<CodeBlock language="html">{html}</CodeBlock>} />
       </div>
     </div>
   );
