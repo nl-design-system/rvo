d9f2e8239eb6a406374825a141eda7b41860c333
Tom Jonkman <tom.jonkman@rvo.nl>
2023-02-06 11:54:29 +0100

    Add article grid to docusaurus frontpage

--- packages/docusaurus/src/pages/index.tsx
+++ packages/docusaurus/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import { Button, Heading, MaxWidthLayout } from '@nl-rvo/components';
+import { Button, Heading, MaxWidthLayout, Link as RVOLink } from '@nl-rvo/components';
 import Layout from '@theme/Layout';
 import clsx from 'clsx';
 import styles from './index.module.css';
@@ -23,9 +23,63 @@ export default function Home(): JSX.Element {
         </div>
       </header>
       <main>
-        <section
-          style={{ alignItems: 'center', display: 'flex', paddingBlock: '2rem', paddingInline: 0, width: '100%' }}
-        ></section>
+        <MaxWidthLayout size="lg" className={clsx('rvo-layout-column', 'rvo-layout-gap--md', styles.hero)}>
+          <section className={styles.articleGrid}>
+            <div className={clsx('rvo-layout-column', 'rvo-layout-gap--sm')}>
+              <Heading type="h2">Huisstijl</Heading>
+              <p>De kleuren, lettertypes, de space en sizes die er beschikbaar zijn binnen ROOS</p>
+              <Link to="/introduction">
+                <RVOLink>Bekijk de huisstijl</RVOLink>
+              </Link>
+            </div>
+            <div className={clsx('rvo-layout-column', 'rvo-layout-gap--sm')}>
+              <Heading type="h2">Componenten</Heading>
+              <p>Een collectie aan gebruiksvriendelijke, toegankelijke componenten die voldoen aan de Rijkshuisstijl</p>
+              <Link to="/introduction">
+                <RVOLink>Bekijk de componenten</RVOLink>
+              </Link>
+            </div>
+            <div className={clsx('rvo-layout-column', 'rvo-layout-gap--sm')}>
+              <Heading type="h2">Patronen</Heading>
+              <p>Een set van componenten die samen een patroon vormen van acties die een gebruiker moet uitvoeren</p>
+              <Link to="/introduction">
+                <RVOLink>Bekijk de patronen</RVOLink>
+              </Link>
+            </div>
+            <div className={clsx('rvo-layout-column', 'rvo-layout-gap--sm')}>
+              <Heading type="h2">Voor designers</Heading>
+              <p>Ben je een designer en wil je ROOS gebruiken? Gebruik dan ons Figma bestand</p>
+              <Link to="https://www.figma.com/file/NHV1JYxJ28vKZInSI9u200/ROOS-(RVO-Design-System)?node-id=282%3A5324">
+                <RVOLink showIcon="after" icon="externe-link">
+                  Figma bestand
+                </RVOLink>
+              </Link>
+            </div>
+            <div className={clsx('rvo-layout-column', 'rvo-layout-gap--sm')}>
+              <Heading type="h2">Voor developers</Heading>
+              <p>Ben je een developer en wil je ROOS gebruiken? Ga dan naar onze Storybook omgeving</p>
+              <Link
+                to={
+                  process.env.NODE_ENV === 'development'
+                    ? 'http://localhost:6006'
+                    : 'https://nl-design-system.github.io/rvo/'
+                }
+              >
+                <RVOLink>Storybook omgeving</RVOLink>
+              </Link>
+            </div>
+            <div className={clsx('rvo-layout-column', 'rvo-layout-gap--sm')}>
+              <Heading type="h2">Hulp nodig?</Heading>
+              <p>
+                Kom je er niet uit? We vinden het altijd leuk als iemand ons Design System wil gebruiken, dus stuur ons
+                een e-mail
+              </p>
+              <Link to="mailto:designsystem@rvo.nl">
+                <RVOLink>designsystem@rvo.nl</RVOLink>
+              </Link>
+            </div>
+          </section>
+        </MaxWidthLayout>
       </main>
     </Layout>
   );
