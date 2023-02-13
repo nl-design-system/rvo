8c38b4ba70931e01c658b250357385afdbb47f5b
Tom Jonkman <tom.jonkman@rvo.nl>
2023-02-13 16:40:24 +0100

    Simplify Checkbox snippet

--- components/form-checkbox/css/template.tsx
+++ components/form-checkbox/css/template.tsx
@@ -76,8 +76,6 @@ export const Checkbox: React.FC<ICheckboxProps> = ({
       invalid && 'rvo-checkbox--invalid',
       indeterminate && 'rvo-checkbox--indeterminate',
       required && 'rvo-checkbox--required',
-      'rvo-layout-row',
-      'rvo-layout-gap--sm',
     )}
     htmlFor={id}
   >
