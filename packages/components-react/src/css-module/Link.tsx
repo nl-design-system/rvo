eb1af95eb349802f694d3ef33ff22c919ecea214
Tom Jonkman <tom.jonkman@rvo.nl>
2023-02-14 15:59:16 +0100

    Fix build issues

--- packages/components-react/src/css-module/Link.tsx
+++ packages/components-react/src/css-module/Link.tsx
@@ -2,7 +2,7 @@
  * @license EUPL-1.2
  * Copyright (c) 2021 Community for NL Design System
  */
-import '../../../../node_modules/@utrecht/components/dist/link/index.css';
+import '@utrecht/components/dist/link/index.css';
 import '../../../../components/link/index.scss';
 
 export * from '../Link';
