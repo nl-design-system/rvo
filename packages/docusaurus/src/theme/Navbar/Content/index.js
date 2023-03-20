f37e6761219efb85a9dad58ebf1f97c7ad119a93
Tom Jonkman <tom.jonkman@rvo.nl>
2023-03-20 13:42:42 +0100

    Include sections in mobile menubar in docusaurus

--- packages/docusaurus/src/theme/Navbar/Content/index.js
+++ packages/docusaurus/src/theme/Navbar/Content/index.js
@@ -1,5 +1,6 @@
 import { useLocation } from '@docusaurus/router';
 import { useThemeConfig } from '@docusaurus/theme-common';
+import { useNavbarSecondaryMenu } from '@docusaurus/theme-common/internal';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import { parseMenuItem } from '@nl-rvo/components/menubar/template';
@@ -14,8 +15,9 @@ export default function NavbarContent() {
   const firstRightItem = items.findIndex((item) => item.position === 'right');
   const searchBarItem = items.find((item) => item.type === 'search');
   const docusaurusContext = useDocusaurusContext();
-  const location = useLocation();
-  const currentSection = location.pathname.replace(docusaurusContext.siteConfig.baseUrl, '').split('/')[0];
+  const location = useLocation().pathname.replace(docusaurusContext.siteConfig.baseUrl, '');
+  const currentPage = location.split('/')[0];
+  const sections = useNavbarSecondaryMenu();
 
   return (
     <React.Fragment>
@@ -23,12 +25,12 @@ export default function NavbarContent() {
         let active = false;
         if (item.docId) {
           const itemSection = item.docId.split('/')[0];
-          if (itemSection === currentSection) {
+          if (itemSection === currentPage) {
             active = true;
           }
         }
 
-        return parseMenuItem({
+        const menuItem = parseMenuItem({
           label: item.label,
           link: item.docId ? useBaseUrl(item.docId) : item.href || '#',
           align: item.position === 'right' && index === firstRightItem && 'right',
@@ -36,6 +38,27 @@ export default function NavbarContent() {
           key: index,
           active,
         });
+
+        if (!active) {
+          return menuItem;
+        } else {
+          return (
+            <>
+              {menuItem}
+              {sections &&
+                sections.content &&
+                sections.content.props.sidebar.map((section, index) => {
+                  return parseMenuItem({
+                    label: section.label,
+                    link: section.href,
+                    type: 'sub',
+                    key: index,
+                    active: section.href.indexOf(location) > -1,
+                  });
+                })}
+            </>
+          );
+        }
       })}
       <NavbarColorModeToggle className={styles.colorModeToggle} />
       {!searchBarItem && (
