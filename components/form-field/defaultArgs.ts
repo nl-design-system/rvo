10ce20a1e6fb8cfe68739357a3c2c1dfc795239f
Tom Jonkman <tom.jonkman@rvo.nl>
2023-03-30 11:07:50 +0200

    Fix bug: helper text always showing

--- components/form-field/defaultArgs.ts
+++ components/form-field/defaultArgs.ts
@@ -3,7 +3,7 @@ import { IFieldProps } from './template';
 export const defaultArgs: IFieldProps = {
   fieldId: 'fieldId',
   label: 'Field label',
-  helperText: 'Helper text',
+  helperText: null,
   helperTextId: 'helperTextId',
   expandableHelperText: false,
   expandableHelperTextTitle: 'Expandable helper text title',
