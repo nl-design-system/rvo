48e61a55fd489d15be1458eca1e40d468ba9edef
Tom Jonkman <tom.jonkman@rvo.nl>
2023-05-22 15:51:36 +0200

    Add Date input field component

---
+++ components/form-field-dateinput/form-field-dateinput.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { defaultArgs } from './defaultArgs';
+import { DateInputField } from './template';
+
+const meta: Meta<typeof DateInputField> = {
+  title: 'Componenten/Form/Date input field',
+  component: DateInputField,
+  args: defaultArgs,
+  parameters: {
+    status: {
+      type: 'WORK IN PROGRESS',
+    },
+    docusaurus: {
+      link: 'form-field-dateinput',
+    },
+  },
+};
+export default meta;
+
+type Story = StoryObj<typeof DateInputField>;
+
+export const Default: Story = { args: { ...defaultArgs, helperText: 'Helper text' }, name: 'Date input field' };
