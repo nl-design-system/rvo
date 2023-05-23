948cb6b39ca45dfcca4a5ec0c3ffb859d05c0b3c
Tom Jonkman <tom.jonkman@rvo.nl>
2023-05-23 14:48:33 +0200

    Fix jongeVissers page

--- documentation/demopages/jongeVissers/ControleerUwGegevens.stories.tsx
+++ documentation/demopages/jongeVissers/ControleerUwGegevens.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import ControleerUwGegevens from './ControleerUwGegevens';
 
 const meta: Meta<typeof ControleerUwGegevens> = {
-  title: "Demo pagina's/Jonge Vissers/ControleerUwGegevens",
+  title: "Demo pagina's/Jonge Vissers/Controleer uw gegevens",
   component: ControleerUwGegevens,
   parameters: {
     status: {
