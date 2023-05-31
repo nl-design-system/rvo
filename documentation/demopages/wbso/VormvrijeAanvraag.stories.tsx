3b343ef637d3bc11fcad1fe62bce58e3b5b2ff11
Tom Jonkman <tom.jonkman@rvo.nl>
2023-05-31 12:00:08 +0200

    Add inline storybook links to WBSO demopages

--- documentation/demopages/wbso/VormvrijeAanvraag.stories.tsx
+++ documentation/demopages/wbso/VormvrijeAanvraag.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import VormvrijeAanvraag from './VormvrijeAanvraag';
 
 const meta: Meta<typeof VormvrijeAanvraag> = {
-  title: "Demo pagina's/WBSO/02 Vormvrije aanvraag",
+  title: "Demo pagina's/WBSO/Vormvrije aanvraag",
   component: VormvrijeAanvraag,
   parameters: {
     status: {
@@ -14,4 +14,4 @@ export default meta;
 
 type Story = StoryObj<typeof VormvrijeAanvraag>;
 
-export const Default: Story = { name: '02 Vormvrije aanvraag' };
+export const Default: Story = { name: 'Vormvrije aanvraag' };
