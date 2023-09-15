03f13d05ca746099d66cdfb47b30f9a0d8dd1caf
Robbert Broersma <git@robbertbroersma.nl>
2023-09-15 14:46:19 +0200

    build: workaround for CSS not being included in build
    ideally we could tweak the Rollup config to ensure .css files from node_modules/ are included

--- packages/components-react/src/css-module/Footer.tsx
+++ packages/components-react/src/css-module/Footer.tsx
@@ -2,7 +2,7 @@
  * @license EUPL-1.2
  * Copyright (c) 2021 Community for NL Design System
  */
-import '@utrecht/components/dist/page-footer/index.css';
+import '../../node_modules/@utrecht/components/dist/page-footer/css/index.css';
 import '../../../../components/footer/index.scss';
 
 export * from '../Footer';
--- packages/components-react/src/css-module/Link.tsx
+++ packages/components-react/src/css-module/Link.tsx
@@ -2,7 +2,7 @@
  * @license EUPL-1.2
  * Copyright (c) 2021 Community for NL Design System
  */
-import '@utrecht/components/dist/link/index.css';
+import '../../node_modules/@utrecht/components/dist/link/css/index.css';
 import '../../../../components/link/index.scss';
 
 export * from '../Link';
