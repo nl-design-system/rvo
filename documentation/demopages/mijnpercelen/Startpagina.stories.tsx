9d6b52714e703bbc9ef02c58022a6cecc488ed24
Robert Roose <robert.roose@rvo.nl>
2023-10-26 10:36:28 +0200

    Add startpagina to Mijn Percelen demopages

---
+++ documentation/demopages/mijnpercelen/Startpagina.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Startpagina from './Startpagina';
+
+const meta: Meta<typeof Startpagina> = {
+  title: "Demo pagina's/Mijn Percelen/Startpagina",
+  component: Startpagina,
+  parameters: {
+    status: {
+      type: 'WORK IN PROGRESS',
+    },
+  },
+};
+export default meta;
+
+type Story = StoryObj<typeof Startpagina>;
+
+export const Default: Story = { name: 'Startpagina' };
