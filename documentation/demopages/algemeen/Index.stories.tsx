2e1770b123d8bdb114ac7e515525c1126d521a6b
Robert Roose <robert.roose@rvo.nl>
2023-10-27 16:57:00 +0200

    Add demo index page

---
+++ documentation/demopages/algemeen/Index.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Index from './Index';
+
+const meta: Meta<typeof Index> = {
+  title: "Demo pagina's/Index",
+  component: Index,
+  parameters: {
+    status: {
+      type: 'WORK IN PROGRESS',
+    },
+  },
+};
+export default meta;
+
+type Story = StoryObj<typeof Index>;
+
+export const Default: Story = { name: 'Index' };
