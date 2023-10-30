de1660daacbd35fac5a267e00840e393996edda2
Robert Roose <103183050+rroose-rvo@users.noreply.github.com>
2023-10-30 10:33:12 +0100

    Add mijn percelen screenshots page (#523)

---
+++ documentation/demopages/mijnpercelen/ScreenshotsUitgangspositie.tsx
@@ -0,0 +1,15 @@
+import {} from '@nl-rvo/components';
+import '../common/rhs-demos.scss';
+
+const ScreenshotsUitgangspositie = () => {
+  return (
+    <body className="rvo-theme rvo-screenshots rvo-demos">
+      <img src="images/screenshots/mijn_percelen/2023-10-30-startpagina.png" alt="" className="rvo-screenshot" />
+      <img src="images/screenshots/mijn_percelen/2023-10-30-percelen.png" alt="" className="rvo-screenshot" />
+      <img src="images/screenshots/mijn_percelen/2023-10-30-perceel-wijzigen.png" alt="" className="rvo-screenshot" />
+      <img src="images/screenshots/mijn_percelen/2023-10-30-versturen.png" alt="" className="rvo-screenshot" />
+    </body>
+  );
+};
+
+export default ScreenshotsUitgangspositie;
