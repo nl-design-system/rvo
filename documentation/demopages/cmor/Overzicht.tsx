c1ed253c998d9d464cde9dcca7b99c1db3d93b3f
Robert Roose <robert.roose@rvo.nl>
2023-11-24 16:57:34 +0100

    Add item list and divider

--- documentation/demopages/cmor/Overzicht.tsx
+++ documentation/demopages/cmor/Overzicht.tsx
@@ -1,4 +1,14 @@
-import { Button, ExpandableText, Heading, Icon, LayoutColumnRow, MenuBar, StatusIcon, Tag } from '@nl-rvo/components';
+import {
+  Button,
+  ExpandableText,
+  Heading,
+  Icon,
+  LayoutColumnRow,
+  Link,
+  MenuBar,
+  StatusIcon,
+  Tag,
+} from '@nl-rvo/components';
 import '../common/rhs-update.scss';
 import '../common/cmor.scss';
 
@@ -139,35 +149,177 @@ const Overzicht = () => {
                   </LayoutColumnRow>
                 </div>
               </LayoutColumnRow>
-              <div className="rvo-cards rvo-cards--grid">
+              <div className="rvo-cards">
                 <LayoutColumnRow size="md">
                   <div className="rvo-heading-w-tag">
                     <h2 className="utrecht-heading-2">Lopende aanvragen</h2>
                     <Tag type="info" showIcon="no" content="2 openstaande acties" />
                   </div>
-                  <div className="rvo-card rvo-card--w-link rvo-card--w-icon">
-                    <div className="rvo-card-content">
-                      <h3 className="utrecht-heading-3">
-                        <a href="#" className="rvo-link--full-click">
-                          ISDE 2023
-                        </a>
-                      </h3>
-                      <LayoutColumnRow size="xs">
-                        <p className="rvo-paragraph rvo-paragraph--sm rvo-paragraph--subtle">
-                          Investeringssubsidie duurzame energie en energiebesparing
-                        </p>
-                        <div className="rvo-card-status">
-                          <StatusIcon type="waarschuwing" size="md" />
-                          <div className="rvo-card-status-text">
-                            Stuur aanvullende informatie <strong>uiterlijk 19 aug 2023</strong>
+                  <div className="rvo-cards-grid">
+                    <div className="rvo-card rvo-card--w-link rvo-card--w-icon">
+                      <div className="rvo-card-content">
+                        <h3 className="utrecht-heading-3">
+                          <a href="#" className="rvo-link--full-click">
+                            ISDE 2023
+                          </a>
+                        </h3>
+                        <LayoutColumnRow size="xs">
+                          <p className="rvo-paragraph rvo-paragraph--sm rvo-paragraph--subtle">
+                            Investeringssubsidie duurzame energie en energiebesparing
+                          </p>
+                          <div className="rvo-card-status">
+                            <StatusIcon type="waarschuwing" size="md" />
+                            <div className="rvo-card-status-text">
+                              Stuur aanvullende informatie <strong>uiterlijk 19 aug 2023</strong>
+                            </div>
                           </div>
-                        </div>
-                      </LayoutColumnRow>
+                        </LayoutColumnRow>
+                      </div>
+                      <div className="rvo-card-icon">
+                        <Icon icon="delta-naar-rechts" size="sm" />
+                      </div>
                     </div>
-                    <div className="rvo-card-icon">
-                      <Icon icon="delta-naar-rechts" size="sm" />
+                    <div className="rvo-card rvo-card--w-link rvo-card--w-icon">
+                      <div className="rvo-card-content">
+                        <h3 className="utrecht-heading-3">
+                          <a href="#" className="rvo-link--full-click">
+                            BSN Melding WBSO 2023
+                          </a>
+                        </h3>
+                        <LayoutColumnRow size="xs">
+                          <p className="rvo-paragraph rvo-paragraph--sm rvo-paragraph--subtle">
+                            Wet Bevordering Speur- en Ontwikkelingswerk
+                          </p>
+                          <div className="rvo-card-status">
+                            <StatusIcon type="waarschuwing" size="md" />
+                            <div className="rvo-card-status-text">
+                              Verzilver uw voucher <strong>uiterlijk 28 aug 2023</strong>
+                            </div>
+                          </div>
+                        </LayoutColumnRow>
+                      </div>
+                      <div className="rvo-card-icon">
+                        <Icon icon="delta-naar-rechts" size="sm" />
+                      </div>
+                    </div>
+                    <div className="rvo-card rvo-card--w-link rvo-card--w-icon">
+                      <div className="rvo-card-content">
+                        <h3 className="utrecht-heading-3">
+                          <a href="#" className="rvo-link--full-click">
+                            Gecombineerde opgave 2023
+                          </a>
+                        </h3>
+                        <LayoutColumnRow size="xs">
+                          <p className="rvo-paragraph rvo-paragraph--sm rvo-paragraph--subtle">
+                            Gemeenschappelijk Landbouwbeleid
+                          </p>
+                          <div className="rvo-card-status">
+                            <Icon icon="zandloper" size="md" />
+                            <div className="rvo-card-status-text">In behandeling</div>
+                          </div>
+                        </LayoutColumnRow>
+                      </div>
+                      <div className="rvo-card-icon">
+                        <Icon icon="delta-naar-rechts" size="sm" />
+                      </div>
+                    </div>
+                    <div className="rvo-card rvo-card--w-link rvo-card--w-icon">
+                      <div className="rvo-card-content">
+                        <h3 className="utrecht-heading-3">
+                          <a href="#" className="rvo-link--full-click">
+                            SVVE 2023
+                          </a>
+                        </h3>
+                        <LayoutColumnRow size="xs">
+                          <p className="rvo-paragraph rvo-paragraph--sm rvo-paragraph--subtle">
+                            Subsidieregeling verduurzaming voor verenigingen van eigenaars
+                          </p>
+                          <div className="rvo-card-status">
+                            <Icon icon="vinkje" size="md" />
+                            <div className="rvo-card-status-text">Goedgekeurd</div>
+                          </div>
+                        </LayoutColumnRow>
+                      </div>
+                      <div className="rvo-card-icon">
+                        <Icon icon="delta-naar-rechts" size="sm" />
+                      </div>
                     </div>
                   </div>
+                  <div className="rvo-link-list">
+                    <Link
+                      showIcon="after"
+                      icon="pijl-naar-rechts"
+                      target="#"
+                      content="Alle lopende aanvragen (13)"
+                      noUnderline={true}
+                    />
+                    <Link
+                      showIcon="after"
+                      icon="pijl-naar-rechts"
+                      target="#"
+                      content="Mijn dossier"
+                      noUnderline={true}
+                    />
+                    <Link showIcon="after" icon="pijl-naar-rechts" target="#" content="Concepten" noUnderline={true} />
+                  </div>
+                </LayoutColumnRow>
+              </div>
+              <hr className="rvo-divider"></hr>
+              <div className="rvo-item-list">
+                <Heading type="h2" textContent="Start een nieuwe aanvraag" />
+                <span className="rvo-item-list-sub-title">Kies uit uw favorieten</span>
+                <LayoutColumnRow size="sm">
+                  <div className="rvo-item-list-items">
+                    <div className="rvo-item-list-item rvo-item-list-item--w-icon rvo-item-list-item--w-link">
+                      <div className="rvo-item-list-item-content">
+                        <div className="rvo-status-indicator rvo-status-indicator--open"></div>
+                        <div className="rvo-item-list-item-title">
+                          <a href="#" className="rvo-link--full-click">
+                            ANLb-Collectieven
+                          </a>
+                        </div>
+                        <div className="rvo-item-list-item-description">Open voor aanvragen</div>
+                      </div>
+                      <div className="rvo-item-list-icon">
+                        <Icon icon="delta-naar-rechts" size="sm" />
+                      </div>
+                    </div>
+                    <div className="rvo-item-list-item rvo-item-list-item--w-icon rvo-item-list-item--w-link">
+                      <div className="rvo-item-list-item-content">
+                        <div className="rvo-status-indicator rvo-status-indicator--closed"></div>
+                        <div className="rvo-item-list-item-title">
+                          <a href="#" className="rvo-link--full-click">
+                            ISDE
+                          </a>
+                        </div>
+                        <div className="rvo-item-list-item-description">Gesloten voor aanvragen</div>
+                      </div>
+                      <div className="rvo-item-list-icon">
+                        <Icon icon="delta-naar-rechts" size="sm" />
+                      </div>
+                    </div>
+                    <div className="rvo-item-list-item rvo-item-list-item--w-icon rvo-item-list-item--w-link">
+                      <div className="rvo-item-list-item-content">
+                        <div className="rvo-status-indicator rvo-status-indicator--tmp-closed"></div>
+                        <div className="rvo-item-list-item-title">
+                          <a href="#" className="rvo-link--full-click">
+                            Gecombineerde Opgave
+                          </a>
+                        </div>
+                        <div className="rvo-item-list-item-description">Tijdelijk gesloten voor aanvragen</div>
+                      </div>
+                      <div className="rvo-item-list-icon">
+                        <Icon icon="delta-naar-rechts" size="sm" />
+                      </div>
+                    </div>
+                  </div>
+                  <Link
+                    content="Zoek een andere regeling op rvo.nl"
+                    showIcon="after"
+                    icon="externe-link"
+                    target="#"
+                    noUnderline={true}
+                  />
                 </LayoutColumnRow>
               </div>
             </LayoutColumnRow>
