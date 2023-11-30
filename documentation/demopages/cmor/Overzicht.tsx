6d42e5682b20ecd687fb5912778695de55daa898
Robert Roose <103183050+rroose-rvo@users.noreply.github.com>
2023-11-30 10:13:08 +0100

    Demopages/cmor (#538)
    * Add faq
    
    * Add contact options
    
    * Make page responsive
    
    * Make footer responsive

--- documentation/demopages/cmor/Overzicht.tsx
+++ documentation/demopages/cmor/Overzicht.tsx
@@ -1,6 +1,9 @@
 import {
+  Accordion,
   Button,
+  ButtonGroup,
   ExpandableText,
+  Footer,
   Heading,
   Icon,
   LayoutColumnRow,
@@ -265,83 +268,86 @@ const Overzicht = () => {
                 </LayoutColumnRow>
               </div>
               <hr className="rvo-divider"></hr>
-              <div className="rvo-item-list">
-                <Heading type="h2" textContent="Start een nieuwe aanvraag" />
-                <span className="rvo-item-list-sub-title">Kies uit uw favorieten</span>
-                <LayoutColumnRow size="sm">
-                  <div className="rvo-item-list-items">
-                    <div className="rvo-item-list-item rvo-item-list-item--w-icon rvo-item-list-item--w-link">
-                      <div className="rvo-item-list-item-content">
-                        <div className="rvo-status-indicator rvo-status-indicator--open"></div>
-                        <div className="rvo-item-list-item-title">
-                          <a href="#" className="rvo-link--full-click">
-                            ANLb-Collectieven
-                          </a>
+              <div className="rvo-block-group">
+                <div className="rvo-item-list">
+                  <Heading type="h2" textContent="Start een nieuwe aanvraag" />
+                  <span className="rvo-item-list-sub-title">Kies uit uw favorieten</span>
+                  <LayoutColumnRow size="sm">
+                    <div className="rvo-item-list-items">
+                      <div className="rvo-item-list-item rvo-item-list-item--w-icon rvo-item-list-item--w-link">
+                        <div className="rvo-item-list-item-content">
+                          <div className="rvo-status-indicator rvo-status-indicator--open"></div>
+                          <div className="rvo-item-list-item-title">
+                            <a href="#" className="rvo-link--full-click">
+                              ANLb-Collectieven
+                            </a>
+                          </div>
+                          <div className="rvo-item-list-item-description">Open voor aanvragen</div>
                         </div>
-                        <div className="rvo-item-list-item-description">Open voor aanvragen</div>
+                        <div className="rvo-item-list-icon">
+                          <Icon icon="delta-naar-rechts" size="sm" />
+                        </div>
                       </div>
-                      <div className="rvo-item-list-icon">
-                        <Icon icon="delta-naar-rechts" size="sm" />
+                      <div className="rvo-item-list-item rvo-item-list-item--w-icon rvo-item-list-item--w-link">
+                        <div className="rvo-item-list-item-content">
+                          <div className="rvo-status-indicator rvo-status-indicator--closed"></div>
+                          <div className="rvo-item-list-item-title">
+                            <a href="#" className="rvo-link--full-click">
+                              ISDE
+                            </a>
+                          </div>
+                          <div className="rvo-item-list-item-description">Gesloten voor aanvragen</div>
+                        </div>
+                        <div className="rvo-item-list-icon">
+                          <Icon icon="delta-naar-rechts" size="sm" />
+                        </div>
+                      </div>
+                      <div className="rvo-item-list-item rvo-item-list-item--w-icon rvo-item-list-item--w-link">
+                        <div className="rvo-item-list-item-content">
+                          <div className="rvo-status-indicator rvo-status-indicator--tmp-closed"></div>
+                          <div className="rvo-item-list-item-title">
+                            <a href="#" className="rvo-link--full-click">
+                              Gecombineerde Opgave
+                            </a>
+                          </div>
+                          <div className="rvo-item-list-item-description">Tijdelijk gesloten voor aanvragen</div>
+                        </div>
+                        <div className="rvo-item-list-icon">
+                          <Icon icon="delta-naar-rechts" size="sm" />
+                        </div>
                       </div>
                     </div>
-                    <div className="rvo-item-list-item rvo-item-list-item--w-icon rvo-item-list-item--w-link">
-                      <div className="rvo-item-list-item-content">
-                        <div className="rvo-status-indicator rvo-status-indicator--closed"></div>
-                        <div className="rvo-item-list-item-title">
-                          <a href="#" className="rvo-link--full-click">
-                            ISDE
-                          </a>
-                        </div>
-                        <div className="rvo-item-list-item-description">Gesloten voor aanvragen</div>
-                      </div>
-                      <div className="rvo-item-list-icon">
-                        <Icon icon="delta-naar-rechts" size="sm" />
-                      </div>
-                    </div>
-                    <div className="rvo-item-list-item rvo-item-list-item--w-icon rvo-item-list-item--w-link">
-                      <div className="rvo-item-list-item-content">
-                        <div className="rvo-status-indicator rvo-status-indicator--tmp-closed"></div>
-                        <div className="rvo-item-list-item-title">
-                          <a href="#" className="rvo-link--full-click">
-                            Gecombineerde Opgave
-                          </a>
-                        </div>
-                        <div className="rvo-item-list-item-description">Tijdelijk gesloten voor aanvragen</div>
-                      </div>
-                      <div className="rvo-item-list-icon">
-                        <Icon icon="delta-naar-rechts" size="sm" />
-                      </div>
-                    </div>
-                  </div>
-                  <Link
-                    content="Zoek een andere regeling op rvo.nl"
-                    showIcon="after"
-                    icon="externe-link"
-                    target="#"
-                    noUnderline={true}
-                  />
-                </LayoutColumnRow>
+                    <Link
+                      content="Zoek een andere regeling op rvo.nl"
+                      showIcon="after"
+                      icon="externe-link"
+                      target="#"
+                      noUnderline={true}
+                    />
+                  </LayoutColumnRow>
+                </div>
+                <div className="rvo-quick-links">
+                  <Heading type="h2" textContent="Snel naar" />
+                  <ul className="rvo-quick-links-list">
+                    <li className="rvo-quick-links-list-item">
+                      <Link content="Wijzig e-mail of telefoonnummer" target="#" noUnderline={true} />
+                    </li>
+                    <li className="rvo-quick-links-list-item">
+                      <Link content="Neem contact op" target="#" noUnderline={true} />
+                    </li>
+                    <li className="rvo-quick-links-list-item">
+                      <Link content="Voeg favoriet toe" target="#" noUnderline={true} />
+                    </li>
+                    <li className="rvo-quick-links-list-item">
+                      <Link content="Machtig iemand anders" target="#" noUnderline={true} />
+                    </li>
+                    <li className="rvo-quick-links-list-item">
+                      <Link content="Meld een technisch probleem" target="#" noUnderline={true} />
+                    </li>
+                  </ul>
+                </div>
               </div>
-              <div className="rvo-quick-links">
-                <Heading type="h2" textContent="Snel naar" />
-                <ul className="rvo-quick-links-list">
-                  <li className="rvo-quick-links-list-item">
-                    <Link content="Wijzig e-mail of telefoonnummer" target="#" noUnderline={true} />
-                  </li>
-                  <li className="rvo-quick-links-list-item">
-                    <Link content="Neem contact op" target="#" noUnderline={true} />
-                  </li>
-                  <li className="rvo-quick-links-list-item">
-                    <Link content="Voeg favoriet toe" target="#" noUnderline={true} />
-                  </li>
-                  <li className="rvo-quick-links-list-item">
-                    <Link content="Machtig iemand anders" target="#" noUnderline={true} />
-                  </li>
-                  <li className="rvo-quick-links-list-item">
-                    <Link content="Meld een technisch probleem" target="#" noUnderline={true} />
-                  </li>
-                </ul>
-              </div>
+
               <div className="rvo-messages">
                 <Heading type="h2" textContent="Berichten" />
                 <div className="rvo-messages-list">
@@ -375,9 +381,96 @@ const Overzicht = () => {
                 </div>
                 <Link content="Alle berichten" showIcon="after" icon="pijl-naar-rechts" noUnderline={true} />
               </div>
+              <div className="rvo-block-group">
+                <div className="rvo-faq">
+                  <Heading type="h2" textContent="Veelgestelde vragen" />
+                  <div className="rvo-accordion--compact">
+                    <Accordion
+                      items={[
+                        {
+                          title: 'Mag ik voor deze regeling subsidies stapelen/combineren?',
+                          teaser: '',
+                          content:
+                            'Per 21 april 2021 kunt u als woningeigenaar ISDE combineren met gemeentelijke of provinciale subsidies om bijvoorbeeld uw woning aan te sluiten op een warmtenet. Dit is terug te vinden in de publicatie in de Staatscourant. Het is niet mogelijk om meer dan een keer subsidie te ontvangen vanuit de Rijksoverheid voor dezelfde maatregel.',
+                        },
+                        {
+                          title: 'Wat is een bestaande thermische schil?',
+                          teaser: '',
+                          content:
+                            'De bestaande thermische schil is de isolerende laag aan de buitenzijde van de woning. Wanden, daken, beglazing en deuren, en vloeren grenzend aan de buitenlucht of grond zijn geïsoleerd om kou te weren en warmte binnen te houden. De thermische schil is de jas van de woning.',
+                        },
+                        {
+                          title: 'Wanneer krijg ik bericht over mijn subsidie?',
+                          teaser: '',
+                          content: 'Zo snel mogelijk.',
+                        },
+                      ]}
+                    />
+                  </div>
+                  <Link
+                    content="Bekijk alle veelgestelde vragen"
+                    showIcon="after"
+                    icon="pijl-naar-rechts"
+                    noUnderline={true}
+                  />
+                </div>
+
+                <div className="rvo-contact-options">
+                  <Heading type="h2" textContent="Neem contact op" />
+                  <p className="rvo-paragraph">Staat uw vraag er niet bij? Neem dan contact met ons op.</p>
+                  <ButtonGroup>
+                    <Button kind="secondary" showIcon="after" icon="communicatie">
+                      Stel uw vraag via onze chat
+                    </Button>
+                    <Button kind="secondary">Bekijk alle contactmogelijkheden</Button>
+                    <Button kind="secondary" showIcon="after" icon="externe-link">
+                      Geef uw mening
+                    </Button>
+                  </ButtonGroup>
+                </div>
+              </div>
             </LayoutColumnRow>
           </div>
         </main>
+        <Footer
+          columns={[
+            {
+              label: 'RVO',
+              items: [
+                { content: 'Over ons', link: '#' },
+                { content: 'Contact', link: '#' },
+                { content: 'Pers', link: '#' },
+                { content: 'Zaken regelen bij RVO', link: '#' },
+              ],
+            },
+            {
+              label: 'Over deze site',
+              items: [
+                { content: 'Privacy', link: '#' },
+                { content: 'Cookies en anti-spam', link: '#' },
+                { content: 'Toegankelijkheid', link: '#' },
+                { content: 'Proclaimer', link: '#' },
+              ],
+            },
+            {
+              label: 'Snel naar',
+              items: [
+                { content: 'Onderwerpen A t/m Z', link: '#' },
+                { content: 'Klantenpanel', link: '#' },
+                { content: 'Documenten en publicaties', link: '#' },
+                { content: 'Overheidspublicaties en auteursrecht', link: '#' },
+              ],
+            },
+            {
+              label: 'Blijf op de hoogte',
+              items: [
+                { content: 'Social media', link: '#' },
+                { content: 'RSS', link: '#' },
+                { content: 'Gratis nieuwsbrief', link: '#' },
+              ],
+            },
+          ]}
+        />
       </div>
     </body>
   );
