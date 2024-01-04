e370fc58296e88873d2e9f5e727b298d65967a06
NURB <tom@tomjonkman.com>
2024-01-04 15:09:17 +0100

    fix: Lint issues

--- documentation/demopages/common/defaultMenuBarItemsMijnRVO.ts
+++ documentation/demopages/common/defaultMenuBarItemsMijnRVO.ts
@@ -1,13 +1,13 @@
 import { linkTo } from '@storybook/addon-links';
 
 export const defaultMenuBarItemsMijnRVO = [
-  { label: 'Zaken', onClick: linkTo("Demo pagina's/Mijn RVO/Mijn zaken") },
-  { label: 'Diensten', onClick: linkTo("Demo pagina's/Mijn RVO/Diensten") },
+  { label: 'Zaken', link: linkTo("Demo pagina's/Mijn RVO/Mijn zaken") },
+  { label: 'Diensten', link: linkTo("Demo pagina's/Mijn RVO/Diensten") },
   { label: 'Help', link: '#' },
-  { label: 'Profiel', onClick: linkTo("Demo pagina's/Mijn RVO/Profiel") },
+  { label: 'Profiel', link: linkTo("Demo pagina's/Mijn RVO/Profiel") },
   {
     label: 'Uitloggen',
-    onClick: linkTo("Demo pagina's/Mijn RVO/Inloggen"),
+    link: linkTo("Demo pagina's/Mijn RVO/Inloggen"),
     align: 'right' as const,
   },
   { label: 'English', icon: 'wereldbol' as const, link: '#', align: 'right' as const },
