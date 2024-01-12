5e902931511c299eab79180328ac64cf3673aa97
Robbert Broersma <git@robbertbroersma.nl>
2024-01-12 15:32:15 +0100

    refactor: prefix design tokens component with `rvo-`

--- documentation/components/design-tokens/LineHeightDesignTokens.tsx
+++ documentation/components/design-tokens/LineHeightDesignTokens.tsx
@@ -7,18 +7,21 @@ type LineHeightDesignTokensProps = {
 
 const LineHeightDesignTokens = ({ tokens }: LineHeightDesignTokensProps) => {
   return (
-    <div className="design-tokens__container" style={{ marginBlockEnd: 'var(--rvo-space-3xl)' }}>
+    <div className="rvo-design-tokens__container" style={{ marginBlockEnd: 'var(--rvo-space-3xl)' }}>
       {tokens.map(({ name, path, value }) => {
         return (
-          <div className="design-tokens__row" key={name} style={{ marginBlockStart: 'var(--rvo-space-md)' }}>
-            <div className="design-tokens__column design-tokens__column--30" style={{ lineHeight: path2css(path) }}>
+          <div className="rvo-design-tokens__row" key={name} style={{ marginBlockStart: 'var(--rvo-space-md)' }}>
+            <div
+              className="rvo-design-tokens__column rvo-design-tokens__column--30"
+              style={{ lineHeight: path2css(path) }}
+            >
               {value}
               <br />
               Voor ondernemende Nederlanders.
               <br />
               Wij helpen u graag vooruit.
             </div>
-            <code className="design-tokens__column">{path2css(path)}</code>
+            <code className="rvo-design-tokens__column">{path2css(path)}</code>
           </div>
         );
       })}
