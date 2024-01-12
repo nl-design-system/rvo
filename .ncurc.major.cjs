af2e2658ce11696c64841fa8668537df177a810f
Robbert Broersma <git@robbertbroersma.nl>
2024-01-12 10:19:22 +0100

    ci: prevent major upgrades

--- .ncurc.major.cjs
+++ .ncurc.major.cjs
@@ -3,26 +3,30 @@ const minorConfig = require("./.ncurc.minor.cjs");
 module.exports = {
   reject: [
     ...minorConfig.reject,
-    ...Object.keys({      
-      "@docusaurus/core": "2.2.0",
+    ...Object.keys({
+      // Docusaurus needs a migration
       "@docusaurus/core": "2.2.0",
       "@docusaurus/eslint-plugin": "2.2.0",
-      "@docusaurus/eslint-plugin": "2.2.0",
-      "@docusaurus/module-type-aliases": "2.2.0",
       "@docusaurus/module-type-aliases": "2.2.0",
       "@docusaurus/preset-classic": "2.2.0",
-      "@docusaurus/preset-classic": "2.2.0",
-      "@docusaurus/theme-classic": "2.2.0",
       "@docusaurus/theme-classic": "2.2.0",
       "@docusaurus/theme-common": "2.2.0",
-      "@docusaurus/theme-common": "2.2.0",
-      "@docusaurus/types": "2.2.0",
       "@docusaurus/types": "2.2.0",
       "@mdx-js/react": "1.6.22",
-      "@mdx-js/react": "1.6.22",
-    "@tsconfig/docusaurus": "1.0.7",
-      "nx": "14.5.10",
-    })
+      "@tsconfig/docusaurus": "1.0.7",
+      "prism-react-renderer": "1.3.5",
+
+      // Nx needs a migration
+      "@nrwl/cli": "15.9.3",
+      "@nrwl/nx-cloud": "16.5.2",
+      "@nrwl/web": "17.2.8",
+      "@nrwl/workspace": "17.2.8",
+      nx: "14.5.10",
+
+      // Prettier 3 is async, needs a refactoring
+      prettier: "3.1.1",
+    }),
   ],
 };
-console.log(module.exports)
+
+console.log(`Reject major updates of:\n\n${module.exports.reject.join("\n")}`);
