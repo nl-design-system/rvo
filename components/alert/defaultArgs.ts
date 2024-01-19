f4fa5ed417e7bc8a7479a1db51147d9e791a4d2f
Robert Roose <103183050+rroose-rvo@users.noreply.github.com>
2024-01-19 13:58:43 +0100

    Fix/wcag (#608)
    * Refactor item list from div to ul
    
    * Add alt text to hero image
    
    * Remove thin and light font weights
    
    * Fix wcag issue with link in alert
    
    * Fix menu issues

--- components/alert/defaultArgs.ts
+++ components/alert/defaultArgs.ts
@@ -3,7 +3,7 @@ import { IAlertProps } from './template';
 export const defaultArgs: IAlertProps = {
   kind: 'info',
   heading: '',
-  content: `This is an example of an alert, with a <a href="#" class="rvo-link rvo-link--donkerblauw">link</a> inside.`,
+  content: `This is an example of an alert, with a <a href="#" class="rvo-link rvo-link--logoblauw">link</a> inside.`,
   closable: false,
   padding: 'md',
 };
