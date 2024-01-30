8a28545ac0de97aaa00a63239bf598ac15c0446f
Robbert Broersma <git@robbertbroersma.nl>
2024-01-30 17:51:17 +0100

    refactor: workaround, make build of SVG files work for uxpin webpack

--- components/favicon/template.tsx
+++ components/favicon/template.tsx
@@ -2,12 +2,11 @@
  * @license EUPL-1.2
  * Copyright (c) 2021 Community for NL Design System
  */
-import FavIconSrc from '@nl-rvo/assets/images/favicon/icon.svg';
 import clsx from 'clsx';
 
 export const Favicon = (props) => {
   const className = props && props.className ? props.className : '';
-  return <img src={FavIconSrc} className={clsx('rvo-favicon', className)} />;
+  return <img src="images/favicon/icon.svg" className={clsx('rvo-favicon', className)} />;
 };
 
 export default Favicon;
