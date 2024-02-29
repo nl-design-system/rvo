aa6ba6fba0943c7755de019159fd97e027586498
Robbert Broersma <git@robbertbroersma.nl>
2024-02-29 14:42:46 +0100

    fix: use <svg> instead of <img> for header logo

--- components/header/template.tsx
+++ components/header/template.tsx
@@ -5,7 +5,7 @@
 import clsx from 'clsx';
 import React, { PropsWithChildren } from 'react';
 import { defaultArgs } from './defaultArgs';
-import { Logo } from '../logo/template';
+import { Logo } from '../../packages/components-react/src/Logo';
 import './index.scss';
 
 export interface IHeaderProps extends PropsWithChildren {
