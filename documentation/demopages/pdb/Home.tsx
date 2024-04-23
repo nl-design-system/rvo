603454c57c66c60f4a6b1a074fc49428bb5da1e9
Robert Roose <robert.roose@rvo.nl>
2024-04-23 13:39:07 +0200

    Add homepage content

--- documentation/demopages/pdb/Home.tsx
+++ documentation/demopages/pdb/Home.tsx
@@ -1,9 +1,9 @@
-import { Header, MenuBar } from '@nl-rvo/components';
+import { Header, Heading, MenuBar } from '@nl-rvo/components';
 
 const MijnDossier = () => {
   return (
     <body className="rvo-theme">
-      <Header />
+      <Header link="#" />
       <MenuBar
         items={[
           {
@@ -32,6 +32,37 @@ const MijnDossier = () => {
         iconPlacement="before"
         maxWidth="md"
       />
+      <main className="rvo-max-width-layout rvo-max-width-layout--sm">
+        <Heading noMargins={true}>Projecten Database</Heading>
+        <p className="rvo-text rvo-text--no-margins">
+          The{' '}
+          <a href="https://english.rvo.nl" className="rvo-link">
+            Netherlands Enterprise Agency (RVO)
+          </a>{' '}
+          administers financial support to projects in the form of subsidies and programmes, on behalf of various Dutch
+          ministries and the European Union.
+        </p>
+        <p className="rvo-text">
+          This website shows the supported projects, starting with Development Cooperation. Adding projects to this
+          website is an ongoing process.
+        </p>
+        <p className="rvo-text"> Watch the latest videos about RVO International Development:</p>
+        <ul className="rvo-ul rvo-ul--no-margin rvo-ul--no-padding rvo-ul--icon rvo-ul--icon-option-1">
+          {' '}
+          <li>
+            <a href="https://youtu.be/H6h0M0PHoSc?" className="rvo-link">
+              Building a sustainable future together
+            </a>{' '}
+            (in English)
+          </li>
+          <li>
+            <a href="https://youtu.be/tW9bFLBReJ8?" className="rvo-link">
+              Samen bouwen aan een duurzame toekomst
+            </a>{' '}
+            (in Dutch)
+          </li>
+        </ul>
+      </main>
     </body>
   );
 };
