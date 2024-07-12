cd96fb66bf375d0a5f9c9a557d292253cf12c958
Robert Roose <103183050+rroose-rvo@users.noreply.github.com>
2024-07-12 16:38:28 +0200

    Relatienummer demopagina (#755)

---
+++ documentation/demopages/algemeen/Relatienummer.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Relatienummer from './Relatienummer';
+
+const meta: Meta<typeof Relatienummer> = {
+  title: "Demo pagina's/Algemeen/Relatienummer",
+  component: Relatienummer,
+  parameters: {
+    status: {
+      type: 'WORK IN PROGRESS',
+    },
+  },
+};
+export default meta;
+
+type Story = StoryObj<typeof Relatienummer>;
+
+export const Default: Story = { name: 'Relatienummer' };
