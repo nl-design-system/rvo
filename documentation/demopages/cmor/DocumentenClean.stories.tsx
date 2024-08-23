16d090243282331b371db8698bc9b2fd30e2a1b4
Robert Roose <103183050+rroose-rvo@users.noreply.github.com>
2024-08-23 15:26:53 +0200

    Add Documenten demopage (#768)

---
+++ documentation/demopages/cmor/DocumentenClean.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import DocumentenClean from './DocumentenClean';
+
+const meta: Meta<typeof DocumentenClean> = {
+  title: "Demo pagina's/CMOR/Documenten",
+  component: DocumentenClean,
+  parameters: {
+    status: {
+      type: 'WORK IN PROGRESS',
+    },
+  },
+};
+export default meta;
+
+type Story = StoryObj<typeof DocumentenClean>;
+
+export const Default: Story = { name: 'Documenten' };
