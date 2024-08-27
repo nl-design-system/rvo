3789986cf3977fda87ad37a036453b6915593736
Robert Roose <103183050+rroose-rvo@users.noreply.github.com>
2024-08-27 15:59:17 +0200

    Cleanfilter (#772)
    * Fix filename
    
    * Fix 1fr2fr in grid css
    
    * Fix filename
    
    * Add Clean version of the Vertical Filter
    
    * Remove layoutcolumn in filter form

---
+++ documentation/demopages/filter/FilterVerticalClean.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import VerticalFilterClean from './VerticalFilterClean';
+
+const meta: Meta<typeof VerticalFilterClean> = {
+  title: "Demo pagina's/Filter/Vertical filter clean",
+  component: VerticalFilterClean,
+  parameters: {
+    status: {
+      type: 'WORK IN PROGRESS',
+    },
+  },
+};
+export default meta;
+
+type Story = StoryObj<typeof VerticalFilterClean>;
+
+export const Default: Story = { name: 'Vertical filter clean' };
