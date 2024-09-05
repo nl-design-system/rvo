dc092d0238eb28395c0a2bfaee6ec30596b955e5
Robert Roose <robert.roose@rvo.nl>
2024-09-05 16:58:44 +0200

    Put algemeen demopages in correct folder

---
+++ documentation/demopages/experimenteel/algemeen/ExpandableArea.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import ExpandableArea from './ExpandableArea';
+
+const meta: Meta<typeof ExpandableArea> = {
+  title: "Pagina's/Experimenteel/Algemeen/ExpandableArea",
+  component: ExpandableArea,
+  parameters: {
+    status: {
+      type: 'WORK IN PROGRESS',
+    },
+  },
+};
+export default meta;
+
+type Story = StoryObj<typeof ExpandableArea>;
+
+export const Default: Story = { name: 'ExpandableArea' };
---
+++ documentation/demopages/experimenteel/algemeen/SubmenuGrid.tsx
@@ -0,0 +1,53 @@
+import { Header, LayoutColumnRow, MenuBar } from '@nl-rvo/components';
+import '../../common/style.scss';
+
+const SubmenuGrid = () => {
+  return (
+    <div className="rvo-demo-page rvo-demo-page--sub-menu">
+      <Header />
+      <LayoutColumnRow size="md">
+        <div className="rvo-navigation">
+          <MenuBar
+            items={[
+              { label: 'Home', icon: 'home', link: '#' },
+              { label: 'Expanded menu item', icon: 'delta-omlaag', link: '#' },
+              { label: 'Uitloggen', icon: 'versleutelen', link: '#', align: 'right' },
+            ]}
+            size="md"
+            useIcons={false}
+            maxWidth="md"
+          />
+          <div className="rvo-tmp-submenu-wrapper rvo-tmp-submenu-wrapper--grid">
+            <MenuBar
+              items={[
+                { label: 'Submenu item', icon: 'plus', link: '#' },
+                { label: 'Submenu item', icon: 'plus', link: '#' },
+                { label: 'Submenu item', icon: 'plus', link: '#', align: 'right' },
+                { label: 'Submenu item', icon: 'plus', link: '#' },
+                { label: 'Submenu item', icon: 'plus', link: '#' },
+                { label: 'Submenu item', icon: 'plus', link: '#' },
+                { label: 'Submenu item', icon: 'plus', link: '#' },
+                { label: 'Submenu item', icon: 'plus', link: '#' },
+              ]}
+              size="md"
+              useIcons={true}
+              iconPlacement="before"
+              maxWidth="md"
+            />
+          </div>
+        </div>
+        <main className="rvo-max-width-layout rvo-max-width-layout--sm">
+          <div className="rvo-content">
+            <h1 className="utrecht-heading-1">Default submenu</h1>
+            <p className="rvo-paragraph rvo-paragraph--md">
+              A submenu is expanded when a user clicks on an expandable menu item which label is followed by a downwards
+              chevron. The submenu expands in place not overlapping existing content.
+            </p>
+          </div>
+        </main>
+      </LayoutColumnRow>
+    </div>
+  );
+};
+
+export default SubmenuGrid;
