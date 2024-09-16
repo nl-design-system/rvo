8bfe881510f502064d573ed02207762607cff976
rroose-rvo <103183050+rroose-rvo@users.noreply.github.com>
2024-09-16 11:39:45 +0000

    Deploying to gh-pages from @ nl-design-system/rvo@714c3987afe6e24d151ebb4f0e563a367172cac9 🚀

---
+++ defaultFooterItems.ts
@@ -0,0 +1,36 @@
+export const defaultFooterItems = [
+  {
+    label: 'Over deze site',
+    items: [
+      { content: 'Privacy', link: '#' },
+      { content: 'Cookies en anti-spam', link: '#' },
+      { content: 'Toegankelijkheid', link: '#' },
+      { content: 'Proclaimer', link: '#' },
+    ],
+  },
+  {
+    label: 'Optioneel menu',
+    items: [
+      { content: 'Menu item 1', link: '#' },
+      { content: 'Menu item 2', link: '#' },
+      { content: 'Menu item 3', link: '#' },
+    ],
+  },
+
+  {
+    label: 'Optioneel menu',
+    items: [
+      { content: 'Menu item 1', link: '#' },
+      { content: 'Menu item 2', link: '#' },
+      { content: 'Menu item 3', link: '#' },
+    ],
+  },
+  {
+    label: 'Optioneel menu',
+    items: [
+      { content: 'Menu item 1', link: '#' },
+      { content: 'Menu item 2', link: '#' },
+      { content: 'Menu item 3', link: '#' },
+    ],
+  },
+];
