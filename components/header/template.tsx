a45ce6a015ead821544c68c296b4ca2eb5d7b618
sushitommy <>
2024-09-23 12:16:44 +0200

    Add onClick to header

--- components/header/template.tsx
+++ components/header/template.tsx
@@ -11,8 +11,10 @@ import './index.scss';
 export interface IHeaderProps extends PropsWithChildren {
   title?: string;
   subtitle?: string;
+  /** @uxpinignoreprop */
   link?: string;
   children?: ReactNode | undefined;
+  onClick?: (event) => void;
 }
 
 export const argTypes = {
@@ -30,6 +32,11 @@ export const argTypes = {
       disable: true,
     },
   },
+  onClick: {
+    table: {
+      disable: true,
+    },
+  },
 };
 
 export const Header: React.FC<IHeaderProps> = ({
@@ -37,9 +44,10 @@ export const Header: React.FC<IHeaderProps> = ({
   title = defaultArgs.title,
   subtitle = defaultArgs.subtitle,
   children,
+  onClick,
 }: IHeaderProps) => (
   <header className={clsx('rvo-header')}>
-    <div className="rvo-header__logo-wrapper">
+    <div className="rvo-header__logo-wrapper" onClick={onClick}>
       {link ? (
         <a href={link} className="rvo-header__logo-link rvo-link rvo-link--no-underline">
           <Logo className="rvo-header__logo-img" title={title} subtitle={subtitle} />
