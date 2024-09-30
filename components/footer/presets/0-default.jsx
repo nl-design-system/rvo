ee086fe14c8e3c8b9917060e5664ecf4ff97f610
sushitommy <>
2024-09-30 13:52:49 +0200

    Add proper Footer preset

--- components/footer/presets/0-default.jsx
+++ components/footer/presets/0-default.jsx
@@ -1,4 +1,65 @@
 import { defaultArgs } from '../defaultArgs';
+import FooterColumn from '../footer-column/template';
+import FooterItem from '../footer-item/template';
 import Footer from '../template';
 
-export default <Footer uxpId="footer" {...defaultArgs} />;
+export default (
+  <Footer uxpId="footer" {...defaultArgs}>
+    <FooterColumn uxpId="footerColumn0" label="RVO">
+      <FooterItem uxpId="footerItem0" link="#">
+        Over ons
+      </FooterItem>
+      <FooterItem uxpId="footerItem1" link="#">
+        Contact
+      </FooterItem>
+      <FooterItem uxpId="footerItem2" link="#">
+        Pers
+      </FooterItem>
+      <FooterItem uxpId="footerItem3" link="#">
+        Zaken regelen bij RVO
+      </FooterItem>
+    </FooterColumn>
+    <FooterColumn uxpId="footerColumn1" label="Over deze site">
+      <FooterItem uxpId="footerItem4" link="#">
+        Privacy
+      </FooterItem>
+      <FooterItem uxpId="footerItem5" link="#">
+        Cookies en anti-spam
+      </FooterItem>
+      <FooterItem uxpId="footerItem6" link="#">
+        Toegankelijkheid
+      </FooterItem>
+      <FooterItem uxpId="footerItem7" link="#">
+        Proclaimer
+      </FooterItem>
+    </FooterColumn>
+    <FooterColumn uxpId="footerColumn2" label="Snel naar">
+      <FooterItem uxpId="footerItem8" link="#">
+        Onderwerpen A t/m Z
+      </FooterItem>
+      <FooterItem uxpId="footerItem9" link="#">
+        Klantenpanel
+      </FooterItem>
+      <FooterItem uxpId="footerItem10" link="#">
+        Documenten en publicaties
+      </FooterItem>
+      <FooterItem uxpId="footerItem11" link="#">
+        Overheidspublicaties en auteursrecht
+      </FooterItem>
+    </FooterColumn>
+    <FooterColumn uxpId="footerColumn3" label="Blijf op de hoogte">
+      <FooterItem uxpId="footerItem12" link="#">
+        Social media
+      </FooterItem>
+      <FooterItem uxpId="footerItem13" link="#">
+        Higherlevel.nl ondernemersforum
+      </FooterItem>
+      <FooterItem uxpId="footerItem14" link="#">
+        Toegankelijkheid
+      </FooterItem>
+      <FooterItem uxpId="footerItem14" link="#">
+        Proclaimer
+      </FooterItem>
+    </FooterColumn>
+  </Footer>
+);
