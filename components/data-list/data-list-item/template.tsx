dffd6f3b892f1ac582874340157d72274313107d
sushitommy <>
2024-10-17 17:24:56 +0200

    Add support for React nodes in data list item

--- components/data-list/data-list-item/template.tsx
+++ components/data-list/data-list-item/template.tsx
@@ -2,21 +2,21 @@
  * @license EUPL-1.2
  * Copyright (c) 2021 Community for NL Design System
  */
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { defaultArgs } from './defaultArgs';
 import parseContentMarkup from '../../utils/parseContentMarkup';
 
 export interface IDataListItemProps {
-  label?: string;
-  value?: string;
+  label: string | ReactNode;
+  value: string | ReactNode;
 }
 
 export const DataListItem: React.FC<IDataListItemProps> = ({
   label = defaultArgs.label,
   value = defaultArgs.value,
 }: IDataListItemProps) => {
-  let dtMarkup: string | React.ReactNode = parseContentMarkup(label);
-  let ddMarkup: string | React.ReactNode = parseContentMarkup(value);
+  let dtMarkup: string | React.ReactNode = label && parseContentMarkup(label);
+  let ddMarkup: string | React.ReactNode = value && parseContentMarkup(value);
 
   return (
     <React.Fragment>
--- components/data-list/template.tsx
+++ components/data-list/template.tsx
@@ -4,18 +4,13 @@
  */
 import clsx from 'clsx';
 import React, { HTMLAttributes, ReactNode } from 'react';
-import DataListItem from './data-list-item/template';
+import DataListItem, { IDataListItemProps } from './data-list-item/template';
 import { defaultArgs } from './defaultArgs';
 import './index.scss';
 
-interface IDataListItem {
-  label: string;
-  value: string;
-}
-
 export interface IDataListProps extends HTMLAttributes<HTMLDListElement> {
   /** @uxpinignoreprop */
-  items?: IDataListItem[];
+  items?: IDataListItemProps[];
   /** @uxpinpropname Data list items */
   children?: ReactNode | undefined;
 }
@@ -35,10 +30,7 @@ export const DataList: React.FC<IDataListProps> = ({
   ...props
 }: IDataListProps) => (
   <dl className={clsx('rvo-data-list')} {...props}>
-    {children ||
-      items?.map((itemContent, index) => (
-        <DataListItem key={index} label={itemContent.label} value={itemContent.value} />
-      ))}
+    {children || items?.map((itemContent, index) => <DataListItem key={index} {...itemContent} />)}
   </dl>
 );
 
