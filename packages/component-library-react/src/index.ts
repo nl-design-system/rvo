25b35ad03ecabfba573a595d93f4b22e4edc4cc5
sushitommy <>
2024-10-17 13:21:59 +0200

    Include utrecht css for now in component-library-react

--- packages/component-library-react/src/index.ts
+++ packages/component-library-react/src/index.ts
@@ -3,6 +3,9 @@
  * Copyright (c) 2021 Community for NL Design System
  */
 
+// Import Utrecht CSS
+import '@utrecht/component-library-css/dist/index.css';
+
 // Components by RVO (included wrapped components from NL Design System community)
 export * from '../../../components';
 
