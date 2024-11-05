5765a0671769e542e4e143403c0486bc984b1eb7
sushitommy <>
2024-11-05 14:36:00 +0100

    Fix landing page paragraphs

--- packages/docusaurus/src/pages/index.tsx
+++ packages/docusaurus/src/pages/index.tsx
@@ -30,28 +30,28 @@ export default function Home(): JSX.Element {
               <Heading type="h2" noMargins={true}>
                 Huisstijl
               </Heading>
-              <p>De kleuren, lettertypes, de space en sizes die er beschikbaar zijn binnen ROOS</p>
+              De kleuren, lettertypes, de space en sizes die er beschikbaar zijn binnen ROOS.
               <Link href={useBaseUrl('/design-tokens/kleur')}>Bekijk de huisstijl</Link>
             </div>
             <div className={clsx('rvo-layout-column', 'rvo-layout-gap--sm')}>
               <Heading type="h2" noMargins={true}>
                 Componenten
               </Heading>
-              <p>Een collectie aan gebruiksvriendelijke, toegankelijke componenten die voldoen aan de Rijkshuisstijl</p>
+              Een collectie aan gebruiksvriendelijke, toegankelijke componenten die voldoen aan de Rijkshuisstijl.
               <Link href={useBaseUrl('/componenten/accordion')}>Bekijk de componenten</Link>
             </div>
             <div className={clsx('rvo-layout-column', 'rvo-layout-gap--sm')}>
               <Heading type="h2" noMargins={true}>
                 Patronen
               </Heading>
-              <p>Een set van componenten die samen een patroon vormen van acties die een gebruiker moet uitvoeren</p>
+              Een set van componenten die samen een patroon vormen van acties die een gebruiker moet uitvoeren
               <Link href={useBaseUrl('/patronen/layout/applicatie-layout')}>Bekijk de patronen</Link>
             </div>
             <div className={clsx('rvo-layout-column', 'rvo-layout-gap--sm')}>
               <Heading type="h2" noMargins={true}>
                 Voor designers
               </Heading>
-              <p>Ben je een designer en wil je ROOS gebruiken? Gebruik dan ons Figma bestand</p>
+              Ben je een designer en wil je ROOS gebruiken? Gebruik dan ons Figma bestand.
               <Link
                 href="https://www.figma.com/file/Sj6myBL1Fvot5M1qGxzvEo/ROOS-(RVO-Design-System)?type=design&node-id=484-13305&t=vZTjQBvAiufxd5Qs-0"
                 showIcon="after"
@@ -64,7 +64,7 @@ export default function Home(): JSX.Element {
               <Heading type="h2" noMargins={true}>
                 Voor developers
               </Heading>
-              <p>Ben je een developer en wil je ROOS gebruiken? Ga dan naar onze Storybook omgeving</p>
+              Ben je een developer en wil je ROOS gebruiken? Ga dan naar onze Storybook omgeving.
               <Link
                 href={
                   process.env.NODE_ENV === 'development'
@@ -81,10 +81,8 @@ export default function Home(): JSX.Element {
               <Heading type="h2" noMargins={true}>
                 Hulp nodig?
               </Heading>
-              <p>
-                Kom je er niet uit? We vinden het altijd leuk als iemand ons Design System wil gebruiken, dus stuur ons
-                een e-mail
-              </p>
+              Kom je er niet uit? We vinden het altijd leuk als iemand ons Design System wil gebruiken, dus stuur ons
+              een e-mail.
               <Link href="mailto:designsystem@rvo.nl" showIcon="after" icon="externe-link">
                 designsystem@rvo.nl
               </Link>
