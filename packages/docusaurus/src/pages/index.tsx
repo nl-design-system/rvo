625ddb693c1f2f0d8bfae9a953cf43888e4dd53b
sushitommy <>
2024-11-05 17:09:38 +0100

    Use Card components for landing page links

--- packages/docusaurus/src/pages/index.tsx
+++ packages/docusaurus/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import { Button, Heading, Link, MaxWidthLayout } from '@nl-rvo/components';
+import { Button, Card, Heading, Icon, LayoutFlow, Link, MaxWidthLayout } from '@nl-rvo/components';
 import Layout from '@theme/Layout';
 import clsx from 'clsx';
 import styles from './index.module.css';
@@ -26,67 +26,84 @@ export default function Home(): JSX.Element {
       <main>
         <MaxWidthLayout size="lg" className={clsx('rvo-layout-column', 'rvo-layout-gap--md', styles.hero)}>
           <section className={styles.articleGrid}>
-            <div className={clsx('rvo-layout-column', 'rvo-layout-gap--sm')}>
-              <Heading type="h2" noMargins={true}>
-                Huisstijl
-              </Heading>
+            <Card
+              title="Huisstijl"
+              background="none"
+              link={useBaseUrl('/design-tokens/kleuren')}
+              outline={true}
+              fullCardLink={true}
+            >
               De kleuren, lettertypes, de space en sizes die er beschikbaar zijn binnen ROOS.
-              <Link href={useBaseUrl('/design-tokens/kleuren')}>Bekijk de huisstijl</Link>
-            </div>
-            <div className={clsx('rvo-layout-column', 'rvo-layout-gap--sm')}>
-              <Heading type="h2" noMargins={true}>
-                Componenten
-              </Heading>
+            </Card>
+            <Card
+              title="Componenten"
+              background="none"
+              link={useBaseUrl('/componenten/accordion')}
+              outline={true}
+              fullCardLink={true}
+            >
               Een collectie aan gebruiksvriendelijke, toegankelijke componenten die voldoen aan de Rijkshuisstijl.
-              <Link href={useBaseUrl('/componenten/accordion')}>Bekijk de componenten</Link>
-            </div>
-            <div className={clsx('rvo-layout-column', 'rvo-layout-gap--sm')}>
-              <Heading type="h2" noMargins={true}>
-                Patronen
-              </Heading>
-              Een set van componenten die samen een patroon vormen van acties die een gebruiker moet uitvoeren
-              <Link href={useBaseUrl('/patronen/layout/applicatie-layout')}>Bekijk de patronen</Link>
-            </div>
-            <div className={clsx('rvo-layout-column', 'rvo-layout-gap--sm')}>
-              <Heading type="h2" noMargins={true}>
-                Voor designers
-              </Heading>
+            </Card>
+            <Card
+              title="Patronen"
+              background="none"
+              link={useBaseUrl('/patronen/layout/applicatie-layout')}
+              outline={true}
+              fullCardLink={true}
+            >
+              Een set van componenten die samen een patroon vormen van acties die een gebruiker moet uitvoeren.
+            </Card>
+
+            <Card
+              background="none"
+              title={
+                <LayoutFlow row={true} gap="sm">
+                  Voor designers <Icon icon="externe-link" />
+                </LayoutFlow>
+              }
+              link="https://www.figma.com/file/Sj6myBL1Fvot5M1qGxzvEo/ROOS-(RVO-Design-System)?type=design&node-id=484-13305&t=vZTjQBvAiufxd5Qs-0"
+              outline={true}
+              fullCardLink={true}
+            >
               Ben je een designer en wil je ROOS gebruiken? Gebruik dan ons Figma bestand.
-              <Link
-                href="https://www.figma.com/file/Sj6myBL1Fvot5M1qGxzvEo/ROOS-(RVO-Design-System)?type=design&node-id=484-13305&t=vZTjQBvAiufxd5Qs-0"
-                showIcon="after"
-                icon="externe-link"
-              >
-                Figma bestand
-              </Link>
-            </div>
-            <div className={clsx('rvo-layout-column', 'rvo-layout-gap--sm')}>
-              <Heading type="h2" noMargins={true}>
-                Voor developers
-              </Heading>
+            </Card>
+
+            <Card
+              background="none"
+              title={
+                <LayoutFlow row={true} gap="sm">
+                  Voor developers <Icon icon="externe-link" />
+                </LayoutFlow>
+              }
+              link={
+                process.env.NODE_ENV === 'development'
+                  ? 'http://localhost:6006'
+                  : 'https://nl-design-system.github.io/rvo/'
+              }
+              outline={true}
+              fullCardLink={true}
+            >
               Ben je een developer en wil je ROOS gebruiken? Ga dan naar onze Storybook omgeving.
-              <Link
-                href={
-                  process.env.NODE_ENV === 'development'
-                    ? 'http://localhost:6006'
-                    : 'https://nl-design-system.github.io/rvo/'
-                }
-                showIcon="after"
-                icon="externe-link"
-              >
-                Storybook omgeving
-              </Link>
-            </div>
-            <div className={clsx('rvo-layout-column', 'rvo-layout-gap--sm')}>
-              <Heading type="h2" noMargins={true}>
-                Hulp nodig?
-              </Heading>
+            </Card>
+            <Card
+              background="none"
+              title={
+                <LayoutFlow row={true} gap="sm">
+                  Hulp nodig? <Icon icon="externe-link" />
+                </LayoutFlow>
+              }
+              link={
+                process.env.NODE_ENV === 'development'
+                  ? 'http://localhost:6006'
+                  : 'https://nl-design-system.github.io/rvo/'
+              }
+              outline={true}
+              fullCardLink={true}
+            >
               Kom je er niet uit? We vinden het altijd leuk als iemand ons Design System wil gebruiken, dus stuur ons
-              een e-mail.
-              <Link href="mailto:designsystem@rvo.nl" showIcon="after" icon="externe-link">
-                designsystem@rvo.nl
-              </Link>
-            </div>
+              een e-mail naar&nbsp;
+              <Link href="mailto:designsystem@rvo.nl">designsystem@rvo.nl</Link>.
+            </Card>
           </section>
         </MaxWidthLayout>
       </main>
