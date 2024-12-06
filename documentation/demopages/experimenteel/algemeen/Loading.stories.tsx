83fe5c6b4eb175b8cb7a89acc60f3e826743951e
Robert Roose <robert.roose@rvo.nl>
2024-12-06 15:44:29 +0100

    Add loading demopage

---
+++ documentation/demopages/experimenteel/algemeen/Loading.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Loading from './Loading';
+
+const meta: Meta<typeof Loading> = {
+  title: "Pagina's/Experimenteel/Algemeen/Loading",
+  component: Loading,
+  parameters: {
+    status: {
+      type: 'WORK IN PROGRESS',
+    },
+  },
+};
+export default meta;
+
+type Story = StoryObj<typeof Loading>;
+
+export const Default: Story = { name: 'Loading' };
