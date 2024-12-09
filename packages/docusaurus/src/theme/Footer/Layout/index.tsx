1e87e4377d275c0d56b121731d470b5c2a2969d6
Tom Jonkman <tom@tomjonkman.com>
2024-12-09 14:07:51 +0100

    Remove double payOff in docusaurus footer (#943)
    Co-authored-by: sushitommy <>

--- packages/docusaurus/src/theme/Footer/Layout/index.tsx
+++ packages/docusaurus/src/theme/Footer/Layout/index.tsx
@@ -1,11 +1,5 @@
-import { Footer, FooterColumn } from '@nl-rvo/components';
+import { Footer } from '@nl-rvo/components';
 
 export default function FooterLayout(): JSX.Element {
-  return (
-    <Footer maxWidth="md">
-      <FooterColumn>
-        <em style={{ fontWeight: 'var(--rvo-font-weight-normal)' }}>Wij helpen u graag vooruit!</em>
-      </FooterColumn>
-    </Footer>
-  );
+  return <Footer maxWidth="md" />;
 }
