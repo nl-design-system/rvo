069e241e17f83979d9627ee76a238fde1f7eea72
Robert Roose <robert.roose@rvo.nl>
2025-01-21 11:15:58 +0100

    Zaak documenten demopage

---
+++ documentation/demopages/voorbeelden/cmor/DocumentenZaak.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import DocumentenZaak from './DocumentenZaak';
+
+const meta: Meta<typeof DocumentenZaak> = {
+  title: "Pagina's/Voorbeelden/CMOR/Zaak documenten",
+  component: DocumentenZaak,
+  parameters: {
+    status: {
+      type: 'WORK IN PROGRESS',
+    },
+  },
+};
+export default meta;
+
+type Story = StoryObj<typeof DocumentenZaak>;
+
+export const Default: Story = { name: 'Zaak documenten' };
