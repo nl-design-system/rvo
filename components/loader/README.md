20bdf3b91c0d6471871abb0afd1650e90144599a
Patrick Lassche <p.lassche@dictu.nl>
2025-02-10 10:31:17 +0100

    New Component Loader

---
+++ components/loader/README.md
@@ -0,0 +1,3 @@
+Het doel van de Loader om tijdelijk een laadscherm te tonen bij het laden van informatie vanuit een api call of verwerken van een request.
+Met behulp van de `overlay` parameter kun je de loader over een geheel element zetten, mits je een `position: relative;` toepast over het element waar in moet vallen.
+De primary en secondary actions zijn beide optioneel, maar maken gebruik van de interface van een Button component.
---
+++ components/loader/loader.stories.tsx
@@ -0,0 +1,84 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { ILoaderStatus, Loader } from './src/template';
+import { IButtonProps } from '../button/src/template';
+
+const argTypes = {
+  animateLoader: {
+    control: { type: 'boolean' },
+  },
+  overlay: {
+    control: { type: 'boolean' },
+  },
+  status: {
+    control: { type: 'object' },
+  },
+  primaryAction: {
+    control: { label: 'object' },
+  },
+  secondaryAction: {
+    control: { label: 'object' },
+  },
+};
+
+// Arguments
+const defaultArgs = {
+  animateLoader: true,
+  overlay: false,
+};
+
+const status: ILoaderStatus = {
+  type: 'waarschuwing',
+  text: 'Het laden duurt iets langer dan verwacht',
+};
+
+const primaryAction: IButtonProps = {
+  label: 'Probeer het opnieuw',
+};
+
+const secondaryAction: IButtonProps = {
+  label: 'Probleem melden',
+};
+
+const meta: Meta<typeof Loader> = {
+  title: 'Componenten/Loader',
+  component: Loader,
+  args: defaultArgs,
+  argTypes,
+  parameters: {
+    status: {
+      type: 'PRODUCTION',
+    },
+    docusaurus: {
+      link: 'loader',
+    },
+  },
+};
+export default meta;
+
+// Stories
+type Story = StoryObj<typeof Loader>;
+
+export const Default: Story = {
+  args: defaultArgs,
+  name: 'Loader',
+};
+
+export const LoaderWithStatus: Story = {
+  args: { status, ...defaultArgs },
+  name: 'Loader Status',
+};
+
+export const LoaderOverlay: Story = {
+  args: { overlay: true, ...defaultArgs },
+  name: 'Loader Overlay',
+};
+
+export const LoaderWithActions: Story = {
+  args: { primaryAction, secondaryAction, ...defaultArgs },
+  name: 'Loader Actions',
+};
+
+export const LoaderWithStatusandActions: Story = {
+  args: { status, primaryAction, secondaryAction, ...defaultArgs },
+  name: 'Loader Status and Actions',
+};
