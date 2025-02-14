fff23ab0f190ea4c099542482b801a955eb00515
Robert Roose <robert.roose@rvo.nl>
2025-02-14 10:02:07 +0100

    Add expandable table row demopage

---
+++ documentation/demopages/experimenteel/algemeen/ExpendableRows.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import ExpendableRows from './ExpendableRows';
+
+const meta: Meta<typeof ExpendableRows> = {
+  title: "Pagina's/Experimenteel/Algemeen/Expendable Rows",
+  component: ExpendableRows,
+  parameters: {
+    status: {
+      type: 'WORK IN PROGRESS',
+    },
+  },
+};
+export default meta;
+
+type Story = StoryObj<typeof ExpendableRows>;
+
+export const Default: Story = { name: 'Expendable Rows' };
