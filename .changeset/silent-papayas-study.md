2759ed6ce9f92539940253e4d3ed8185aa5c9ded
PLassche-dictu <p.lassche@dictu.nl>
2025-02-28 08:01:34 +0100

    Checkbox Group returns Id in currentSelection (#1078)

---
+++ .changeset/silent-papayas-study.md
@@ -0,0 +1,7 @@
+---
+"@nl-rvo/css-form-checkbox-group": patch
+"@nl-rvo/checkbox-filter": patch
+"@nl-rvo/component-library-react": patch
+---
+
+Checkbox Group returns Id in currentSelection
--- components/form-checkbox-group/src/template.tsx
+++ components/form-checkbox-group/src/template.tsx
@@ -16,7 +16,7 @@ export interface ICheckboxGroupProps {
   /**
    * @uxpinbind onChange 0
    */
-  currentSelection?: number[];
+  currentSelection?: string[];
   /** @uxpinpropname Checkboxes */
   children?: ReactNode | undefined;
 }
@@ -51,9 +51,9 @@ export const CheckboxGroup: React.FC<ICheckboxGroupProps> = ({
   const onUpdateGroup = useCallback(() => {
     if (checkboxGroupRef.current) {
       const allCheckboxElements = Array.from(checkboxGroupRef.current.getElementsByTagName('input'));
-      const currentGroupSelection = allCheckboxElements.reduce((currentGroupSelection, checkbox, currentIndex) => {
+      const currentGroupSelection = allCheckboxElements.reduce((currentGroupSelection, checkbox) => {
         if ((checkbox as any).checked) {
-          (currentGroupSelection as number[]).push(currentIndex);
+          (currentGroupSelection as string[]).push(checkbox.id);
         }
         return currentGroupSelection;
       }, []);
