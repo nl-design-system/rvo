a304c3fa7ee6383f2312c9b29d41ccb6c5a029b7
Robert Roose <robert.roose@rvo.nl>
2025-03-11 12:35:57 +0100

    Add feedback prototype

---
+++ documentation/demopages/prototypes/www/Feedback.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Feedback from './Feedback';
+
+const meta: Meta<typeof Feedback> = {
+  title: 'Prototypes/www/Feedback',
+  component: Feedback,
+  parameters: {
+    status: {
+      type: 'WORK IN PROGRESS',
+    },
+  },
+};
+export default meta;
+
+type Story = StoryObj<typeof Feedback>;
+
+export const Default: Story = { name: 'Feedback' };
