6006b01f9043806b28683ccea419551121cf2a97
Robert Roose <robert.roose@rvo.nl>
2025-03-11 09:11:31 +0100

    Temporary menu fix

--- packages/docusaurus/src/theme/Navbar/Content/index.tsx
+++ packages/docusaurus/src/theme/Navbar/Content/index.tsx
@@ -1,25 +1,25 @@
-import { useLocation } from '@docusaurus/router';
-import { useThemeConfig } from '@docusaurus/theme-common';
-import { useNavbarSecondaryMenu } from '@docusaurus/theme-common/internal';
-import useBaseUrl from '@docusaurus/useBaseUrl';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import { MenuBarItem } from '@nl-rvo/components';
-import NavbarColorModeToggle from '@theme/Navbar/ColorModeToggle';
-import React from 'react';
-import styles from './styles.module.css';
+// import { useLocation } from '@docusaurus/router';
+// import { useThemeConfig } from '@docusaurus/theme-common';
+// import { useNavbarSecondaryMenu } from '@docusaurus/theme-common/internal';
+// import useBaseUrl from '@docusaurus/useBaseUrl';
+// import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+// import { MenuBarItem } from '@nl-rvo/components';
+// import NavbarColorModeToggle from '@theme/Navbar/ColorModeToggle';
+// import React from 'react';
+// import styles from './styles.module.css';
 
 export default function NavbarContent(): JSX.Element {
-  const items = useThemeConfig().navbar.items as any;
+  // const items = useThemeConfig().navbar.items as any;
   // const searchBarItem = items.find((item) => item.type === 'search');
-  const firstRightItem = items.findIndex((item) => item.position === 'right');
-  const docusaurusContext = useDocusaurusContext();
-  const location = useLocation().pathname.replace(docusaurusContext.siteConfig.baseUrl, '');
-  const currentPage = location.split('/')[0];
-  const sections = useNavbarSecondaryMenu();
+  // const firstRightItem = items.findIndex((item) => item.position === 'right');
+  // const docusaurusContext = useDocusaurusContext();
+  // const location = useLocation().pathname.replace(docusaurusContext.siteConfig.baseUrl, '');
+  // const currentPage = location.split('/')[0];
+  // const sections = useNavbarSecondaryMenu();
 
   return (
     <>
-      {items.map((item, index) => {
+      {/* {items.map((item, index) => {
         let active = false;
         if (item.docId) {
           const itemSection = item.docId.split('/')[0];
@@ -42,11 +42,11 @@ export default function NavbarContent(): JSX.Element {
         };
 
         if (!active) {
-          return <MenuBarItem key={`item-${index}`} {...menuItemProps} />;
+          return <MenuBarItem key={`item-${index}`} {...menuItemProps} size="md" iconPlacement="before" />;
         } else {
           return (
             <React.Fragment key={index}>
-              <MenuBarItem {...menuItemProps} />
+              <MenuBarItem {...menuItemProps} size="md" iconPlacement="before" />
               {sections &&
                 sections.content &&
                 sections.content.props.sidebar.map((section, index) => {
@@ -56,10 +56,11 @@ export default function NavbarContent(): JSX.Element {
                         key={`${section.label}-${index}`}
                         label={section.label}
                         link={section.href}
-                        type="sub"
                         active={section.href.indexOf(location) > -1}
                         useIcons={false}
                         linkColor="logoblauw"
+                        size="md"
+                        iconPlacement="before"
                       />
                     );
                   } else if (section.type === 'category') {
@@ -69,10 +70,11 @@ export default function NavbarContent(): JSX.Element {
                           key={`${section.label}}`}
                           label={section.label}
                           link={section.href}
-                          type="sub"
                           active={section.href && section.href.indexOf(location) > -1}
                           useIcons={false}
                           linkColor="logoblauw"
+                          size="md"
+                          iconPlacement="before"
                         />
                       </React.Fragment>
                     );
@@ -90,6 +92,78 @@ export default function NavbarContent(): JSX.Element {
           <SearchBar />
         </NavbarSearch>
       )} */}
+      {/* Temporary menu fix */}
+      <div className="rvo-menubar__background ">
+        <nav className="rvo-menubar rvo-menubar--md">
+          <ul className="rvo-menubar__list">
+            <div className="rvo-menubar__group--flex">
+              <li className="rvo-menubar__item">
+                <a className="rvo-link rvo-menubar__link rvo-link--logoblauw" href="/rvo/docs">
+                  ROOS
+                </a>
+              </li>
+              <li className="rvo-menubar__item">
+                <a className="rvo-link rvo-menubar__link rvo-link--logoblauw" href="/rvo/docs/aan-de-slag/introductie">
+                  Aan de slag
+                </a>
+              </li>
+              <li className="rvo-menubar__item">
+                <a className="rvo-link rvo-menubar__link rvo-link--logoblauw" href="/rvo/docs/design-tokens/kleuren">
+                  Huisstijl
+                </a>
+              </li>
+              <li className="rvo-menubar__item">
+                <a className="rvo-link rvo-menubar__link rvo-link--logoblauw" href="/rvo/docs/componenten/accordion">
+                  Componenten
+                </a>
+              </li>
+              <li className="rvo-menubar__item">
+                <a
+                  className="rvo-link rvo-menubar__link rvo-link--logoblauw"
+                  href="/rvo/docs/patronen/layout/applicatie-layout"
+                >
+                  Patronen
+                </a>
+              </li>
+              <li className="rvo-menubar__item">
+                <a
+                  className="rvo-link rvo-menubar__link rvo-link--logoblauw"
+                  href="/rvo/docs/schrijfwijzer/waarom-deze-schrijfwijzer"
+                >
+                  Schrijfwijzer
+                </a>
+              </li>
+            </div>
+            <div className="rvo-menubar__group--flex">
+              <li className="rvo-menubar__item">
+                <a
+                  className="rvo-link rvo-menubar__link rvo-link--logoblauw"
+                  href="https://www.figma.com/file/Sj6myBL1Fvot5M1qGxzvEo/ROOS-(RVO-Design-System)?type=design&node-id=484-13305&t=vZTjQBvAiufxd5Qs-0"
+                >
+                  Figma
+                </a>
+              </li>
+              <li className="rvo-menubar__item">
+                <a
+                  className="rvo-link rvo-menubar__link rvo-link--logoblauw"
+                  href="https://github.com/nl-design-system/rvo"
+                >
+                  Github
+                </a>
+              </li>
+              <li className="rvo-menubar__item">
+                <a
+                  className="rvo-link rvo-menubar__link rvo-link--logoblauw"
+                  href="https://nl-design-system.github.io/rvo/"
+                >
+                  Storybook
+                </a>
+              </li>
+            </div>
+          </ul>
+        </nav>
+      </div>
+      {/* End temporary menu fix */}
     </>
   );
 }
