24d6afab8e08b9816bdf39bc334197c7dedba6c2
PLassche-dictu <p.lassche@dictu.nl>
2025-03-20 13:47:42 +0100

    Figma Token javascript file to generate figma.tokens.json (#1102)
    * Figma Token javascript file to generate figma.tokens.json
    
    * rename to figma tokens

---
+++ proprietary/design-tokens/processJsonFiles.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+
+let figmaTokens = { rvo: {}, utrecht: {} };
+
+const fileDirectories = [
+  './src/brand/rvo',
+  './src/common/rvo',
+  './src/common/utrecht',
+  './src/components/rvo',
+  './src/components/utrecht',
+];
+
+function findAllByKeyAndReplace(object, key, replace) {
+  const looper = function (obj) {
+    for (let k in obj) {
+      if (k === key) {
+        obj[replace] = obj[k];
+        delete obj[k];
+      } else if (typeof obj[k] === 'object') {
+        looper(obj[k]);
+      }
+    }
+  };
+  looper(object);
+}
+
+const fConsume = () => {
+  fileDirectories.forEach((directory) => {
+    const files = fs.readdirSync(directory);
+
+    files.forEach((file) => {
+      let rawData = fs.readFileSync(`${directory}/${file}`);
+      let readable = JSON.parse(rawData);
+
+      if (readable.rvo) {
+        figmaTokens.rvo = { ...readable.rvo, ...figmaTokens.rvo };
+      }
+      if (readable.utrecht) {
+        figmaTokens.utrecht = { ...readable.utrecht, ...figmaTokens.utrecht };
+      }
+    });
+  });
+};
+
+fConsume();
+findAllByKeyAndReplace(figmaTokens, 'value', '$value');
+findAllByKeyAndReplace(figmaTokens, 'type', '$type');
+
+fs.writeFile('./figma/figma.tokens.json', JSON.stringify(figmaTokens, null, 2), 'utf8', (err) => {
+  if (err) {
+    console.error('Error writing to file', err);
+  } else {
+    console.log('Data written to file');
+  }
+});
