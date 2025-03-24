148572463d58f33debc7db7407bb5fcc0a284654
PLassche-dictu <p.lassche@dictu.nl>
2025-03-24 08:37:27 +0000

    Deploying to gh-pages from @ nl-design-system/rvo@569401b125eaac43f1f6dc2e1e4cc2387ce4495f 🚀

---
+++ defaultSecondaryFooterItems.ts
@@ -0,0 +1,6 @@
+export const defaultSecondaryFooterItems = [
+  { content: 'Privacy', link: '#' },
+  { content: 'Cookies en anti-spam', link: '#' },
+  { content: 'Toegankelijkheid', link: '#' },
+  { content: 'Proclaimer', link: '#' },
+];
