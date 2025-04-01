14bfdcf5ccd119c17cbc1ab33f6fe1a00d46768c
rroose-rvo <103183050+rroose-rvo@users.noreply.github.com>
2025-04-01 08:43:32 +0000

    Deploying to gh-pages from @ nl-design-system/rvo@eb1e6995b2062127c912618da68df01544f3a98f 🚀

--- defaultSecondaryFooterItems.ts
+++ defaultSecondaryFooterItems.ts
@@ -1,6 +1,8 @@
 export const defaultSecondaryFooterItems = [
-  { content: 'Privacy', link: '#' },
-  { content: 'Cookies en anti-spam', link: '#' },
+  { content: 'Contact', link: 'https://www.rvo.nl/onderwerpen/contact' },
+  { content: 'Privacy', link: 'https://www.rvo.nl/onderwerpen/privacy' },
+  { content: 'Cookies en anti-spam', link: 'https://www.rvo.nl/onderwerpen/cookies' },
   { content: 'Toegankelijkheid', link: '#' },
-  { content: 'Proclaimer', link: '#' },
+  { content: 'Proclaimer', link: 'https://www.rvo.nl/onderwerpen/proclaimer' },
+  { content: 'Kwetsbaarheid op website', link: 'https://www.rvo.nl/onderwerpen/kwetsbaarheid' },
 ];
