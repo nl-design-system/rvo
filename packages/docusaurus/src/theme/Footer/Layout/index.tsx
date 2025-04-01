eb1e6995b2062127c912618da68df01544f3a98f
Robert Roose <103183050+rroose-rvo@users.noreply.github.com>
2025-04-01 10:39:09 +0200

    Docs/roles (#1157)
    * Add Design System Lead role
    
    * Fix docusaurus Footer
    
    * Fix docusaurus footer
    
    * Update default footer items

--- packages/docusaurus/src/theme/Footer/Layout/index.tsx
+++ packages/docusaurus/src/theme/Footer/Layout/index.tsx
@@ -1,5 +1,17 @@
 import { Footer } from '@nl-rvo/components';
 
 export default function FooterLayout(): JSX.Element {
-  return <Footer maxWidth="md" primaryMenu={[]} payOff="" />;
+  return (
+    <Footer
+      maxWidth="md"
+      secondaryMenu={[
+        { content: 'Contact', link: 'https://www.rvo.nl/onderwerpen/contact' },
+        { content: 'Privacy', link: 'https://www.rvo.nl/onderwerpen/privacy' },
+        { content: 'Cookies en anti-spam', link: 'https://www.rvo.nl/onderwerpen/cookies' },
+        { content: 'Proclaimer', link: 'https://www.rvo.nl/onderwerpen/proclaimer' },
+        { content: 'Kwetsbaarheid op website', link: 'https://www.rvo.nl/onderwerpen/kwetsbaarheid' },
+      ]}
+      payOff=""
+    />
+  );
 }
