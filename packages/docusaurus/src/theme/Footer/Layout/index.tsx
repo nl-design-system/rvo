a62263ec086742339f88582a8282d0038e6ab770
Robert Roose <robert.roose@rvo.nl>
2025-04-01 09:57:33 +0200

    Fix docusaurus footer

--- packages/docusaurus/src/theme/Footer/Layout/index.tsx
+++ packages/docusaurus/src/theme/Footer/Layout/index.tsx
@@ -8,7 +8,6 @@ export default function FooterLayout(): JSX.Element {
         { content: 'Contact', link: 'https://www.rvo.nl/onderwerpen/contact' },
         { content: 'Privacy', link: 'https://www.rvo.nl/onderwerpen/privacy' },
         { content: 'Cookies en anti-spam', link: 'https://www.rvo.nl/onderwerpen/cookies' },
-        { content: 'Toegankelijkheid', link: '#' },
         { content: 'Proclaimer', link: 'https://www.rvo.nl/onderwerpen/proclaimer' },
         { content: 'Kwetsbaarheid op website', link: 'https://www.rvo.nl/onderwerpen/kwetsbaarheid' },
       ]}
