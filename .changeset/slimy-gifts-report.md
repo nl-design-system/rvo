82d7db91bef73ea5f97b0fb328dcadf43cd40ed3
PLassche-dictu <p.lassche@dictu.nl>
2025-04-14 13:14:32 +0200

    Custom Image Support for Hero component

---
+++ .changeset/slimy-gifts-report.md
@@ -0,0 +1,6 @@
+---
+"@nl-rvo/component-library-css": patch
+"@nl-rvo/component-library-react": patch
+---
+
+Added support to use img as a acceptable param for the hero image.
---
+++ components/utils/typeUtils.ts
@@ -0,0 +1,2 @@
+export const isOfType = <T>(varToBeChecked: any, propertyToCheckFor: keyof T): varToBeChecked is T =>
+  (varToBeChecked as T)[propertyToCheckFor] !== undefined;
