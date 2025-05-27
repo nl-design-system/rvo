0cffc4fd567fb1f2aefc5aaa74dacc214e5eb997
Jase Terhaar <jase_terhaar@msn.com>
2025-05-27 15:58:13 +0200

    Feature/expandable table component (#1203)
    * Feature - New Expandable Table Component
    ---------
    
    Co-authored-by: terhaarj1 <j.terhaar@dictu.nl>
    Co-authored-by: c.harris1 <c.harris1@dictu.nl>

---
+++ components/expandable-table/table.stories.tsx
@@ -0,0 +1,42 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { defaultArgs } from './src/defaultArgs';
+import ExpandableTable from './src/template';
+
+const argTypes = {
+  tableDescription: {
+    control: {
+      type: 'text',
+    },
+  },
+  className: {
+    control: {
+      type: 'text',
+    },
+  },
+};
+
+const meta: Meta<typeof ExpandableTable> = {
+  title: 'Experimentele Componenten/ExpandableTable',
+  component: ExpandableTable,
+  args: defaultArgs,
+  argTypes,
+  parameters: {
+    controls: {
+      exclude: ['children'],
+    },
+    status: {
+      type: 'WORK IN PROGRESS',
+    },
+    docusaurus: {
+      link: 'table',
+    },
+  },
+};
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  name: 'Expandable Table',
+  args: defaultArgs,
+};
