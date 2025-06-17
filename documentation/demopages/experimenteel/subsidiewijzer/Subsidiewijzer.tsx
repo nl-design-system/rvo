7ee944006fd2c6e04c1392f974b0193e7813a829
Robert Roose <robert.roose@rvo.nl>
2025-06-17 09:45:44 +0200

    Change land filter label text

--- documentation/demopages/experimenteel/subsidiewijzer/Subsidiewijzer.tsx
+++ documentation/demopages/experimenteel/subsidiewijzer/Subsidiewijzer.tsx
@@ -32,7 +32,6 @@ const Subsidiewijzer = () => {
                 link: '#',
               },
               {
-                active: true,
                 label: 'Subsidies en financiering',
                 link: '#',
               },
@@ -196,7 +195,7 @@ const Subsidiewijzer = () => {
 
                         <details open className="rvo-collapsible-filter">
                           <summary className="rvo-collapsible-filter-label">In het land</summary>
-                          <TextInputField label="Zoek land"></TextInputField>
+                          <TextInputField label="Typ land om te zoeken"></TextInputField>
                           <div className="rvo-collapsible-filter--scroll">
                             <CheckboxField
                               label=" "
