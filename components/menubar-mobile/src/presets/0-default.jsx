da6da3a611c1b4052d7df829ea46832c440cf192
Patrick Lassche <p.lassche@dictu.nl>
2025-06-19 08:20:32 +0200

    updated route

--- components/menubar-mobile/src/presets/0-default.jsx
+++ components/menubar-mobile/src/presets/0-default.jsx
@@ -1,4 +1,4 @@
-import { defaultArgs } from '../../defaultArgs';
+import { defaultArgs } from '../../menubar.mobile.stories';
 import MobileMenuBar from '../template';
 
 export default <MobileMenuBar uxpId="mobileMenuBar" {...defaultArgs} />;
