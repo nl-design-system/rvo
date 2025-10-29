f8080fe8bbcf78393fda06ff8dd401f93b75f681
Robert Roose <robert.roose@rvo.nl>
2025-10-29 15:57:12 +0100

    Add Empty Item List example

---
+++ documentation/demopages/voorbeelden/empty-states/EmptyItemList.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import EmptyItemList from './EmptyItemList';
+
+const meta: Meta<typeof EmptyItemList> = {
+  title: "Pagina's/Voorbeelden/Empty states/Empty Item list",
+  component: EmptyItemList,
+  parameters: {
+    status: {
+      type: 'WORK IN PROGRESS',
+    },
+  },
+};
+export default meta;
+
+type Story = StoryObj<typeof EmptyItemList>;
+
+export const Default: Story = { name: 'Empty Item list' };
