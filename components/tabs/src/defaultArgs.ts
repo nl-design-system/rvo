02e472e9e37af10b827998a2804fcbe760661c92
Patrick Lassche <p.lassche@dictu.nl>
2025-12-02 09:04:08 +0100

    Fix storybook property issues

--- components/tabs/src/defaultArgs.ts
+++ components/tabs/src/defaultArgs.ts
@@ -1,6 +1,7 @@
+import { HTMLAttributes } from 'react';
 import { ITabsProps } from './template';
 
-export const defaultArgs: ITabsProps = {
+export const defaultArgs: Partial<ITabsProps & HTMLAttributes<HTMLUListElement>> = {
   tabs: [
     { label: 'Tab 1', href: '#tab-1' },
     { label: 'Tab 2', href: '#tab-2' },
