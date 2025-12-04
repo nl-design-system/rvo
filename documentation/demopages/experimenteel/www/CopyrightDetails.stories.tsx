8b6b6b0df1575af71332ccc2f955f35840e43fbe
Robert Roose <103183050+rroose-rvo@users.noreply.github.com>
2025-12-04 10:22:24 +0100

    Add copyright expandable content variant demopage (#1403)

---
+++ documentation/demopages/experimenteel/www/CopyrightDetails.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import CopyrightDetails from './CopyrightDetails';
+
+const meta: Meta<typeof CopyrightDetails> = {
+  title: "Pagina's/Experimenteel/www/Copyright Details",
+  component: CopyrightDetails,
+  parameters: {
+    status: {
+      type: 'WORK IN PROGRESS',
+    },
+  },
+};
+export default meta;
+
+type Story = StoryObj<typeof CopyrightDetails>;
+
+export const Default: Story = { name: 'Copyright Details' };
