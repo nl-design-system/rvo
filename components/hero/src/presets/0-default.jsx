00a912adbf4c63009f6f26a143bf3942ad1f9144
Dennis Prins <d.prins@dictu.nl>
2026-01-29 13:48:59 +0100

    ROOS-250: Remove content option for hero component. (#1448)

--- components/hero/src/presets/0-default.jsx
+++ components/hero/src/presets/0-default.jsx
@@ -5,7 +5,6 @@ export default (
     uxpId="hero"
     title="Rijksdienst voor Ondernemend Nederland"
     subtitle="Wij helpen u graag vooruit!"
-    content=""
     image={{ src: 'https://placehold.co/600x400', alt: '' }}
     imageAlt="Alt text"
   />
