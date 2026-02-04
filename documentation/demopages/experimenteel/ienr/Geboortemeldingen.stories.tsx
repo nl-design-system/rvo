ddc7bf5c41448643beeb54e55f342e6caca0d4c0
Robert Roose <robert.roose@rvo.nl>
2026-02-04 13:07:31 +0100

    Add geboortemeldingen demopage

---
+++ documentation/demopages/experimenteel/ienr/Geboortemeldingen.stories.tsx
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Geboortemeldingen from './Geboortemeldingen';
+
+const meta: Meta<typeof Geboortemeldingen> = {
+  title: "Pagina's/Experimenteel/I&R/Geboortemeldingen",
+  component: Geboortemeldingen,
+  parameters: {
+    status: {
+      type: 'WORK IN PROGRESS',
+    },
+  },
+};
+export default meta;
+
+type Story = StoryObj<typeof Geboortemeldingen>;
+
+export const Default: Story = { name: 'Geboortemeldingen' };
