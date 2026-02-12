410cca47b9194d653ddfbc02fb20fa698b42cddc
Dennis Prins <d.prins@dictu.nl>
2026-02-12 11:47:41 +0100

    ROOS-303: Add missing alt tags.

--- components/hero/src/presets/0-default.jsx
+++ components/hero/src/presets/0-default.jsx
@@ -5,6 +5,6 @@ export default (
     uxpId="hero"
     title="Rijksdienst voor Ondernemend Nederland"
     subtitle="Wij helpen u graag vooruit!"
-    image={{ src: 'https://placehold.co/600x400', alt: '' }}
+    image={{ src: 'https://placehold.co/600x400', alt: 'placeholder' }}
   />
 );
