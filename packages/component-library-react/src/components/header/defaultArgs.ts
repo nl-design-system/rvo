e9b2a99642d75d1aa0bf3ca4e8335140e2c2c0b8
rroose-rvo <robert.roose@rvo.nl>
2026-06-26 10:47:59 +0200

    Remove link property from Header default arguments

--- packages/component-library-react/src/components/header/defaultArgs.ts
+++ packages/component-library-react/src/components/header/defaultArgs.ts
@@ -4,5 +4,4 @@ import { defaultArgs as logoDefaultArgs } from '../logo/defaultArgs';
 export const defaultArgs: IHeaderProps = {
   title: logoDefaultArgs.title,
   subtitle: logoDefaultArgs.subtitle,
-  link: '',
 };
